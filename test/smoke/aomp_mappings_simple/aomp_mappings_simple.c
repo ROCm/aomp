00e458fffb1728ed43d27e4e7b888775d6cece51
estewart08 <ethan@amd.com>
2019-04-05 12:09:30 -0500

    Added aomp_mappings_simple and updated Makefiles

---
+++ test/smoke/aomp_mappings_simple/aomp_mappings_simple.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <omp.h>
+int main()
+{
+  int N = 102;
+
+  int team_num[N];
+  int smid[N];
+  int i;
+
+  for (i=0; i<N; i++)
+   team_num[i]=smid[i]= -1;
+
+#pragma omp target teams
+//#pragma omp parallel
+  {
+    for (int j = 0; j< N; j++) {
+       team_num[j] = omp_get_team_num();
+       smid[j] = omp_get_smid();
+    }
+  }
+
+  int rc = 0;
+  fprintf(stderr,"    i team# smid\n");
+  for (i=0; i<N; i++)
+    fprintf(stderr," %4d  %4d  %4d\n",
+    i,team_num[i],smid[i]);
+  return rc;
+}
+
+
