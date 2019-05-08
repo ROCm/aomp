f8d30391c6cb1c1dfec3ca2085df581b6a9ec797
estewart08 <ethan@amd.com>
2019-05-08 13:12:12 -0500

    Automated Scripts for Testing (Smoke/HIP/OpenMP/HIP-OpenMP)

---
+++ test/hip-openmp/check_hip-openmp.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+#
+# Checks all tests in smoke directory using make check. Programs return 0 for success or a number > 0 for failure.
+# Tests for visual debugging: aomp_mappings, aomp_mappings_simple
+# Tests that need to be visually inspected: devices, pfspecify, pfspecify_str, stream
+#
+#
+echo ""
+echo ""
+path=$(pwd)
+base=$(basename $path)
+echo ""
+echo "RUNNING ALL TESTS IN: $path "
+echo ""
+
+echo "" > hip-openmp-check.txt
+echo "" >> hip-openmp-check.txt
+echo "*******A non-zero exit code means a failure occured.*******" >> hip-openmp-check.txt
+echo "*************************************************************" >> hip-openmp-check.txt
+
+#Loop over all directories and make run / make check depending on directory name
+for directory in ./*/; do 
+	(cd "$directory" && path=$(pwd) && base=$(basename $path) 
+		make clean
+		make	
+		make run
+		echo " Return Code for $base: $?" >> ../hip-openmp-check.txt
+		make clean	
+		
+	)
+	
+done
+
+#Print run.log for all tests that need visual inspection
+for directory in ./*/; do
+	(cd "$directory" && path=$(pwd) && base=$(basename $path)
+	)
+done
+
+cat hip-openmp-check.txt
