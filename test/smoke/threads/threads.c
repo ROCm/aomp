9bfa0a75e9ae1e01758728d53760a6590b231748
Ron Lieberman <ron.lieberman@amd.com>
2019-05-08 15:02:39 -0500

    Modify tests for const thread limit
    Extend threads, Threadsxxxx to 1024 const thread limit

--- test/smoke/threads/threads.c
+++ test/smoke/threads/threads.c
@@ -3,13 +3,27 @@
 
 int main()
 {
-int thread_id = -1;
+int thread_id[1024] ;
+for (int i=0; i < 1024; i++) thread_id[i] = -1;
 //#pragma omp target map (tofrom: thread_id)
-#pragma omp target parallel num_threads(1) map(tofrom: thread_id)
-{
-    printf ("Thread: %d\n", omp_get_thread_num());
-    thread_id = omp_get_thread_num();
+  #pragma omp target parallel for num_threads(1024)
+  for (int i=0; i< 1024; i++) {
+    if (i >950)
+      printf ("Thread: %d\n", omp_get_thread_num());
+    thread_id[i] = omp_get_thread_num();
+  }
+  // SPMD: if (thread_id[1023] == 1023 ) {
+  int maxThrd = -1;
+  for (int i=0; i < 1024; i++) {
+    if (thread_id[i] > maxThrd)
+     maxThrd = thread_id[i];
+  }
+  printf("Max thread id %d\n", maxThrd);
+  if (maxThrd == 959) {
+    printf("Passed\n");
+    return 0;
+  } else {
+    printf("Failed\n");
+    return 1;
+  }
 }
-  return (thread_id == 0)? 0:1;
-}
-
