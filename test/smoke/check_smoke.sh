489d429a0dabdbb4e2280a22f8907e64805df89c
estewart08 <ethan@amd.com>
2019-05-22 15:44:22 -0500

    Fixed spacing issues on check_smoke.sh

--- test/smoke/check_smoke.sh
+++ test/smoke/check_smoke.sh
@@ -36,87 +36,87 @@ echo "**************************************************************************
 
 #Loop over all directories and make run / make check depending on directory name
 for directory in ./*/; do
-	shopt -s lastpipe
-	echo -n "$directory " | tr -d ./ | read tempDir
-	tempDir+=" "
-	allTests+="$tempDir"
-	let count++
-	(cd "$directory" && path=$(pwd) && base=$(basename $path) 
-		if [ $base == 'devices' ] || [ $base == 'pfspecifier' ] || [ $base == 'pfspecifier_str' ] || [ $base == 'stream' ] ; then 
-			make
+  shopt -s lastpipe
+  echo -n "$directory " | tr -d ./ | read tempDir
+  tempDir+=" "
+  allTests+="$tempDir"
+  let count++
+  (cd "$directory" && path=$(pwd) && base=$(basename $path)
+    if [ $base == 'devices' ] || [ $base == 'pfspecifier' ] || [ $base == 'pfspecifier_str' ] || [ $base == 'stream' ] ; then
+      make
       if [ $? -ne 0 ]; then
-			echo "$base: Make Failed" >> ../make-fail.txt
+        echo "$base: Make Failed" >> ../make-fail.txt
       fi
-			make run > /dev/null 2>&1
-		  make check > /dev/null 2>&1
+      make run > /dev/null 2>&1
+      make check > /dev/null 2>&1
 
-		#flags has multiple runs
-		elif [ $base == 'flags' ] ; then
-			file="options.txt"
+    #flags has multiple runs
+    elif [ $base == 'flags' ] ; then
+      file="options.txt"
 
-			#Regex to search for "march=gfxXXX or sm_XX"
-			march_regex="(march=[a-z]+[0-9]*_?[0-9]+)"
+      #Regex to search for "march=gfxXXX or sm_XX"
+      march_regex="(march=[a-z]+[0-9]*_?[0-9]+)"
 
-			#Read file and replace march with correct GPU and keep track of execution number
-			test_num=0;
-			while read -r line; do
-				((test_num++))
-				#if GPU is involved
-				if [[ "$line" =~ $march_regex ]]; then
-					march_match=${BASH_REMATCH[1]}
-					#remove march from command and replace with correct version
-					temp_line=${line/"-$march_match"}
-					mygpu=$AOMP_GPU
-					#If NVIDIA system, add nvptx targets, cuda, and remove amdgcn targets. This is done for testing purpose to avoid rewriting original amd command.
-					if [[ "$AOMP_GPU" == *"sm"* ]]; then
-						target_regex="(-fopenmp-[a-z]*=[a-z,-]*).*(-Xopenmp-[a-z]*=[a-z,-]*)"
-						if [[ "$line" =~ $target_regex ]]; then
-							target_match="${BASH_REMATCH[1]} ${BASH_REMATCH[2]}"
-							temp_line=${temp_line/"$target_match"}
-							nvidia_args="-fopenmp-targets=nvptx64-nvidia-cuda -Xopenmp-target=nvptx64-nvidia-cuda"
-							cuda_args="-L/usr/local/cuda/targets/x86_64-linux/lib -lcudart"
-						fi
-				  fi
-					#send variables to make
-					make make_options="$temp_line" nvidia_targets="$nvidia_args" march="-march=$mygpu" cuda="$cuda_args"
-					make make_options="$temp_line" nvidia_targets="$nvidia_args" march="-march=$mygpu" cuda="$cuda_args" test_num=$test_num check > /dev/null 2>&1
-					if [ $? -ne 0 ]; then
-						echo "$base $test_num: Make Failed" >> ../make-fail.txt
-					fi
-				else
-					make make_options="$line"
-					make make_options="$line" test_num=$test_num check > /dev/null 2>&1
-					if [ $? -ne 0 ]; then
-						echo "$base $test_num: Make Failed" >> ../make-fail.txt
-					fi
-				fi
-				echo ""
-			done < "$file"
+      #Read file and replace march with correct GPU and keep track of execution number
+      test_num=0;
+      while read -r line; do
+        ((test_num++))
+        #if GPU is involved
+        if [[ "$line" =~ $march_regex ]]; then
+          march_match=${BASH_REMATCH[1]}
+          #remove march from command and replace with correct version
+          temp_line=${line/"-$march_match"}
+          mygpu=$AOMP_GPU
+          #If NVIDIA system, add nvptx targets, cuda, and remove amdgcn targets. This is done for testing purpose to avoid rewriting original amd command.
+          if [[ "$AOMP_GPU" == *"sm"* ]]; then
+            target_regex="(-fopenmp-[a-z]*=[a-z,-]*).*(-Xopenmp-[a-z]*=[a-z,-]*)"
+            if [[ "$line" =~ $target_regex ]]; then
+              target_match="${BASH_REMATCH[1]} ${BASH_REMATCH[2]}"
+              temp_line=${temp_line/"$target_match"}
+              nvidia_args="-fopenmp-targets=nvptx64-nvidia-cuda -Xopenmp-target=nvptx64-nvidia-cuda"
+              cuda_args="-L/usr/local/cuda/targets/x86_64-linux/lib -lcudart"
+            fi
+          fi
+          #send variables to make
+          make make_options="$temp_line" nvidia_targets="$nvidia_args" march="-march=$mygpu" cuda="$cuda_args"
+          make make_options="$temp_line" nvidia_targets="$nvidia_args" march="-march=$mygpu" cuda="$cuda_args" test_num=$test_num check > /dev/null 2>&1
+          if [ $? -ne 0 ]; then
+            echo "$base $test_num: Make Failed" >> ../make-fail.txt
+          fi
+        else
+          make make_options="$line"
+          make make_options="$line" test_num=$test_num check > /dev/null 2>&1
+          if [ $? -ne 0 ]; then
+            echo "$base $test_num: Make Failed" >> ../make-fail.txt
+          fi
+        fi
+        echo ""
+      done < "$file"
 
-		else
-			make
+    else
+      make
       if [ $? -ne 0 ]; then
-				echo "$base: Make Failed" >> ../make-fail.txt
+        echo "$base: Make Failed" >> ../make-fail.txt
       fi
-			make check > /dev/null 2>&1
-		fi
-		echo ""
-	)
+      make check > /dev/null 2>&1
+    fi
+    echo ""
+    )
 	
 done
 
 #Print run.log for all tests that need visual inspection
 for directory in ./*/; do
-	(cd "$directory" && path=$(pwd) && base=$(basename $path)
-		if [ $base == 'devices' ] || [ $base == 'pfspecifier' ] || [ $base == 'pfspecifier_str' ] || [ $base == 'stream' ] ; then 
-			echo ""
-			echo -e "$ORG"$base - Run Log:"$BLK"
-			echo "--------------------------"
-			cat run.log
-			echo ""
-		  echo ""
+  (cd "$directory" && path=$(pwd) && base=$(basename $path)
+    if [ $base == 'devices' ] || [ $base == 'pfspecifier' ] || [ $base == 'pfspecifier_str' ] || [ $base == 'stream' ] ; then
+      echo ""
+      echo -e "$ORG"$base - Run Log:"$BLK"
+      echo "--------------------------"
+      cat run.log
+      echo ""
+      echo ""
     fi
-	)
+  )
 done
 
 #Replace false positive return codes with 'Check the run.log' so that user knows to visually inspect those.
@@ -129,10 +129,10 @@ echo ""
 #Gather Test Data
 ((total_tests=$(wc -l <  passing-tests.txt)))
 if [ -e make-fail.txt ]; then
-	((total_tests+=$(wc -l <  make-fail.txt)))
+  ((total_tests+=$(wc -l <  make-fail.txt)))
 fi
 if [ -e failing-tests.txt ]; then
-	((total_tests+=$(wc -l <  failing-tests.txt)))
+  ((total_tests+=$(wc -l <  failing-tests.txt)))
 fi
 
 #Print Results
@@ -145,16 +145,16 @@ echo ""
 #Print failed tests
 echo -e "$RED"
 if [ -e failing-tests.txt ]; then
-	echo "Runtime Fails"
-	echo "--------------------"
-	cat failing-tests.txt
-	echo ""
+  echo "Runtime Fails"
+  echo "--------------------"
+  cat failing-tests.txt
+  echo ""
 fi
 
 if [ -e make-fail.txt ]; then
-	echo "Compile Fails"
-	echo "--------------------"
-	cat make-fail.txt
+  echo "Compile Fails"
+  echo "--------------------"
+  cat make-fail.txt
 fi
 echo -e "$BLK"
 
@@ -173,8 +173,8 @@ make clean > /dev/null 2>&1
 rm check-smoke.txt
 rm passing-tests.txt
 if [ -e failing-tests.txt ]; then
-	rm failing-tests.txt
+  rm failing-tests.txt
 fi
 if [ -e make-fail.txt ]; then
-	rm make-fail.txt
+  rm make-fail.txt
 fi
