5b9b63b32303573098841b3d92570003e55d74bd
estewart08 <ethan@amd.com>
2019-06-18 16:16:15 -0500

    Smoke test changes: firstprivate2-added correct pragma, error checking; added new test target_teams_reduction; fixed Makefile typo

--- test/smoke/data_enter_issue01/data_enter_issue01.cpp
+++ test/smoke/data_enter_issue01/data_enter_issue01.cpp
@@ -1,17 +1,26 @@
 #include <cmath>
+#include <stdio.h>
 
 int main(int argc, char ** argv) {
   double x0 = 1.0;
   double sumx = 0.0;
+  double n = 1000.0;
 
   #pragma omp target enter data map(to:x0)
 
   #pragma omp target teams distribute parallel for map(tofrom: sumx) reduction(+:sumx)
-  for (std::size_t i = 0; i < 1000; ++i ) {
+  for (std::size_t i = 0; i < n; ++i ) {
     sumx += x0;
   }
 
   #pragma omp target exit data map(delete: x0)
-}
+  if(sumx != n){
+    printf("Fail\n");
+    return 1;
+  }
 
+  else
+    printf("Success!\n");
+  return 0;
+}
 
