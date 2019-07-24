cc2cd4c0da3b8368234cb3847f9acff9b0f4e7ba
estewart08 <ethan@amd.com>
2019-07-24 09:52:33 -0500

    [threds smoke test]-Simplified verification of execution mode

--- test/smoke/threads/threads.c
+++ test/smoke/threads/threads.c
@@ -2,35 +2,9 @@
 #include <omp.h>
 #include <string.h>
 #include <stdlib.h>
-/*Starting with clang version 9 this test is considered to be SPMD mode. Previously, clang 8 considered this to be generic,
-so now this test determines the clang version to verify the results.*/
 
 int main()
 {
-  FILE * file;
-  char ln[256];
-
-  //Default Clang version set to 8
-  int clangVersion = 8;
-
-  //Determine which Clang version
-  system("$AOMP/bin/clang --version > clang.txt");
-  file = fopen("clang.txt", "r");
-  if (!file)
-    printf("Clang version file not found!\n");
-  else{
-    char *found = NULL;
-    while((fgets(ln, 256, file) != NULL)){
-       found = strstr(ln, "9.");
-       if(found){
-         clangVersion = 9;
-         printf("Clang version: %s\n", found);
-         break;
-       }
-    }
-  }
-  fclose(file);
-  system("rm clang.txt");
   //Determine which GPU type (NVIDIA or AMD)
   char* nvidia= "sm";
   char* aomp_gpu= getenv("AOMP_GPU");
@@ -40,14 +14,9 @@ int main()
   if(aomp_gpu && strstr(aomp_gpu, nvidia) != NULL)
     isAMDGPU = 0;
 
-  if(isAMDGPU && clangVersion < 9)
-    masterWarpThread = 959;
-
-  if(!isAMDGPU)
-    masterWarpThread = 991;
-
   int thread_id[1024] ;
   for (int i=0; i < 1024; i++) thread_id[i] = -1;
+
   //#pragma omp target map (tofrom: thread_id)
   #pragma omp target parallel for num_threads(1024)
   for (int i=0; i< 1024; i++) {
@@ -62,15 +31,33 @@ int main()
      maxThrd = thread_id[i];
   }
   printf("Max thread id %d\n", maxThrd);
-  if (maxThrd == masterWarpThread && clangVersion < 9) {
-    printf("Passed\n");
-    return 0;
-  } else if (maxThrd == 1023 && clangVersion > 8){
-    printf("Passed\n");
+
+  //Determine execution Mode
+  if (maxThrd == 1023)
+    printf("Running in SPMD Mode\n");
+  else
+    printf("Running in Generic Mode\n");
+
+  //Verify Results
+  int passed = 0;
+
+  //Check SPMD results
+  if (maxThrd == 1023)
+    passed = 1;
+  else{
+    //Check generic results
+    if(isAMDGPU)
+      maxThrd += 64;
+    else
+       maxThrd += 32;
+    if (maxThrd == 1023)
+      passed = 1;
+  }
+  if (passed){
+    printf("Passed!\n");
     return 0;
   } else{
     printf("Failed\n");
     return 1;
-
   }
 }
