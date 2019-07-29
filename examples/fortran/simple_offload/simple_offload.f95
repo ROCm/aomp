7634c94d26f7df17cadea66f6cc64e742a3060fa
richardbleikamp <richard.bleikamp@amd.com>
2019-07-29 16:08:38 -0400

    Add simple Fortran offloading test for AMD GPU

---
+++ examples/fortran/simple_offload/simple_offload.f95
@@ -0,0 +1,33 @@
+! example of simple Fortran AMD GPU offloading
+program main
+  parameter (nsize=1000000)
+  real a(nsize), b(nsize), c(nsize)
+  integer i
+
+  do i=1,nsize
+    a(i)=0
+    b(i) = i
+    c(i) = 10
+  end do
+
+  call foo(a,b,c)
+
+  write(6,*)"a(1)=", a(1), "    a(2)=", a(2)
+  if (a(1).ne.11 .or. a(2).ne.22) then
+    stop "ERROR: wrong answers"
+  endif
+  write(6,*)"Success: if a diagnostic line starting with DEVID was output"
+  return
+end
+subroutine foo(a,b,c)
+  parameter (nsize=1000000)
+  real a(nsize), b(nsize), c(nsize)
+  integer i
+!$omp target map(from:a) map(to:b,c)
+!$omp parallel do
+  do i=1,nsize
+    a(i) = b(i) * c(i) + i
+  end do
+!$omp end target
+  return
+end
