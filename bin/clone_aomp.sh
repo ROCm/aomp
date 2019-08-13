60edb12ebdf5121fa5cff66ce8beb6bdc3ef2a2a
gregrodgers <Gregory.Rodgers@amd.com>
2019-08-13 11:40:44 -0500

    Add internal repo for OpenMP application testing

--- bin/clone_aomp.sh
+++ bin/clone_aomp.sh
@@ -133,3 +133,19 @@ repo_web_location=$GITROCLIB
 reponame=$AOMP_APPS_REPO_NAME
 COBRANCH=$AOMP_APPS_REPO_BRANCH
 clone_or_pull
+
+# ---------------------------------------
+# The following repo is internal to AMD
+# ---------------------------------------
+ping -c 1 $AOMP_INTERNAL_IP
+if [ $? == 0 ] ; then
+   echo
+   echo " +---------------------------------------------------------------"
+   echo " |  WARNING: USE YOUR AMD USERID AND PASSWORD TO CLONE FROM $GITINTERNAL"
+   echo " +---------------------------------------------------------------"
+   echo
+   repo_web_location=$GITINTERNAL
+   reponame=$AOMP_IAPPS_REPO_NAME
+   COBRANCH=$AOMP_IAPPS_REPO_BRANCH
+   clone_or_pull
+fi
