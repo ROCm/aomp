eb0690c06b19b55e71b929e803c2df04e7c6426f
gregrodgers <Gregory.Rodgers@amd.com>
2019-09-30 16:26:46 -0500

    this new smoke test has a barrier in the sequential part of the target offload that causes a hang in 0.7-3 because it introduces a full barrier while the worker warps are waiting for the master to schedule work.  In nvptx, they use a partial barrier for this.  Here we use the same trick we used for the master.  That is we loop on the worker barrier till the master is not busy

---
+++ test/smoke/MasterBarrier/MasterBarrier.cpp
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <omp.h>
+
+#define N     100000
+#define VALUE 123456
+
+int main(int argc, char *argv[]) {
+
+  int host[N] = {0x0};
+
+  for(int dev = 0; dev < omp_get_num_devices(); ++dev)
+  {
+    #pragma omp target device(dev) map(from:host[0:N]) 
+    {
+      static int A;
+      #pragma omp allocate(A) allocator(omp_pteam_mem_alloc)
+      A = VALUE;
+      #pragma omp barrier
+
+      #pragma omp parallel for num_threads(N)
+      for(int i = 0 ; i<N ; i++) {
+        host[i] = A;
+      }
+    }
+
+    for (int i = 0; i < N;  ++i) {
+      if (host[i] != VALUE) {
+        printf("Failed on device %d host[%d]: %d (instead of %d)\n",dev,i,host[i],VALUE);
+        return 1;
+      }
+    }
+  }
+
+  printf("OK\n");
+
+  return 0;
+}
+
+
