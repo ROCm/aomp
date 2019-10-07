cceca8229bce4300b2fc01bc482b52511c78461f
gregrodgers <gregory.rodgers@amd.com>
2019-10-07 15:37:36 -0500

    thread local variables need to be declared thread local with the new openmp alloator

--- test/smoke/math_modff/math_modff.cpp
+++ test/smoke/math_modff/math_modff.cpp
@@ -1,4 +1,5 @@
 #include <cmath>
+#include <omp.h>
 using namespace std;
 
 int main(int argc, char **argv)
@@ -6,12 +7,14 @@ int main(int argc, char **argv)
   #pragma omp target
   {
     float intpart, res;
+    #pragma omp allocate(intpart) allocator(omp_thread_mem_alloc)
     res = modff(1.1f, &intpart);
   }
 
   #pragma omp target
   {
     double intpart, res;
+    #pragma omp allocate(intpart) allocator(omp_thread_mem_alloc)
     res = modf(1.1, &intpart);
   }
   return 0;
