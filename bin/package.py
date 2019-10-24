d4e744edb5aca359dcbd7a40dd0f5c4097495fba
gregrodgers <Gregory.Rodgers@amd.com>
2019-10-24 14:09:21 -0500

    add the correct sha256 value for the 0.7-5 source tarball

--- bin/package.py
+++ bin/package.py
@@ -12,9 +12,10 @@ class Aomp(MakefilePackage):
     """  llvm openmp compiler from AMD"""
 
     homepage = "https://github.com/ROCm-Developer-Tools/aomp"
-    url      = "https://github.com/ROCm-Developer-Tools/aomp/releases/download/rel_0.7-3/aomp-0.7-3.tar.gz"
+    url      = "https://github.com/ROCm-Developer-Tools/aomp/releases/download/rel_0.7-5/aomp-0.7-5.tar.gz"
 
-    version('0.7-3', sha256='9a4971df847a0b0d9913ced5ba19e6574e8823d5b74aaac725945f7360402be1')
+    version('0.7-5', sha256='8f3b20e57bf2032d388879429f29b729ce9a46bee5e7ba76976fc77ea48707a7')
+
     family = 'compiler'
 
     def edit(self, spec, prefix):
