d65e0d661605ff6d98c797aac64cbc84466920e1
Ron Lieberman <ron.lieberman@amd.com>
2019-11-26 09:26:39 -0600

    use original acceptance test, not modified one

--- examples/fortran/helloAcceptance/hello.f90
+++ examples/fortran/helloAcceptance/hello.f90
@@ -1,24 +1,19 @@
-         program simple
-         use omp_lib, ONLY : omp_is_initial_device
-         integer onGPU
-         integer n
-         n=1
-         print *, "Putting FORTRAN print before target GPU test!"
-         onGPU = 0;
-!$omp target teams distribute parallel do  map(tofrom:onGPU) private(i)
-         do i=1,1
-         if (.not. omp_is_initial_device()) then
-              onGPU = 1
-         else
-              onGPU = 2
-         end if
-         end do
-!$omp end target teams distribute parallel do
-         if (onGPU .eq. 1) then
-            print *, 'Fortran is running on the GPU, returned', onGPU
-         else
-            print *, 'Fortran is NOT running on the GPU, returned', onGPU
-         end if
-         print *, 'Exiting program for fortran'
-         end
-
+     program simple
+     use omp_lib, ONLY : omp_is_initial_device
+     integer onGPU
+     print *, "Putting FORTRAN print before target GPU test!"
+     onGPU = 0;
+     !$omp target map(from:onGPU)
+     if (.not. omp_is_initial_device()) then
+     onGPU = 1
+     else
+     onGPU = 2
+     end if
+     !$omp end target
+     if (onGPU .eq. 1) then
+     print *, 'Fortran is running on the GPU, returned', onGPU
+     else
+     print *, 'Fortran is NOT running on the GPU, returned', onGPU
+     end if
+     print *, 'Exiting program for fortran'
+     end
