3ac2f4018e7753901bf33af01eb32e19ea61ca0b
Ron Lieberman <ron.lieberman@amd.com>
2019-12-15 21:49:23 -0600

    Cleanup snap_red

--- test/smoke/snap_red/snap_red.c
+++ test/smoke/snap_red/snap_red.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 #define N   10
 
 
@@ -12,14 +11,13 @@ int main (void)
   int cmom = 14;
   int nxyz = 5000;
 // fails for 149 and above: nxyz=149;
-  #pragma omp target teams distribute num_teams(149) thread_limit(ng*(cmom-1)) map(tofrom:aa)
+  #pragma omp target teams distribute num_teams(nxyz) thread_limit(ng*(cmom-1)) map(tofrom:aa)
   for (int gid = 0; gid < nxyz; gid++) {
-//  int bb=0;
-    #pragma omp parallel for collapse(2)
+    #pragma omp parallel for  collapse(2) 
     for (unsigned int g = 0; g < ng; g++) {
       for (unsigned int l = 0; l < cmom-1; l++) {
         int a = 0;
-        #pragma omp  parallel for reduction(+:a)
+        #pragma omp parallel for reduction(+:a)
         for (int i = 0; i < N; i++) {
           a += i;
         }
@@ -27,12 +25,11 @@ int main (void)
         aa += a;
       }
     }  
-//  #pragma omp atomic
-  //aa += bb;
   }
-  printf ("The result is = %ld!\n", aa);
-  if (aa != 35100000) {
-    printf("Failed\n");
+  long exp = (long)ng*(cmom-1)*nxyz*(N*(N-1)/2);
+  printf ("The result is = %ld exp:%ld!\n", aa,exp);
+  if (aa != exp) {
+    printf("Failed %ld\n",aa);
     return 1;
   }
   return 0;
