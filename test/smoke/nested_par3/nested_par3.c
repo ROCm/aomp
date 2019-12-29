412865fa3db17206c8f4247e5ca5a32c45d8d5b2
Ron Lieberman <ron.lieberman@amd.com>
2019-12-29 12:14:17 -0600

    add different number of nested parallel levels
    4 kernels with differing levels: 1, 0, 3, 2

--- test/smoke/nested_par3/nested_par3.c
+++ test/smoke/nested_par3/nested_par3.c
@@ -12,7 +12,80 @@ int main (void)
   int nxyz = 5;
 #pragma omp target teams distribute num_teams(nxyz) thread_limit(4) map(tofrom:aa)
   for (int gid = 0; gid < nxyz; gid++) {
-    #pragma omp parallel for  collapse(2) 
+    #pragma omp parallel for  collapse(2)
+    for (unsigned int g = 0; g < ng; g++) {
+      for (unsigned int l = 0; l < cmom-1; l++) {
+        int a = 0;
+        for (int ii = 0; ii < N+2; ii++) {
+          #pragma omp parallel for reduction(+:a)
+          for (int i = 0; i < N; i++) {
+            a += i;
+          }
+        }
+        #pragma omp atomic
+        aa += a;
+      }
+    }
+  }
+  long exp = (long)ng*(cmom-1)*nxyz*(N*(N-1)/2)*(N+2);
+  fprintf (stderr, "The result is = %ld exp:%ld!\n", aa,exp);
+  if (aa != exp) {
+    fprintf(stderr, "Failed %ld\n",aa);
+    return 1;
+  }
+  aa = 0;
+#pragma omp target teams distribute num_teams(nxyz) thread_limit(4) map(tofrom:aa)
+  for (int gid = 0; gid < nxyz; gid++) {
+    for (unsigned int g = 0; g < ng; g++) {
+      for (unsigned int l = 0; l < cmom-1; l++) {
+        int a = 0;
+        for (int ii = 0; ii < N+2; ii++) {
+          #pragma omp parallel for reduction(+:a)
+          for (int i = 0; i < N; i++) {
+            a += i;
+          }
+        }
+        #pragma omp atomic
+        aa += a;
+      }
+    }
+  }
+  exp = (long)ng*(cmom-1)*nxyz*(N*(N-1)/2)*(N+2);
+  fprintf (stderr, "The result is = %ld exp:%ld!\n", aa,exp);
+  if (aa != exp) {
+    fprintf(stderr, "Failed %ld\n",aa);
+    return 1;
+  }
+  aa = 0;
+#pragma omp target teams distribute num_teams(nxyz) thread_limit(4) map(tofrom:aa)
+  for (int gid = 0; gid < nxyz; gid++) {
+    #pragma omp parallel for
+    for (unsigned int g = 0; g < ng; g++) {
+      #pragma omp parallel for
+      for (unsigned int l = 0; l < cmom-1; l++) {
+        int a = 0;
+        #pragma omp parallel for
+        for (int ii = 0; ii < N+2; ii++) {
+          #pragma omp parallel for reduction(+:a)
+          for (int i = 0; i < N; i++) {
+            a += i;
+          }
+        }
+        #pragma omp atomic
+        aa += a;
+      }
+    }
+  }
+  exp = (long)ng*(cmom-1)*nxyz*(N*(N-1)/2)*(N+2);
+  fprintf (stderr, "The result is = %ld exp:%ld!\n", aa,exp);
+  if (aa != exp) {
+    fprintf(stderr, "Failed %ld\n",aa);
+    return 1;
+  }
+  aa = 0;
+#pragma omp target teams distribute num_teams(nxyz) thread_limit(7) map(tofrom:aa)
+  for (int gid = 0; gid < nxyz; gid++) {
+    #pragma omp parallel for  collapse(2)
     for (unsigned int g = 0; g < ng; g++) {
       for (unsigned int l = 0; l < cmom-1; l++) {
         int a = 0;
@@ -23,15 +96,14 @@ int main (void)
             a += i;
           }
         }
-        #pragma omp atomic 
+        #pragma omp atomic
         aa += a;
       }
-    }  
+    }
   }
-  long exp = (long)ng*(cmom-1)*nxyz*(N*(N-1)/2)*(N+2);
-  printf ("The result is = %ld exp:%ld!\n", aa,exp);
+  fprintf (stderr, "The result is = %ld exp:%ld!\n", aa,exp);
   if (aa != exp) {
-    printf("Failed %ld\n",aa);
+    fprintf(stderr, "Failed %ld\n",aa);
     return 1;
   }
   return 0;
