af6a91232217dd6b6a8dfeeceefa8f045bf6ea5f
jsjodin <jan_sjodin@yahoo.com>
2020-03-11 19:00:49 -0400

    Revert "Add hip to RAJA vecadd example."

--- examples/raja/vecadd/vecadd.cpp
+++ examples/raja/vecadd/vecadd.cpp
@@ -1,4 +1,4 @@
-#include <ostream>
+#include <stdio.h>
 #include <omp.h>
 
 #include "RAJA/RAJA.hpp"
@@ -6,57 +6,51 @@
 using namespace RAJA;
 using namespace RAJA::statement;
 
-const int N = 1000000;
+#define N 100
 int main()
 {
 
-  int* a = new int[N];
-  int* b = new int[N];
-  int* c = new int[N];
+  int* a = new int[N*N];
+  int* b = new int[N*N];
+  int* r = new int[N*N];
 
-  for (int i = 0; i < N; ++i) {
-    a[i] = 2*i;
-    b[i] = i;
-  }
+  int i;
 
-#if defined(RAJA_USE_HIP)
-  // HIP vector add
-  int *device_a;
-  int *device_b;
-  int *device_c;
+  for (i=0; i<N*N; i++)
+    a[i]=i;
 
-  hipErrchk(hipMalloc((void **)&device_a, sizeof(int) * N));
-  hipErrchk(hipMalloc((void **)&device_b, sizeof(int) * N));
-  hipErrchk(hipMalloc((void **)&device_c, sizeof(int) * N));
+  for (i=0; i<N*N; i++)
+    b[i]=i*2;
 
-  hipErrchk(hipMemcpy( device_a, a, N * sizeof(int), hipMemcpyHostToDevice ));
-  hipErrchk(hipMemcpy( device_b, b, N * sizeof(int), hipMemcpyHostToDevice ));
-
-  RAJA::forall<RAJA::hip_exec<256>>(RAJA::RangeSegment(0, N),
-    [=] RAJA_DEVICE (int i) {
-    device_c[i] = device_a[i] + device_b[i];
-  });
-
-  hipErrchk(hipMemcpy( c, device_c, N * sizeof(int), hipMemcpyDeviceToHost ));
-
-#else
-  // OpenMP target vecadd
-#pragma omp target data map(to: a[0:N]) map(to: b[0:N]) map(from: c[0:N]) use_device_ptr(a) use_device_ptr(b)
-  RAJA::forall<RAJA::omp_target_parallel_for_exec<256>>(RAJA::RangeSegment(0, N),
-    [=](int i) {
-    c[i] = a[i] + b[i];
-  });
-#endif
-
-  // Check output
   for(int i = 0; i < N; ++i) {
-    if(c[i] != 3*i) {
-      std::cout << "Error, wrong result at index " << i << " expected " << 3*i << " got " << c[i] << "\n";
-      return 1;
+    for(int j = 0; j < N; ++j) {
+      r[i + (N*j)] = a[i + (N*j)] + b[i + (N*j)];
     }
   }
 
-  std::cout << "Success!\n";
-  return 0;
+   using Pol = KernelPolicy<
+    Collapse<omp_target_parallel_collapse_exec, ArgList<0,1>, Lambda<0> > >;
+
+#pragma omp target data map(tofrom: a[0:N*N]) map(to: b[0:N*N]) use_device_ptr(a) use_device_ptr(b)
+  RAJA::kernel<Pol>(
+      RAJA::make_tuple(
+        RAJA::RangeSegment(0,N),
+        RAJA::RangeSegment(0,N)),
+      [=] (int i, int j) {
+	a[i + (N*j)] = a[i + (N*j)] + b[i + (N*j)];
+      });
+
+  // Check Result
+  int rc = 0;
+  for(int w = 0; w < N*N; ++w) {
+    if (a[w] != r[w]) {
+      printf("Error a[%d] = %d, expected %d\n",w, a[w], r[w]);
+    }
+  }
+
+  if (!rc)
+    printf("Success\n");
+
+  return rc;
 }
 
