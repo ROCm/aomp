14f3b6e43aa7673a92adde2e3bd8ed1d4f4e1eee
Ron Lieberman <ron.lieberman@amd.com>
2020-04-11 13:42:38 -0500

    Add map_zero_bug - fails with copy async failure
    [/home/rlieberm/git/aomp11/atmi/src/runtime/core/data.cpp:729] Copy async between memory pools failed: HSA_STATUS_ERROR_INVALID_ARGUMENT

--- test/smoke/check_smoke.sh
+++ test/smoke/check_smoke.sh
@@ -34,7 +34,7 @@ echo "                   A non-zero exit code means a failure occured." >> check
 echo "Tests that need to be visually inspected: devices, pfspecify, pfspecify_str, stream" >> check-smoke.txt
 echo "***********************************************************************************" >> check-smoke.txt
 
-known_fails="red_bug_51 reduction_array_section target_teams_reduction tasks"
+known_fails="reduction_array_section target_teams_reduction tasks map_zero_bug"
 
 if [ "$SKIP_FAILURES" == 1 ] ; then
   skip_tests=$known_fails
