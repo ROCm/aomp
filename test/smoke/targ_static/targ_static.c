67e3808b2b2f018ce9ef70ee06bf61bdf054789b
gregrodgers <Gregory.Rodgers@amd.com>
2020-05-04 16:24:46 -0500

    remove teams that cause this to fail on nvidia

--- test/smoke/targ_static/targ_static.c
+++ test/smoke/targ_static/targ_static.c
@@ -11,17 +11,16 @@ static int colstat[NZ];
 int main(){
 	colstat[0]=-1;
 #pragma omp target map(alloc:colstat[0:NZ])
-#pragma omp teams 
   {
     colstat[1] = 1111;
   }
 #pragma omp target map(alloc:colstat[:0])
-#pragma omp teams 
   {
     colstat[2] = 2222;
   }
+  fprintf(stderr, "BEFORE colstat[0..2] %d %d %d \n", colstat[0], colstat[1], colstat[2]);
 #pragma omp target update from(colstat)
-  printf("colstat[0..2] %d %d %d \n", colstat[0], colstat[1], colstat[2]);
-  return 0;
+  fprintf(stderr, "AFTER colstat[0..2] %d %d %d \n", colstat[0], colstat[1], colstat[2]);
+  return 0;  // RON FIXME:
 }
 
