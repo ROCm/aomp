52c8f5a75fc9fa913b206cd77e95efd03dc6b8cb
Ethan Stewart <ethan.stewart@amd.com>
2020-05-26 10:53:05 -0500

    Add extern_init smoke test to known fails in check_smoke.sh

--- test/smoke/check_smoke.sh
+++ test/smoke/check_smoke.sh
@@ -36,7 +36,7 @@ echo "                   A non-zero exit code means a failure occured." >> check
 echo "Tests that need to be visually inspected: devices, pfspecify, pfspecify_str, stream" >> check-smoke.txt
 echo "***********************************************************************************" >> check-smoke.txt
 
-known_fails="reduction_array_section target_teams_reduction tasks map_zero_bug targ_static complex lto_teams"
+known_fails="reduction_array_section target_teams_reduction tasks map_zero_bug targ_static complex lto_teams extern_init"
 
 if [ "$SKIP_FAILURES" == 1 ] ; then
   skip_tests=$known_fails
