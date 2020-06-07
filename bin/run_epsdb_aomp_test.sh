98ed7178b2d0f289b5e44d30eb3d775bf4f03f3c
Ron Lieberman <ron.lieberman@amd.com>
2020-06-07 17:50:10 -0500

    Add some extra output to see clang --version, amdgcn/bitcode dir

--- bin/run_epsdb_aomp_test.sh
+++ bin/run_epsdb_aomp_test.sh
@@ -3,10 +3,15 @@
 #  run_epsdb_aomp_test.sh
 #
 
+set -x
 
 export AOMP_GPU=`$AOMP/bin/mygpu`
 echo AOMP_GPU = $AOMP_GPU
 export EXTRA_OMP_FLAGS=--rocm-path=$AOMP/
+$AOMP/bin/clang --version
+ls $AOMP/amdgcn/bitcode
+
+set +x
 
 cd ../test/smoke
 EPSDB=1 ./check_smoke.sh 
