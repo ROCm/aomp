9272e3e19b9b3dcfe481baad90cb67fbce17f5e4
Ethan Stewart <ethan.stewart@amd.com>
2020-07-06 11:50:18 -0500

    These changes are to allow for testing of the new features in 11.6-1 and 11.6-2 in ROCm builds. -Added known failure to skip in rocmaster_check_omp5.sh. -Removed 5.0 flag from omp5 tests now that we default to OpenMP 5.0. -Adjusted run_sollve.sh to support ROCMASTER variable that locks in sollve repo to a specific hash for testing consistency. -Adjusted check_sollve.sh to support both 4.5 and 5.0 verification.

--- bin/check_sollve.sh
+++ bin/check_sollve.sh
@@ -1,62 +1,64 @@
 #!/bin/bash
 #
 #  check_sollve.sh
+#  Assumes run_sollve.sh has been executed. To lock the sollve repo at a hash use ROCMASTER=1 ./run_sollve.sh
 #
 
-#Input file
-file="results.json"
-pushd $HOME/git/aomp-test/sollve_vv/results_report
+# Input file
+file="combined-results.txt"
+pushd $HOME/git/aomp-test/sollve_vv
 
-#Regex for results json
-testname_regex='Test name":."(.*)"'
-compile_regex='Compiler result": "(.*)"'
-runtime_regex='Runtime result": "(.*)"'
+# Regex for getting testname and compiler/runtime fail type
+testname_regex='(.*\.cpp|.*\.c|.*\.F90).*\(([a-z]*)\)'
 
-#skip known failures for now
-skip_tests='test_target_enter_data_classes_inheritance.cpp test_target_enter_exit_data_classes.cpp declare_target_module.F90 test_target_firstprivate.F90 test_target_private.F90 test_target_teams_distribute_firstprivate.F90 test_target_teams_distribute_reduction_min.F90 test_target_teams_distribute_default_firstprivate.F90'
+# Skip known failures for now
+skip_tests='declare_target_module.F90 test_target_enter_data_if.F90 test_target_firstprivate.F90 test_target_private.F90 test_target_teams_distribute_default_firstprivate.F90 test_target_teams_distribute_reduction_min.F90 test_declare_target_nested.c test_declare_variant.c test_requires_dynamic_allocators.c test_target_data_use_device_addr.c test_target_mapping_before_alloc.c test_target_mapping_before_delete.c test_target_teams_distribute_reduction_bitand.F90 test_target_teams_distribute_reduction_bitor.c test_target_teams_distribute_reduction_min.F90 test_target_teams_distribute_reduction_sub.F90 test_teams_distribute_default_none.c test_target_teams_distribute_defaultmap.F90'
 
 total_fails=0
 while read -r line; do
+  # If line in file matches a testname and fail type save the info
   if [[ "$line" =~ $testname_regex ]]; then
     sollve_test=${BASH_REMATCH[1]}
-  elif [[ "$line" =~ $compile_regex ]]; then
-    compile_result=${BASH_REMATCH[1]}
-  elif [[ "$line" =~ $runtime_regex ]]; then
-    runtime_result=${BASH_REMATCH[1]}
+    failure_type=${BASH_REMATCH[2]}
   fi
   if [[ $sollve_test != "" ]] ; then
      skip=1
-     if [[ $compile_result == "FAIL" ]] ; then
+     if [[ "$failure_type" == "compiler" ]] ; then
+      # If failing tests is not in skip_tests, add to compiler unexpected failures.
       if [[ ! $skip_tests =~ $sollve_test ]]; then
         compile_fails+="$sollve_test "
         skip=0
       fi
     fi
-    if [[ $runtime_result == "FAIL" ]] ; then
+    if [[ "$failure_type" == "runtime" ]] ; then
+      # If failing tests is not in skip_tests, add to runtime unexpected failures.
       if [[ ! $skip_tests =~ $sollve_test ]]; then
         runtime_fails+="$sollve_test "
         skip=0
       fi
     fi
-    if [[ $runtime_result == "FAIL" ]] || [[ $compile_result == "FAIL" ]] && [[ $skip == 0 ]]; then
+    if [[ "$failure_type" == "compiler" ]] || [[ $failure_type == "runtime" ]] && [[ $skip == 0 ]]; then
       ((total_fails+=1))
     fi
     sollve_test=""
-    compile_result=""
-    runtime_result=""
+    failure_type=""
   fi
 done < "$file"
 echo " "
-echo "Runtime Fails: "
-for fail in $runtime_fails; do
-  echo "$fail"
-done
+if [[ "$runtime_fails" != "" ]]; then
+  echo "Runtime Fails: "
+  for fail in $runtime_fails; do
+    echo "$fail"
+  done
+fi
 echo ""
 echo ""
-echo "Compile Fails: $compile_fails"
-for fail in $compile_fails; do
-  echo "$fail"
-done
-echo " "
+if [[ "$compile_fails" != "" ]]; then
+  echo "Compile Fails:"
+  for fail in $compile_fails; do
+    echo "$fail"
+  done
+  echo " "
+fi
 echo "Total Failures: $total_fails"
 popd
