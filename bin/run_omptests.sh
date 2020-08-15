7d5672c01e2e2b7a5559b40b03dec1c356512d0e
Ethan Stewart <ethan.stewart@amd.com>
2020-08-15 13:39:30 -0500

    Update run_omptests.sh to skip t-critical, which causes the entire suite to hang.

--- bin/run_omptests.sh
+++ bin/run_omptests.sh
@@ -36,6 +36,12 @@ DEVICE_TARGET=${DEVICE_TARGET:-amdgcn-amd-amdhsa}
 pushd $AOMP_REPOS_TEST/$AOMP_OMPTESTS_REPO_NAME
 rm -f runtime-fails.txt
 rm -f compile-fails.txt
+
+# Move t-critical to avoid soft hang
+if [ -d t-critical ]; then
+  mv t-critical test-critical-fails
+fi
+
 log=$(date --iso-8601=minutes).log
 
 env TARGET="-fopenmp-targets=$DEVICE_TARGET -fopenmp -Xopenmp-target=$DEVICE_TARGET -march=$DEVICE_ARCH" HOSTRTL=$AOMP/lib/libdevice TARGETRTL=$AOMP/lib GLOMPRTL=$AOMP/lib LLVMBIN=$AOMP/bin make -i 2>&1 | tee omptests_run_$log
@@ -44,6 +50,8 @@ env TARGET="-fopenmp-targets=$DEVICE_TARGET -fopenmp -Xopenmp-target=$DEVICE_TAR
 compile_fails=0
 runtime_fails=0
 total_tests=$(ls | grep "^t-*" | wc -l)
+# Increment total tests, t-critical is skipped
+((total_tests+=1))
 if [ -e compile-fails.txt ]; then
   echo
   echo -----Compile Fails-----
@@ -54,7 +62,10 @@ if [ -e runtime-fails.txt ]; then
   echo
   echo -----Runtime Fails-----
   cat runtime-fails.txt
+  echo t-critical - soft hang
   runtime_fails=$(wc -l < runtime-fails.txt)
+  # Increment fails, t-critical is skipped.
+  ((runtime_fails+=1))
 fi
 
 successful_tests=$(( total_tests - compile_fails - runtime_fails ))
@@ -81,6 +92,5 @@ echo
   echo
 } >> omptests_run_$log
 popd
-
 
 removepatch $AOMP_REPOS_TEST/$AOMP_OMPTESTS_REPO_NAME
