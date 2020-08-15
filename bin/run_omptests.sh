e55b6b55390e24e87de2817ff1d7f4dfe106a022
Ethan Stewart <ethan.stewart@amd.com>
2020-08-15 09:36:13 -0500

    Add run_omptest.sh and patch for Makefiles.

---
+++ bin/run_omptests.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+# --- Start standard header to set build environment variables ----
+function getdname(){
+   local __DIRN=`dirname "$1"`
+   if [ "$__DIRN" = "." ] ; then
+      __DIRN=$PWD;
+   else
+      if [ ${__DIRN:0:1} != "/" ] ; then
+         if [ ${__DIRN:0:2} == ".." ] ; then
+               __DIRN=`dirname $PWD`/${__DIRN:3}
+         else
+            if [ ${__DIRN:0:1} = "." ] ; then
+               __DIRN=$PWD/${__DIRN:2}
+            else
+               __DIRN=$PWD/$__DIRN
+            fi
+         fi
+      fi
+   fi
+   echo $__DIRN
+}
+thisdir=$(getdname $0)
+[ ! -L "$0" ] || thisdir=$(getdname `readlink "$0"`)
+. $thisdir/aomp_common_vars
+# --- end standard header ----
+
+patchrepo $AOMP_REPOS_TEST/$AOMP_OMPTESTS_REPO_NAME
+
+
+AOMP=${AOMP:-/usr/lib/aomp}
+AOMP_GPU=`$AOMP/bin/mygpu`
+DEVICE_ARCH=${DEVICE_ARCH:-$AOMP_GPU}
+DEVICE_TARGET=${DEVICE_TARGET:-amdgcn-amd-amdhsa}
+
+
+pushd $AOMP_REPOS_TEST/$AOMP_OMPTESTS_REPO_NAME
+rm -f runtime-fails.txt
+rm -f compile-fails.txt
+log=$(date --iso-8601=minutes).log
+
+env TARGET="-fopenmp-targets=$DEVICE_TARGET -fopenmp -Xopenmp-target=$DEVICE_TARGET -march=$DEVICE_ARCH" HOSTRTL=$AOMP/lib/libdevice TARGETRTL=$AOMP/lib GLOMPRTL=$AOMP/lib LLVMBIN=$AOMP/bin make -i 2>&1 | tee omptests_run_$log
+
+# Get Results
+compile_fails=0
+runtime_fails=0
+total_tests=$(ls | grep "^t-*" | wc -l)
+if [ -e compile-fails.txt ]; then
+  echo
+  echo -----Compile Fails-----
+  cat compile-fails.txt
+  compile_fails=$(wc -l < compile-fails.txt)
+fi
+if [ -e runtime-fails.txt ]; then
+  echo
+  echo -----Runtime Fails-----
+  cat runtime-fails.txt
+  runtime_fails=$(wc -l < runtime-fails.txt)
+fi
+
+successful_tests=$(( total_tests - compile_fails - runtime_fails ))
+pass_rate=`bc -l <<< "scale=4; ($successful_tests/$total_tests) * 100" | sed "s/0//g"`
+echo
+echo ----- Results -----
+echo Compile Fails: $compile_fails
+echo Runtime Fails: $runtime_fails
+
+echo Successful Tests: $successful_tests/$total_tests
+echo Pass Rate: $pass_rate%
+echo -------------------
+echo
+# Log Results
+{
+  echo
+  echo ----- Results -----
+  echo Compile Fails: $compile_fails
+  echo Runtime Fails: $runtime_fails
+
+  echo Successful Tests: $successful_tests/$total_tests
+  echo Pass Rate: $pass_rate%
+  echo -------------------
+  echo
+} >> omptests_run_$log
+popd
+
+
+removepatch $AOMP_REPOS_TEST/$AOMP_OMPTESTS_REPO_NAME
