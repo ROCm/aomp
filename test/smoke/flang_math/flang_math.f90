11bf86ec19cec7509f4bfde0d6551ecf08aed9be
Ron Lieberman <ron.lieberman@amd.com>
2020-09-29 19:53:07 -0500

    add more intrinsic refs

--- test/smoke/flang_math/flang_math.f90
+++ test/smoke/flang_math/flang_math.f90
@@ -1,11 +1,14 @@
 subroutine MATH(a,b)
         real*8 :: a,b
+        print *, cos(b) + sin(b) + sqrt(b) + exp(b) + a**b
    !$omp target map(tofrom:a)
-        a = cos(b)
+        a = cos(b) + sin(b) + sqrt(b) + exp(b) + a**b
    !$omp end target 
         end subroutine
 program main
-   REAL*8 :: x
-   call MATH(x,12.0)
+   REAL*8 :: x,y
+   x = 2.0
+   y = 12.0
+   call MATH(x,y)
    print *,x
 end program
