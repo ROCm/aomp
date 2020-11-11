3d1b4bfec2066e04709b55b6019842b52aa81e06
Gregory Rodgers <Gregory.Rodgers@amd.com>
2020-11-11 17:26:16 +0000

    HIP build fails when an old version of /opt/rocm/include exists.  This gets rid of the hardcoding of /opt/rocm/include and turns off building of hip precompiled headers for aomp

--- bin/build_hipvdi.sh
+++ bin/build_hipvdi.sh
@@ -110,6 +110,7 @@ if [ "$1" != "nocmake" ] && [ "$1" != "install" ] ; then
  -DHSA_PATH=$ROCM_DIR \
  -DCMAKE_MODULE_PATH=$ROCM_DIR/cmake \
  -DROCclr_DIR=$ROCclr_ROOT \
+ -D__HIP_ENABLE_PCH=OFF \
  -DLIBROCclr_STATIC_DIR=$ROCclr_BUILD_DIR \
  -DCMAKE_PREFIX_PATH=$ROCclr_BUILD_DIR;$ROCM_DIR/include;$ROCM_DIR;$ROCM_DIR/lib \
  -DCMAKE_CXX_FLAGS=-Wno-ignored-attributes "
