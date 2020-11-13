a67c7154dc88651a5e6861fca3bfa871aac89ee7
Ethan Stewart <ethan.stewart@amd.com>
2020-11-13 10:31:09 -0600

    [build-scripts] - Clean up build_fixups.sh and build_project.sh
    build_project: Install llvm-lit in all builds for testing purposes. Before building release package build_fixups.sh
    will remove llvm-lit as it references source files not shipped with the release.
    
    build_fixups: Remove references to hcc and copy of hipcc for non-standalone builds. Add removal of rocclr dir and llvm-lit.

--- bin/build_project.sh
+++ bin/build_project.sh
@@ -194,14 +194,13 @@ if [ "$1" == "install" ] ; then
       fi
       $SUDO ln -sf $INSTALL_PROJECT $AOMP   
    fi
+
    # add executables forgot by make install but needed for testing
-   # Only do this if this is not a production build
-   if [ $AOMP != "/usr/lib/aomp" ] || [ $AOMP != "/opt/rocm" ] || [ $AOMP != "/opt/rocm/aomp" ] ; then
-      $SUDO cp -p $BUILD_DIR/build/$AOMP_PROJECT_REPO_NAME/bin/llvm-lit $AOMP/bin/llvm-lit
-      # update map_config and llvm_source_root paths in the copied llvm-lit file
-      SED_AOMP_REPOS=`echo $AOMP_REPOS | sed -e 's/\//\\\\\//g' `
-      sed -ie "s/..\/..\/..\//$SED_AOMP_REPOS\//g" $AOMP/bin/llvm-lit
-   fi
+   $SUDO cp -p $BUILD_DIR/build/$AOMP_PROJECT_REPO_NAME/bin/llvm-lit $AOMP/bin/llvm-lit
+   # update map_config and llvm_source_root paths in the copied llvm-lit file
+   SED_AOMP_REPOS=`echo $AOMP_REPOS | sed -e 's/\//\\\\\//g' `
+   sed -ie "s/..\/..\/..\//$SED_AOMP_REPOS\//g" $AOMP/bin/llvm-lit
+
    $SUDO cp -p $BUILD_DIR/build/$AOMP_PROJECT_REPO_NAME/bin/FileCheck $AOMP/bin/FileCheck
    $SUDO cp -p $BUILD_DIR/build/$AOMP_PROJECT_REPO_NAME/bin/count $AOMP/bin/count
    $SUDO cp -p $BUILD_DIR/build/$AOMP_PROJECT_REPO_NAME/bin/not $AOMP/bin/not
