92c32077cbb3dc1c2c2c3bcf078fdbbda3a3a786
gregrodgers <Gregory.Rodgers@amd.com>
2020-11-19 09:15:44 -0600

    make alignedattribute test case simpler to use a single array and a single pragma with printf inside target region

--- test/smoke/alignedattribute/alignedattribute.c
+++ test/smoke/alignedattribute/alignedattribute.c
@@ -1,37 +1,32 @@
 #include <stdio.h>
-#include "assert.h"
-#include <unistd.h>
 
-void vmul(int*a, int*b, int*c, int N){
-  int*aptr __attribute__ ((aligned(64))) = a;
-  //  int*aptr = a;  THIS WORKS.  THE USE of aligned attribute fails
-#pragma omp target map(to: aptr[0:N],b[0:N]) map(from:c[0:N*N])
-#pragma omp teams distribute parallel for collapse(2)
-   for(int i=0;i<N;i++)
-     for(int j=0;j<N;j++){
-      c[i*N+j]=aptr[i]*b[j];
+void write_index(int*a, int N){
+   int*aptr __attribute__ ((aligned(64))) = a; // THIS FAILS
+   // int*aptr = a; // THIS WORKS
+   printf(" ===> Encounter target teams distribute par for map tofrom:aptr\n");
+#pragma omp target teams distribute parallel for map(tofrom: aptr[0:N])
+   for(int i=0;i<N;i++) {
+      printf("updating aptr[%d] addr:%p\n",i,&aptr[i]);
+      aptr[i]=i;
    }
 }
 
 int main(){
-    const int N = 1000;
-    int a[N],b[N],c[N*N],validate[N*N];
-    int flag=-1; // Mark Success
-    for(int i=0;i<N;i++){
-      a[i]=i+1;
-      for(int j=0;j<N;j++){
-        b[j]=j+2;
-        validate[i*N+j]=a[i]*b[j];
-      }
+    const int N = 10;    
+    int a[N],validate[N];
+    for(int i=0;i<N;i++) {
+        a[i]=0;
+        validate[i]=i;
     }
 
-    vmul(a,b,c,N);
+    write_index(a,N);
 
-    for(int i=0;i<N*N;i++) {
-        if(c[i]!=validate[i]) {
+    int flag=-1; // Mark Success
+    for(int i=0;i<N;i++) {
+        if(a[i]!=validate[i]) {
 //          print 1st bad index
             if( flag == -1 ) 
-              printf("First fail: c[%d](%d) != validate[%d](%d)\n",i,c[i],i,validate[i]);
+              printf("First fail: a[%d](%d) != validate[%d](%d)\n",i,a[i],i,validate[i]);
             flag = i;
         }
     }
@@ -39,7 +34,7 @@ int main(){
         printf("Success\n");
         return 0;
     } else {
-        printf("Last fail: c[%d](%d) != validate[%d](%d)\n",flag,c[flag],flag,validate[flag]);
+        printf("Last fail: a[%d](%d) != validate[%d](%d)\n",flag,a[flag],flag,validate[flag]);
         printf("Fail\n");
         return 1;
     }
