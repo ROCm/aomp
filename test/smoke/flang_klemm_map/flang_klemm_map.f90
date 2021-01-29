eae6dc4f3c406d3618b4648b97fae6ba633b7ab6
Ron Lieberman <ron.lieberman@amd.com>
2021-01-29 12:41:48 -0600

    Add test flang_klemm_map for issue 185

---
+++ test/smoke/flang_klemm_map/flang_klemm_map.f90
@@ -0,0 +1,50 @@
+! compile with
+! flang -fopenmp -fopenmp-targets=amdgcn-amd-amdhsa -Xopenmp-target=amdgcn-amd-amdhsa -march=gfx908 -o map map.F90
+!
+! Expected output
+! 0.000000000000000         0.000000000000000         0.000000000000000
+! 0.000000000000000         0.000000000000000         0.000000000000000
+! 0.000000000000000         0.000000000000000         0.000000000000000
+! 0.000000000000000
+
+! 42.00000000000000         42.00000000000000         42.00000000000000
+! 42.00000000000000         42.00000000000000         42.00000000000000
+! 42.00000000000000         42.00000000000000         42.00000000000000
+! 42.00000000000000
+
+subroutine proc(arr, n)
+    use iso_fortran_env
+
+    implicit none
+
+    integer                         :: n
+    real(kind=real64), dimension(*) :: arr
+
+    integer :: i
+
+! $omp target map(tofrom:arr(1:n))
+    do i = 1,n
+        arr(i) = 42.0
+    end do
+! $omp end target
+
+end subroutine
+
+program map
+    use iso_fortran_env
+
+    implicit none
+
+    integer, parameter :: N = 1000
+    integer            :: i
+
+    real(kind=real64), dimension(N) :: array
+
+    array(:) = 0.0
+
+    write (*,*) array(1:N/100)
+    write (*,*)
+    call proc(array, N)
+    write (*,*) array(1:N/100)
+
+end program
