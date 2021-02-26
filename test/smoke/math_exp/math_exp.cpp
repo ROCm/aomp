64e8183311382163dd246822c293341e5df424d3
gregrodgers <Gregory.Rodgers@amd.com>
2021-02-26 10:28:25 -0600

    show comparison of host and device calls to complex exp.  This is only change in the output

--- test/smoke/math_exp/math_exp.cpp
+++ test/smoke/math_exp/math_exp.cpp
@@ -12,7 +12,8 @@ int main(int argc, char **argv)
   {
     res = exp(inp);
   }
-  cout << res << "\n";
+  std::complex<float> host_res = exp(inp);
+  cout << "device:" << res << "  host:" << host_res << "\n";
 
   std::complex<double> dinp(11.0,1.0);
   std::complex<double> dres;
@@ -20,6 +21,7 @@ int main(int argc, char **argv)
   {
     dres = exp(dinp);
   }
-  cout << dres << "\n";
+  std::complex<double> host_dres = exp(dinp);
+  cout << "device:" << dres << "  host:" << host_dres << "\n";
   return 0;
 }
