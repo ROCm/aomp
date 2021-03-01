eb24ffc12257dd6255923b1642041a9cae2b0879
Ron Lieberman <Ron.Lieberman@amd.com>
2021-02-28 22:10:17 -0500

    Add flang-274773

---
+++ test/smoke/flang-274773/flang-274773.f90
@@ -0,0 +1,29 @@
+program test
+      integer :: i,j
+      complex(8) :: M,N
+      complex(8),pointer :: A(:)
+
+      allocate(A(10))
+      M=(0,0)
+
+      do i=1, 10
+        A(i)=(1,1)
+      enddo
+
+!$omp target  parallel do map(to:A) map(tofrom:M) reduction(+:M)
+!!$omp target  parallel do map(to:A) map(tofrom:M) private(N) reduction(+:M)
+      do i=1, 10
+        N=(0,0)
+        do j=1, 10
+          N=N+A(j)
+        enddo
+        M=M+N
+      enddo
+!$omp end target parallel do
+
+      write(*, *), "M=", M
+
+end program test
+ 
+
+
