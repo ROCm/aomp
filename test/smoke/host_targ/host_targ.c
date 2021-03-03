cf06031fca4b7f47ca64d41d3db08926f704181b
Ron Lieberman <ron.lieberman@amd.com>
2021-03-03 10:49:14 -0600

    Add device count print from target as host

--- test/smoke/host_targ/host_targ.c
+++ test/smoke/host_targ/host_targ.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
+#include <omp.h>
 
 int arr[100];
 int nt =12;
 int main()
 {
+  fprintf(stderr, "Omp host get_num_devices %d\n", omp_get_num_devices());
 #pragma omp target teams distribute parallel for num_threads(nt)
   for (int i=0; i<100;i++)
     arr[i] =i;
