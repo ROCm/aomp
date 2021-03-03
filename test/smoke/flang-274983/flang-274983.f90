cdb61ddb57e22258f2f4cf1e55d3f50b0c797baa
Ron Lieberman <ron.lieberman@amd.com>
2021-03-02 18:09:56 -0600

    add test flang-274983

---
+++ test/smoke/flang-274983/flang-274983.f90
@@ -0,0 +1,31 @@
+program test
+    use omp_lib
+    use f90deviceio
+      integer :: i
+      real, pointer  :: res, x(:)
+
+      allocate(x(128))
+
+      do i=1, 128
+         x(i)=1
+      enddo
+
+      !$omp target enter data  map(to:x)
+
+      do i=1, 128
+         x(i)=2
+      enddo
+
+      !$omp target
+        if (omp_get_thread_num() .eq. 0) then
+          call f90printf("In tagret data=", x(1))
+        endif
+      !$omp end target
+
+      write(*, *) "Outside tagret data=", x(1)
+
+      !$omp target exit data map(release:x)
+
+      nullify(x)
+
+end program test
