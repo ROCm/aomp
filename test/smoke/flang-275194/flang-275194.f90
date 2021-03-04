f54cac5dd0e326924d28bc1c87d5778a5734158a
Ron Lieberman <ron.lieberman@amd.com>
2021-03-04 17:39:52 -0600

    add flang-kerneltrace example

--- test/smoke/flang-275194/flang-275194.f90
+++ test/smoke/flang-275194/flang-275194.f90
@@ -2,12 +2,15 @@ program test
       integer :: i,k
       real,pointer :: x(:)
       allocate(x(10))
-      k=0
+      k=2
 
-      !$omp target parallel do if(k)
+     !$omp target if(k > 0) map(from:x)
+      k=2
+      !$omp parallel do
       do i=1, 10
          x(i)=1
       enddo
-      !$omp end target parallel do
+      !$omp end parallel do
+     !$omp end target
 
 end program test
