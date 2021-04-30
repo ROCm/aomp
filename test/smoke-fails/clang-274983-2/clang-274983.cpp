95878c272459ec5f78ce2017adc1fd3b60483906
Ron Lieberman <ron.lieberman@amd.com>
2021-04-30 13:42:36 -0500

    Add clang-274983-2

---
+++ test/smoke-fails/clang-274983-2/clang-274983.cpp
@@ -0,0 +1,15 @@
+#include <omp.h>
+#include <stdio.h>
+int main(){
+    omp_set_default_device(0);
+    int N=10;
+    double *x = (double*) omp_target_alloc(N*sizeof(double),omp_get_default_device());
+    for (int i = 0; i < N; ++i) x[i] = 2.0;
+    fprintf(stderr, "before target data: x[1] 0x%p = %g\n",&x[1], x[1]);
+    #pragma omp target data use_device_ptr(x)
+    {
+      fprintf(stderr, "in target data: x[1] 0x%p = %g\n",&x[1], x[1]);
+    }
+
+  return 0;
+}
