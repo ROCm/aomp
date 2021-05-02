f72d2f86eb1acbf632c4417ee70cda06d9b09d36
Ron Lieberman <ron.lieberman@amd.com>
2021-05-01 22:11:01 -0500

    remove race condition from 274775

--- test/smoke/flang-274775/flang_274775.f90
+++ test/smoke/flang-274775/flang_274775.f90
@@ -10,15 +10,13 @@ program test
           A(i)=(1,1)
         enddo
  
-!$omp target teams distribute map(to:A) map(tofrom:M) reduction(+:M)
+!$omp target teams distribute map(to:A) map(tofrom:M) reduction(+:M) 
        do i=1, 10
-       N=(0,0)
-!$omp parallel do reduction(+:N)
+!$omp parallel do
          do j=1, 10
-           N=N+A(j)
+           M=M+A(j)
          enddo
 !$omp end parallel do
-         M=M+N
       enddo
 !$omp end target teams distribute
 
