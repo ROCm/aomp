baaa9a2ce70a8173535a00c1bc3e9f5c2c7791a0
Ethan Stewart <ethan.stewart@amd.com>
2021-05-14 16:23:39 -0500

    Add run_rocm_test.sh for QA validation.
        Adds new rocm-test dir that holds expected passes for various rocm releases.
        Reworks check_examples.sh, gets rid of all the check scripts for various examples.
        Reworks check_sollve.sh to report passing, failing tests similar to check_smoke.sh.
        ROCm QA testing will now be based on expected passes to allow the use of aomp-dev
          branch to get visibility on the most recent tests, while not causing known fails
          to create blockage.

--- bin/check_sollve.sh
+++ bin/check_sollve.sh
@@ -1,73 +1,72 @@
 #!/bin/bash
 #
 #  check_sollve.sh
-#  Assumes run_sollve.sh has been executed. To lock the sollve repo at a hash use ROCMASTER=1 ./run_sollve.sh
+#  Assumes run_sollve.sh has been executed.
 #
-
-# Input file
-file="combined-results.txt"
+set -e
 pushd $HOME/git/aomp-test/sollve_vv
-
-# Regex for getting testname and compiler/runtime fail type
-testname_regex='(.*\.cpp|.*\.c|.*\.F90).*\(([a-z]*)\)'
-
-# Skip known failures for now
-
-skip_tests='test_target_data_map_to_array_sections.F90 declare_target_module.F90 test_target_enter_data_if.F90 test_target_firstprivate.F90 test_target_private.F90 test_target_teams_distribute_default_firstprivate.F90 test_target_teams_distribute_reduction_min.F90 test_declare_target_nested.c test_declare_variant.c test_requires_dynamic_allocators.c test_target_data_use_device_addr.c test_target_mapping_before_alloc.c test_target_mapping_before_delete.c test_target_teams_distribute_reduction_bitand.F90 test_target_teams_distribute_reduction_bitor.c test_target_teams_distribute_reduction_min.F90 test_target_teams_distribute_reduction_sub.F90 test_teams_distribute_default_none.c test_target_teams_distribute_defaultmap.F90 test_target_data_map_to_array_sections.F90'
-
-
-if [ "$EPSDB" == "1" ]; then
-  skip_tests+=" test_target_depends.c test_target_enter_data_depend.c test_target_enter_exit_data_depend.c test_target_teams_distribute_depend_array_section.c test_target_teams_distribute_depend_disjoint_section.c test_target_teams_distribute_depend_in_in.c test_target_teams_distribute_depend_in_out.c test_target_teams_distribute_depend_list.c test_target_teams_distribute_depend_out_in.c test_target_teams_distribute_depend_out_out.c test_target_teams_distribute_depend_unused_data.c test_target_update_depend.c"
-fi
-
-# Additional failures seen with updated sollve repo as of 9/10/20
-skip_tests+=" test_target_update_mapper_to_discontiguous.c test_declare_mapper_target_struct.c test_requires_unified_shared_memory.c test_requires_unified_shared_memory_heap.c test_requires_unified_shared_memory_heap_is_device_ptr.c test_requires_unified_shared_memory_heap_map.c test_requires_unified_shared_memory_omp_target_alloc.c test_requires_unified_shared_memory_omp_target_alloc_is_device_ptr.c test_requires_unified_shared_memory_stack.c test_requires_unified_shared_memory_stack_is_device_ptr.c test_requires_unified_shared_memory_stack_map.c test_requires_unified_shared_memory_static.c test_requires_unified_shared_memory_static_is_device_ptr.c test_requires_unified_shared_memory_static_map.c"
-
-total_fails=0
-while read -r line; do
-  # If line in file matches a testname and fail type save the info
-  if [[ "$line" =~ $testname_regex ]]; then
-    sollve_test=${BASH_REMATCH[1]}
-    failure_type=${BASH_REMATCH[2]}
-  fi
-  if [[ $sollve_test != "" ]] ; then
-     skip=1
-     if [[ "$failure_type" == "compiler" ]] ; then
-      # If failing tests is not in skip_tests, add to compiler unexpected failures.
-      if [[ ! $skip_tests =~ $sollve_test ]]; then
-        compile_fails+="$sollve_test "
-        skip=0
-      fi
+testname_regex='Test\sname":\s"(.*)"'
+compiler_regex='Compiler\sresult":\s"([A-Z]+)'
+runtime_regex='Runtime\sresult":\s"([A-Z]+)'
+function parse(){
+  local -n myarray=$1
+  local file=$2
+  while read -r line; do
+    local sollvetest=""
+    if [[ "$line" =~ $testname_regex ]]; then
+      sollvetest=${BASH_REMATCH[1]}
+    elif [[ "$line" =~ $compiler_regex ]]; then
+      compresult=${BASH_REMATCH[1]}
+    elif [[ "$line" =~ $runtime_regex ]]; then
+      runresult=${BASH_REMATCH[1]}
     fi
-    if [[ "$failure_type" == "runtime" ]] ; then
-      # If failing tests is not in skip_tests, add to runtime unexpected failures.
-      if [[ ! $skip_tests =~ $sollve_test ]]; then
-        runtime_fails+="$sollve_test "
-        skip=0
-      fi
+    if [ "$sollvetest" != "" ];then
+      myarray[$sollvetest]="$compresult"
+      myarray[$sollvetest]+="$runresult"
+      compresult=""
+      runresult=""
     fi
-    if [[ "$failure_type" == "compiler" ]] || [[ $failure_type == "runtime" ]] && [[ $skip == 0 ]]; then
-      ((total_fails+=1))
+  done < "$file"
+}
+
+function checkresult(){
+  tmppassfile="results_report$2/tmppassing-tests.txt"
+  tmpcompfailfile="results_report$2/tmpmake-fail.txt"
+  tmprunfailfile="results_report$2/tmpfailing-tests.txt"
+
+  passfile="results_report$2/passing-tests.txt"
+  compfailfile="results_report$2/make-fail.txt"
+  runfailfile="results_report$2/failing-tests.txt"
+
+  rm -f $tmppassfile $tmpcompfailfile $tmprunfailfile
+  local -n myarray=$1
+  for i in "${!myarray[@]}"; do
+    local val=${myarray[$i]}
+    if [ "$val" == "PASSPASS" ]; then
+      echo $i >> $tmppassfile
+    elif [ "$val" == "FAIL" ]; then
+      echo $i >> $tmpcompfailfile
+    elif [ "$val" == "PASSFAIL" ]; then
+      echo $i >> $tmprunfailfile
     fi
-    sollve_test=""
-    failure_type=""
-  fi
-done < "$file"
-echo " "
-if [[ "$runtime_fails" != "" ]]; then
-  echo "Runtime Fails: "
-  for fail in $runtime_fails; do
-    echo "$fail"
   done
-fi
-echo ""
-echo ""
-if [[ "$compile_fails" != "" ]]; then
-  echo "Compile Fails:"
-  for fail in $compile_fails; do
-    echo "$fail"
-  done
-  echo " "
-fi
-echo "Total Failures: $total_fails"
+  if [ -e "$tmppassfile" ]; then
+    sort $tmppassfile > results_report$2/passing-tests.txt
+  fi
+  if [ -e "$tmpcompfailfile" ]; then
+    sort $tmpcompfailfile > results_report$2/make-fail.txt
+  fi
+  if [ -e "$tmprunfailfile" ]; then
+    sort $tmprunfailfile > results_report$2/failing-tests.txt
+  fi
+  rm -f $tmppassfile $tmpcompfailfile $tmprunfailfile
+}
+vers="45 50"
+for ver in $vers; do
+  declare -A results$ver
+  parse results$ver "results_report$ver/results.json"
+  checkresult results$ver $ver
+done
+
+exit
 popd
