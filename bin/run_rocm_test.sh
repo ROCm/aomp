781942ee0e8ffebb36055ded34c9c94bc95c40ae
estewart08 <ethan.stewart@amd.com>
2021-05-20 10:47:40 -0500

    Rocm qa test update (#212)
    * Add run_rocm_test.sh for QA validation.
    
        Adds new rocm-test dir that holds expected passes for various rocm releases.
        Reworks check_examples.sh, gets rid of all the check scripts for various examples.
        Reworks check_sollve.sh to report passing, failing tests similar to check_smoke.sh.
        ROCm QA testing will now be based on expected passes to allow the use of aomp-dev
          branch to get visibility on the most recent tests, while not causing known fails
          to create blockage.
    
    * Add rocm testing updates for examples.
    
    * Print unexpected passes/fails at the end to show a concise summary.
    
    * Remove test_target_depends.F90 from solve45_passes.
    
    Test fails on multi gpu machines.
    Correct printing of concise summary in run_rocm_test.sh
    
    * Add smoke-fails, 4.3 versioning to run_rocm_test.sh
    
    * Add bogus path for RAE to avoid user set arch file.
    
    * Update run_rocm_test.sh
    
    Add Error checking for clang binary and AOMP_GPU.
    If the rocm symlink is not found, the script sets the newest
    installation of rocm (time-wise) instead of just the last X.Y.Z detected.
    
    * Update run_rocm_test.sh
    
    4.3 baseline for expected passes.
    
    * Update run_rocm_test.sh
    
    Remove unwanted echos.
    Clean up regular expressions.
    Switch to 3 number rocm versions.
    
    * Add copy of fortran/openmp examples from release.
    
    * Rename passes/4.3 to passes/4.3.0
    
    * check_examples.sh - add ability to pass user specified examples to run.

---
+++ bin/run_rocm_test.sh
@@ -0,0 +1,268 @@
+#!/bin/bash
+#
+#  run_rocm_test.sh
+#
+
+scriptdir=$(dirname "$0")
+parentdir=`eval "cd $scriptdir;pwd;cd - > /dev/null"`
+aompdir="$(dirname "$parentdir")"
+resultsdir="$aompdir/bin/rocm-test/results"
+rocmtestdir="$aompdir"/bin/rocm-test
+summary="$resultsdir"/summary.txt
+unexpresults="$resultsdir"/unexpresults.txt
+scriptfails=0
+
+# Set AOMP to point to rocm symlink or newest version.
+if [ -L /opt/rocm ]; then
+  AOMP=${AOMP:-"/opt/rocm/llvm"}
+else
+  newestrocm=$(ls --sort=time /opt | grep -m 1 rocm)
+  AOMP=${AOMP:-"/opt/$newestrocm/llvm"}
+fi
+export AOMP
+echo "AOMP = $AOMP"
+
+# Make sure clang is present.
+$AOMP/bin/clang --version
+if [ $? -ne 0 ]; then
+  echo "Error: Clang not found at "$AOMP"/bin/clang."
+  exit 1
+fi
+
+# Parent dir should be ROCm base dir.
+AOMPROCM=$AOMP/..
+
+# Use bogus path to avoid using target.lst, a user-defined target list
+# used by rocm_agent_enumerator.
+export ROCM_TARGET_LST=/opt/nowhere
+
+echo "RAE devices:"
+$AOMPROCM/bin/rocm_agent_enumerator
+
+# Set AOMP_GPU.
+# Regex skips first result 'gfx000' and selects second id.
+AOMP_GPU=$($AOMPROCM/bin/rocm_agent_enumerator | grep -m 1 -E gfx[^0]{1}.{2})
+# mygpu will eventually relocate to /opt/rocm/bin, support both cases for now.
+if [ "$AOMP_GPU" != "" ]; then
+  echo "AOMP_GPU set with rocm_agent_enumerator."
+else
+  echo "AOMP_GPU is empty, use mygpu."
+  if [ -a $AOMP/bin/mygpu ]; then
+    AOMP_GPU=$($AOMP/bin/mygpu)
+  else
+    AOMP_GPU=$($AOMP/../bin/mygpu)
+  fi
+fi
+if [ "$AOMP_GPU" == "" ]; then
+  echo "Error: AOMP_GPU was not able to be set with RAE or mygpu."
+  exit 1
+fi
+export AOMP_GPU
+
+# Determines ROCm version and openmp-extras version to choose which set
+# of expected passes to use. Example ROCm 4.3 build installed but
+# openmp-extras version is 4.2. This can happen in mainline builds. Do not expect
+# this version mismatch on release testing. We will choose the lower version so that
+# unsupported tests are not included.
+function getversion(){
+  supportedvers="4.3.0 4.4.0"
+  declare -A versions
+  versions[430]=4.3.0
+  versions[440]=4.4.0
+
+  # Determine ROCm version.
+  rocm=$(cat "$AOMPROCM"/.info/version-dev)
+  rocmregex="([0-9]+\.[0-9]+\.[0-9]+)"
+  if [[ "$rocm" =~ $rocmregex ]]; then
+    rocmver=$(echo ${BASH_REMATCH[1]} | sed "s/\.//g")
+    echo rocmver: $rocmver
+  else
+    echo Unable to determine rocm version.
+    exit 1
+  fi
+
+  # Determine OS flavor to properly query openmp-extras version.
+  osname=$(cat /etc/os-release | grep -e ^NAME=)
+  ompextrasregex="\s[0-9]+\.([0-9]+)\.([0-9]+)"
+  echo $osname
+  if [[ $osname =~ "Red Hat|CentOS|SLES" ]]; then
+    echo "Red Hat/CentOS/SLES found"
+    ompextraspkg=$(rpm -qa | grep openmp-extras)
+  elif [[ $osname =~ "Ubuntu" ]]; then
+    echo "Ubuntu found"
+    ompextraspkg=$(dpkg --list | grep openmp-extras)
+  fi
+  if [[ "$ompextraspkg" =~ $ompextrasregex ]]; then
+    ompextrasver=${BASH_REMATCH[1]}${BASH_REMATCH[2]}
+    echo ompextrasver: $ompextrasver
+  else
+    echo Unable to determine openmp-extras package version.
+    exit 1
+  fi
+
+  # Set the final version to use for expected passing lists. The expected passes
+  # will include an aggregation of suported versions up to and including the chosen
+  # version.  Example: If 4.4 is selected then the final list will include expected passes
+  # from 4.3 and 4.4. Openmp-extras should not be a higher version than rocm.
+  if [ "$rocmver" == "$ompextrasver" ] || [ "$rocmver" > "$ompextrasver" ]; then
+    compilerver=${versions[$ompextrasver]}
+  else
+    compilerver=${versions[$rocmver]}
+  fi
+  echo Chosen Version: $compilerver
+  versionregex="(.*$compilerver)"
+  if [[ "$supportedvers" =~ $versionregex ]]; then
+    finalvers=${BASH_REMATCH[1]}
+  else
+    echo "Unsupported compiler build."
+    exit 1
+  fi
+}
+
+function copyresults(){
+  # $1 name of test suite
+
+  # Copy logs from suite to results folder
+  if [ -e failing-tests.txt ]; then
+    cp failing-tests.txt "$resultsdir/$1"/"$1"_failing_tests.txt
+  fi
+  if [ -e make-fail.txt ]; then
+    cp make-fail.txt "$resultsdir/$1"/"$1"_make_fail.txt
+  fi
+  if [ -e passing-tests.txt ]; then
+    echo""
+    cp passing-tests.txt "$resultsdir/$1"/"$1"_passing_tests.txt
+  fi
+
+
+  # Begin logging info in summary.txt.
+  cd $resultsdir/$1
+  echo ===== $1 ===== | tee -a $summary $unexpresults
+  if [ -e "$1"_passing_tests.txt ]; then
+    # Sort test reported passes
+    sort -f -d "$1"_passing_tests.txt > "$1"_sorted_passes
+
+    # Sort expected passes
+    for ver in $finalvers; do
+      if [ -e "$rocmtestdir"/passes/$ver/$1/"$1"_passes.txt ]; then
+        cat "$rocmtestdir"/passes/$ver/$1/"$1"_passes.txt >> "$1"_combined_exp_passes
+      fi
+    done
+    sort -f -d "$1"_combined_exp_passes > "$1"_sorted_exp_passes
+
+    # Unexpected passes
+    unexpectedpasses=$(diff "$1"_sorted_exp_passes "$1"_sorted_passes | grep '>' | wc -l)
+    echo Unexpected Passes: $unexpectedpasses | tee -a $summary $unexpresults
+    diff "$1"_sorted_exp_passes "$1"_sorted_passes | grep '>' | sed 's/> //' >> $summary
+    echo >> $summary
+
+    # Unexpected Fails
+    unexpectedfails=$(diff "$1"_sorted_exp_passes "$1"_sorted_passes | grep '<' | wc -l)
+    ((totalunexpectedfails+=$unexpectedfails))
+    echo "Unexpected Fails: $unexpectedfails" | tee -a $summary $unexpresults
+    diff "$1"_sorted_exp_passes "$1"_sorted_passes | grep '<' | sed 's/< //' >> $summary
+    echo >> $summary
+
+    # Failing Tests
+    if [ -e "$1"_failing_tests.txt ]; then
+      echo Runtime Fails: >> $summary
+      cat "$1"_failing_tests.txt >> $summary
+      echo >> $summary
+    fi
+    if [ -e "$1"_make_fail.txt ]; then
+      echo Compile Fails: >> $summary
+      cat "$1"_make_fail.txt >> $summary
+      echo >> $summary
+    fi
+
+  else
+    # No passing-tests.txt found, count expected passes as fails.
+    # Does not count combined expected passes from previous rocm versions
+    numtests=$(cat "$rocmtestdir"/passes/"$compilerver"/"$1"/"$1"_passes.txt | wc -l)
+    ((totalunexpectedfails+=$numtests))
+  fi
+}
+
+function checkrc(){
+  if [ "$1" != 0 ]; then
+    ((scriptfails++))
+  fi
+}
+
+
+# Get version of installed ROCm and compare against openmp-extras version.
+getversion
+echo "Included Versions: $finalvers"
+
+cd "$aompdir"/bin
+rm -rf $resultsdir
+mkdir -p $resultsdir
+
+# Fortran Examples
+mkdir -p "$resultsdir"/fortran
+echo "cp -rf "$AOMP"/examples/fortran "$aompdir"/examples"
+cp -rf "$AOMP"/examples/fortran "$aompdir"/examples
+cd "$aompdir"/examples/fortran
+EPSDB=1 ../check_examples.sh fortran
+checkrc $?
+copyresults fortran
+
+# Openmp Examples
+mkdir -p "$resultsdir"/openmp
+echo "cp -rf "$AOMP"/examples/openmp "$aompdir"/examples"
+cp -rf "$AOMP"/examples/openmp "$aompdir"/examples
+cd "$aompdir"/examples/openmp
+EPSDB=1 ../check_examples.sh openmp
+checkrc $?
+copyresults openmp
+
+# Smoke
+mkdir -p "$resultsdir"/smoke
+cd "$aompdir"/test/smoke
+CLEANUP=0 AOMPHIP=$AOMPROCM ./check_smoke.sh
+checkrc $?
+copyresults smoke
+
+# Smoke-fails
+mkdir -p "$resultsdir"/smoke-fails
+cd "$aompdir"/test/smoke-fails
+./check_smoke_fails.sh
+checkrc $?
+copyresults smoke-fails
+
+# Omp5
+mkdir -p "$resultsdir"/omp5
+cd "$aompdir"/test/omp5
+./check_omp5.sh
+checkrc $?
+copyresults omp5
+
+
+# Sollve
+mkdir -p "$resultsdir"/sollve45
+mkdir -p "$resultsdir"/sollve50
+cd "$aompdir"/bin
+./clone_aomp_test.sh
+./run_sollve.sh
+./check_sollve.sh
+checkrc $?
+
+# 4.5 Results
+cd "$HOME"/git/aomp-test/sollve_vv/results_report45
+copyresults sollve45
+
+# 5.0 Results
+cd "$HOME"/git/aomp-test/sollve_vv/results_report50
+copyresults sollve50
+
+cat $unexpresults >> $summary
+echo >> $summary
+echo Overall Unexpected fails: $totalunexpectedfails >> $summary
+echo Script Errors: $scriptfails >> $summary
+if [ "$totalunexpectedfails" -gt 0 ] || [ "$scriptfails" != 0 ]; then
+  echo FAIL >> $summary
+else
+  echo PASS >> $summary
+fi
+
+cat $summary
