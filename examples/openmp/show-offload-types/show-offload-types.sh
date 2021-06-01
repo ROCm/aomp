f753565027dd69b1cf9d26656f03e8449554fba7
gregrodgers <Gregory.Rodgers@amd.com>
2021-06-01 10:02:04 -0500

    added all variants and combinations of offload-type and values for OMP_TARGET_OFFLOAD.  Reordered so demo runs in consistent order

--- examples/openmp/show-offload-types/show-offload-types.sh
+++ examples/openmp/show-offload-types/show-offload-types.sh
@@ -4,8 +4,8 @@
 #
 # find AOMP if not set in an environment variable 
 AOMP=${AOMP:-$HOME/rocm/aomp}
-[[ -f $AOMP/bin/clang ]] ||  AOMP=/usr/lib/aomp
-if [ ! -f $AOMP/bin/clang ] ; then 
+[[ -f $AOMP/bin/clang ]] || AOMP=/usr/lib/aomp
+if [ ! -f $AOMP/bin/clang ] ; then
   echo please build or install AOMP
   exit 1
 fi
@@ -24,78 +24,95 @@ if((omp_get_thread_num()==0) && (omp_get_team_num()==0))
 }
 EOF
 echo
-echo "==== $tmpcfile ===="
+echo "======== $tmpcfile ========"
 cat $tmpcfile
 echo; echo "========== COMPILES ==========="
 
 gpu=`$AOMP/bin/offload-arch`
 
-[[ -f $tdir/gpu-offload ]] && rm $tdir/gpu-offload
-[[ -f $tdir/offload-xnack ]] && rm $tdir/offload-xnack
-[[ -f $tdir/host-offload ]] && rm $tdir/host-offload
 [[ -f $tdir/no-offload ]] && rm $tdir/no-offload
+[[ -f $tdir/host-offload ]] && rm $tdir/host-offload
+[[ -f $tdir/gpu-offload ]] && rm $tdir/gpu-offload
+[[ -f $tdir/qualed-offload ]] && rm $tdir/qualed-offload
 
-cmd="$AOMP/bin/clang -fopenmp -o $tdir/gpu-offload   $tmpcfile --offload-arch=$gpu"
-echo; echo "$cmd" ; $cmd
+cmd="$AOMP/bin/clang $tmpcfile -fopenmp -o $tdir/no-offload     "
+echo "$cmd" ; $cmd
 
-if [[ "${gpu##*sm_}" == "${gpu}" ]] ; then 
-   echo AMD
-  cmd="$AOMP/bin/clang -fopenmp -o $tdir/offload-xnack $tmpcfile --offload-arch=$gpu:xnack+"
-  echo; echo "$cmd" ; $cmd
+cmd="$AOMP/bin/clang $tmpcfile -fopenmp -o $tdir/host-offload   --offload-arch=x86"
+echo "$cmd" ; $cmd
+
+cmd="$AOMP/bin/clang $tmpcfile -fopenmp -o $tdir/gpu-offload    --offload-arch=$gpu"
+echo "$cmd" ; $cmd
+
+# This does not work for nvidia devices 
+if [[ "${gpu##*sm_}" == "${gpu}" ]] ; then
+  cmd="$AOMP/bin/clang $tmpcfile -fopenmp -o $tdir/qualed-offload --offload-arch=$gpu:xnack+"
+  echo "$cmd" ; $cmd
 fi
 
-cmd="$AOMP/bin/clang -fopenmp -o $tdir/host-offload  $tmpcfile --offload-arch=znver1"
-echo; echo "$cmd" ; $cmd
 
-cmd="$AOMP/bin/clang -fopenmp -o $tdir/no-offload    $tmpcfile"
-echo; echo "$cmd" ; $cmd
-
-
-
-echo; echo "========== EXECUTE==========="
-if [[ -f $tdir/gpu-offload ]] ; then 
- echo ; echo  ==== gpu-offload ==== ; $tdir/gpu-offload
-fi
- 
-if [[ -f $tdir/host-offload ]] ; then 
-  echo ; echo  ==== host-offload ==== ; $tdir/host-offload 
-fi
-
-if [[ -f $tdir/no-offload ]] ; then 
+echo; echo "========== DEFAULT EXECUTION ==========="
+if [[ -f $tdir/no-offload ]] ; then
   echo ; echo  ==== no-offload ==== ; $tdir/no-offload
 fi
 
-if [[ -f $tdir/offload-xnack ]] ; then 
-  echo ; echo  ==== gpu-offload a missing requirement ==== ; $tdir/offload-xnack
+if [[ -f $tdir/host-offload ]] ; then
+  echo ; echo  ==== host-offload ==== ; $tdir/host-offload 
 fi
 
-echo; echo export OMP_TARGET_OFFLOAD=DISABLED
+if [[ -f $tdir/gpu-offload ]] ; then
+ echo ; echo  ==== gpu-offload ==== ; $tdir/gpu-offload
+fi
+ 
+if [[ -f $tdir/qualed-offload ]] ; then
+  echo ; echo  ==== qualed-offload with no qualified devices ==== ; $tdir/qualed-offload
+fi
+
+echo; echo "========== EXECUTE DISABLED ==========="
+echo export OMP_TARGET_OFFLOAD=DISABLED
 export OMP_TARGET_OFFLOAD=DISABLED
 
-if [[ -f $tdir/gpu-offload ]] ; then 
-  echo ; echo  ==== gpu-offload with offload disabled appears as if no-offload ==== ; $tdir/gpu-offload
+if [[ -f $tdir/no-offload ]] ; then
+  echo ; echo  ==== no-offload with offload disabled === ; $tdir/no-offload
 fi
 
-if [[ -f $tdir/host-offload ]] ; then 
+if [[ -f $tdir/host-offload ]] ; then
   echo ; echo  ==== host-offload with offload disabled appears as if no-offload ==== ; $tdir/host-offload
 fi
 
-echo; echo export OMP_TARGET_OFFLOAD=MANDATORY
+if [[ -f $tdir/gpu-offload ]] ; then
+  echo ; echo  ==== gpu-offload with offload disabled appears as if no-offload ==== ; $tdir/gpu-offload
+fi
+
+if [[ -f $tdir/qualed-offload ]] ; then
+  echo ; echo  ==== qualed-offload with offload disabled and no qualified devices === ; $tdir/qualed-offload
+fi
+
+echo; echo "========== EXECUTE MANDATORY ==========="
+echo export OMP_TARGET_OFFLOAD=MANDATORY
 export OMP_TARGET_OFFLOAD=MANDATORY
 
-if [[ -f $tdir/host-offload ]] ; then 
+if [[ -f $tdir/no-offload ]] ; then
+  echo ; echo  ==== no-offload with offload mandatory === SHOULD THIS FAIL ?? ; $tdir/no-offload
+fi
+
+if [[ -f $tdir/host-offload ]] ; then
   echo ; echo  ==== host-offload with offload mandatory === ; $tdir/host-offload
 fi
 
-if [[ -f $tdir/offload-xnack ]] ; then 
-  echo ; echo  ==== gpu-offload with offload mandatory and missing requirement === ; $tdir/offload-xnack
+if [[ -f $tdir/gpu-offload ]] ; then
+  echo ; echo  ==== gpu-offload with offload mandatory === ; $tdir/gpu-offload
+fi
+
+if [[ -f $tdir/qualed-offload ]] ; then
+  echo ; echo  ==== qualed-offload with offload mandatory and no qualified devices === ; $tdir/qualed-offload
 fi
 
 # cleanup
-rm $tmpcfile 
-[[ -f $tdir/gpu-offload ]] && rm $tdir/gpu-offload 
-[[ -f $tdir/host-offload ]] && rm $tdir/host-offload 
+rm $tmpcfile
 [[ -f $tidir/no-offload ]] && rm $tdir/no-offload
-[[ -f $tdir/offload-xnack ]] && rm $tdir/offload-xnack
+[[ -f $tdir/host-offload ]] && rm $tdir/host-offload
+[[ -f $tdir/gpu-offload ]] && rm $tdir/gpu-offload
+[[ -f $tdir/qualed-offload ]] && rm $tdir/qualed-offload
 
 echo
