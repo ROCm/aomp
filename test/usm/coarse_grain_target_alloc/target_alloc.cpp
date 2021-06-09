1d2f88290de4aff32615814ff6f3534e85eda471
Carlo Bertolli <carlo.bertolli@amd.com>
2021-06-09 09:53:39 -0500

    fix test returning error upon succcess

--- test/usm/coarse_grain_target_alloc/target_alloc.cpp
+++ test/usm/coarse_grain_target_alloc/target_alloc.cpp
@@ -12,32 +12,28 @@ int main() {
   int n = N;
   int *a = (int *)omp_target_alloc(n*sizeof(int), omp_get_default_device());
   int *b = new int[n*sizeof(int)];
-  int err = 1;
+  int err = 0;
 
   // same
-  if ((err = omp_is_coarse_grain_mem_region(a, n*sizeof(int))))
-    cout << "Memory region is correctly registered as coarse grain" << endl;
-  else
+  err = err || !omp_is_coarse_grain_mem_region(a, n*sizeof(int));
+  if (err)
     cout << "Memory region is *not* correctly registered as coarse grain" << endl;
 
   // subset
-  if ((err = omp_is_coarse_grain_mem_region(a+10, n/2*sizeof(int))))
-    cout << "Memory region is correctly registered as coarse grain" << endl;
-  else
+  err = err || !omp_is_coarse_grain_mem_region(a+10, n/2*sizeof(int));
+  if (err)
     cout << "Memory region is *not* correctly registered as coarse grain" << endl;
 
   // partially contained
   // 10* to make sure the malloc'ed pointer does not span two pages
   // It cannot span three pages because its size is precisely one page
-  if (!(err = omp_is_coarse_grain_mem_region(&a[n/2], 10*n*sizeof(int))))
-    cout << "Memory region is correctly registered as not coarse grain" << endl;
-  else
+  err = err || omp_is_coarse_grain_mem_region(&a[n/2], 10*n*sizeof(int));
+  if (err)
     cout << "Memory region is *not* correctly registered as not coarse grain" << endl;
 
   // completely separated
-  if (!(err = omp_is_coarse_grain_mem_region(b, n*sizeof(int))))
-    cout << "Memory region is correctly registered as not coarse grain" << endl;
-  else
+  err = err || omp_is_coarse_grain_mem_region(b, n*sizeof(int));
+  if (err)
     cout << "Memory region is *not* correctly registered as not coarse grain" << endl;
 
   // needed, otherwise clang will not gen tgt_register_requires call for usm mode
@@ -45,5 +41,5 @@ int main() {
   {
   }
 
-  return !err;
+  return err;
 }
