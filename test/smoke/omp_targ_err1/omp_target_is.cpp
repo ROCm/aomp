559229e17195445068d79c20911cc66430e6728b
Ron Lieberman <ron.lieberman@amd.com>
2021-06-24 13:47:23 -0500

    Fix omp_targ_err1 mapping issue

--- test/smoke/omp_targ_err1/omp_target_is.cpp
+++ test/smoke/omp_targ_err1/omp_target_is.cpp
@@ -5,7 +5,7 @@ int main() {
   a[0] = 1;
   a[1] = 2;
 
-#pragma omp target
+#pragma omp target map(to: a[0:1])
   {
     printf("DEVICE : a:%p &a[1]:%p a[0]:%d  a[1]:%d\n", (void*) a, &a[1],a[0],a[1]);
   }
