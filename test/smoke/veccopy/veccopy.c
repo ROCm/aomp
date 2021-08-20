11cc6f3264f75f18d139aaff2ba9a0139d715f35
Dhruva Chakrabarti <Dhruva.Chakrabarti@amd.com>
2021-08-19 19:37:52 -0700

    Working version of veccopy with trace record generation

--- test/smoke/veccopy/veccopy.c
+++ test/smoke/veccopy/veccopy.c
@@ -1,5 +1,20 @@
 #include <stdio.h>
 #include <omp.h>
+#include <omp-tools.h>
+
+static ompt_start_trace_t ompt_start_trace;
+
+#if 0
+static void ompt_callback_buffer_request_on (
+  int device_num,
+  ompt_buffer_t **buffer,
+  size_t *bytes
+) {
+  *bytes = OMPT_BUFFER_REQUEST_SIZE;
+  *buffer = malloc(*bytes);
+  printf("Allocated %lu bytes at %p in buffer request callback\n", *bytes, *buffer);
+}
+#endif
 
 int main()
 {
@@ -35,3 +50,126 @@ int main()
   return rc;
 }
 
+// Tool related code below
+static ompt_set_callback_t ompt_set_callback;
+
+static void on_ompt_callback_device_initialize
+(
+  int device_num,
+  const char *type,
+  ompt_device_t *device,
+  ompt_function_lookup_t lookup,
+  const char *documentation
+ ) {
+  printf("Invoked on_ompt_callback_device_initialize: %d %s\n", device_num, type);
+  ompt_start_trace = (ompt_start_trace_t) lookup("ompt_start_trace");
+  printf("ompt_start_trace=%p\n", ompt_start_trace);
+  // In many scenarios, this will be a good place to start the
+  // trace. If start_trace is called from the main program, the
+  // programmer has to be careful to place the call after the first
+  // target construct, otherwise the program will fail. This is
+  // because this device_init callback is invoked during the first
+  // target construct implementation and any start_trace must come
+  // afterwards.
+
+  // TODO move the ompt_start_trace to the main program before any
+  // target construct and ensure we error out gracefully. The program
+  // should not assert or crash.
+  //  int status = ompt_start_trace(0, &ompt_callback_buffer_request_on, 0);
+  //  printf("Start trace status=%d\n", status);
+}
+
+static void on_ompt_callback_device_load
+    (
+     int device_num,
+     const char *filename,
+     int64_t offset_in_file,
+     void *vma_in_file,
+     size_t bytes,
+     void *host_addr,
+     void *device_addr,
+     uint64_t module_id
+     ) {
+  printf("Invoked on_ompt_callback_device_load: %d %s %p %p %lu\n",
+     device_num, filename, host_addr, device_addr, bytes);
+}
+
+static void on_ompt_callback_target_data_op
+    (
+     ompt_scope_endpoint_t endpoint,
+     ompt_id_t target_id,
+     ompt_id_t host_op_id,
+     ompt_target_data_op_t optype,
+     void *src_addr,
+     int src_device_num,
+     void *dest_addr,
+     int dest_device_num,
+     size_t bytes,
+     const void *codeptr_ra
+     ) {
+  printf("Invoked on_ompt_callback_target_data_op: %p %p %lu %p\n",
+     src_addr, dest_addr, bytes, codeptr_ra);
+}
+
+static void on_ompt_callback_target
+    (
+     ompt_target_t kind,
+     ompt_scope_endpoint_t endpoint,
+     int device_num,
+     ompt_data_t *task_data,
+     ompt_id_t target_id,
+     const void *codeptr_ra
+     ) {
+  printf("Invoked on_ompt_callback_target: %d %lu %p\n",
+     device_num, target_id, codeptr_ra);
+}
+
+static void on_ompt_callback_target_submit
+    (
+     ompt_scope_endpoint_t endpoint,
+     ompt_id_t target_id,
+     ompt_id_t host_op_id,
+     unsigned int requested_num_teams
+     ) {
+  printf("Invoked on_ompt_callback_target_submit: %d %lu %lu %d\n",
+     endpoint, target_id, host_op_id, requested_num_teams);
+}
+
+int ompt_initialize(
+  ompt_function_lookup_t lookup,
+  int initial_device_num,
+  ompt_data_t *tool_data)
+{
+  ompt_set_callback = (ompt_set_callback_t) lookup("ompt_set_callback");
+  printf("ompt_set_callback=%p\n", ompt_set_callback);
+  
+  ompt_set_callback(ompt_callback_device_initialize,
+		    (ompt_callback_t)&on_ompt_callback_device_initialize);
+  ompt_set_callback(ompt_callback_device_load,
+		    (ompt_callback_t)&on_ompt_callback_device_load);
+  ompt_set_callback(ompt_callback_target_data_op,
+		    (ompt_callback_t)&on_ompt_callback_target_data_op);
+  ompt_set_callback(ompt_callback_target,
+		    (ompt_callback_t)&on_ompt_callback_target);
+  ompt_set_callback(ompt_callback_target_submit,
+		    (ompt_callback_t)&on_ompt_callback_target_submit);
+  return 1; //success
+}
+
+void ompt_finalize(ompt_data_t *tool_data)
+{
+}
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+ompt_start_tool_result_t *ompt_start_tool(
+  unsigned int omp_version,
+  const char *runtime_version)
+{
+  static ompt_start_tool_result_t ompt_start_tool_result = {&ompt_initialize,&ompt_finalize, 0};
+  return &ompt_start_tool_result;
+}
+#ifdef __cplusplus
+}
+#endif
