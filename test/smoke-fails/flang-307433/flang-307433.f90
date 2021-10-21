07cdf02c7856cf0451b136902fb6114638a3b083
Raghu Maddhipatla <Raghu.Maddhipatla@amd.com>
2021-10-21 11:37:39 -0500

    Added smoke-fails tests for SWDEV-307433 and SWDEV-307434. 	new file:   flang-307433/Makefile 	new file:   flang-307433/flang-307433.f90 	new file:   flang-307434/Makefile 	new file:   flang-307434/flang-307434.f90

---
+++ test/smoke-fails/flang-307433/flang-307433.f90
@@ -0,0 +1,43 @@
+PROGRAM test_target_teams_distribute_depend
+  USE iso_fortran_env
+  USE omp_lib
+  implicit none
+  INTEGER :: errors
+  INTEGER, parameter :: N = 1024
+  errors = 0
+
+CONTAINS
+  INTEGER FUNCTION depend_array_section()
+    INTEGER :: errors, x
+    INTEGER,DIMENSION(N) :: a, b, c, d
+
+    DO x = 1, N
+       a(x) = x
+       b(x) = 2 * x
+       c(x) = 0
+       d(x) = 0
+    END DO
+
+    !$omp target data map(to: a(1:N), b(1:N)) map(alloc: c(1:N)) map( &
+    !$omp& from: d(1:N))
+    !$omp target teams distribute nowait depend(out: c(1:N)) map(alloc: &
+    !$omp& a(1:N), b(1:N), c(1:N))
+    DO x = 1, N
+       c(x) = a(x) + b(x)
+    END DO
+    !$omp target teams distribute nowait depend(out: c(1:N)) map(alloc: &
+    !$omp& b(1:N), c(1:N), d(1:N))
+    DO x = 1, N
+       d(x) = c(x) + b(x)
+    END DO
+    !$omp taskwait
+    !$omp end target data
+
+    errors = 0
+
+
+    depend_array_section = errors
+  END FUNCTION depend_array_section
+END PROGRAM test_target_teams_distribute_depend 
+
+
