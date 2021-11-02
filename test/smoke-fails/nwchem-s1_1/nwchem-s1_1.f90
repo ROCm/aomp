639445f37a425bc452c5cbe248de9d2e869f6dc2
Dhruva Chakrabarti <Dhruva.Chakrabarti@amd.com>
2021-11-01 19:15:49 -0700

    o Add a C version of nwchem-s1_1 o Modify the F90 version to match the C version

--- test/smoke-fails/nwchem-s1_1/nwchem-s1_1.f90
+++ test/smoke-fails/nwchem-s1_1/nwchem-s1_1.f90
@@ -4,7 +4,7 @@
 
   implicit integer (a-z)
   l1 = 1; l2 = 1; l3 = 1; l4 = 1; l5 = 1; l6 = 1;
-  u1 = 16; u2 = 16; u3 = 16; u4 = 16; u5 = 16; u6 = 16;
+  u1 = 24; u2 = 24; u3 = 24; u4 = 24; u5 = 24; u6 = 24;
   call tgt_sd_t_s1_1(l1,l2,l3,l4,l5,l6, u1,u2,u3,u4,u5,u6)
   end
 
@@ -12,15 +12,15 @@
 
 subroutine tgt_sd_t_s1_1(l1,l2,l3,l4,l5,l6, u1,u2,u3,u4,u5,u6)
   implicit integer (a-z)
-  real a(16,16,16,16,16,16)
-  real b(16,16,16,16,16,16)
+  real a(24,24,24,24,24,24)
+  real b(24,24,24,24,24,24)
   
   a=3.0
   b=0.0
 
  
 
-!$omp target teams distribute parallel do simd collapse(6) num_teams(240)
+!$omp target teams distribute parallel do simd collapse(6)
   do i1 = l1, u1
    do i2 = l2, u2
     do i3 = l3, u3
@@ -39,6 +39,6 @@ subroutine tgt_sd_t_s1_1(l1,l2,l3,l4,l5,l6, u1,u2,u3,u4,u5,u6)
 !  write(6,*) b(1,1,1,1,1,1)
 !  write(6,*) a(1,1,1,1,1,1)
   
-  write(6,*) ((b(k,j,1,1,1,1),j=1,16),k=1,16)
+  write(6,*) ((b(k,j,1,1,1,1),j=1,4),k=1,4)
   return
 end
