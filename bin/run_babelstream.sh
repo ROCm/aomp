b1255d2368feae6b79fb1c39934606c2f37b37d4
gregrodgers <Gregory.Rodgers@amd.com>
2021-12-08 11:18:10 -0600

    fix script to find the repo and sources correctly.  Also copy sources to a /tmp directory and patch OMPStream.cpp to customize number of teams and threads

--- bin/run_babelstream.sh
+++ bin/run_babelstream.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-# run_babelstream.sh - runs babelstream in the $AOMP_REPOS_TEST dir.
+# run_babelstream.sh - runs babelstream in the $BABELSTREAM_BUILD dir.
 # User can set RUN_OPTIONS to control what variants(openmp, hip) are selected.
+# User can also set BABELSTREAM_BUILD,  BABELSTREAM_REPO, and AOMP env vars.
+# The babelstream source OMPStream.cpp is patched to override number of teams
+# and threads.
 
 # --- Start standard header to set build environment variables ----
 function getdname(){
@@ -30,6 +33,9 @@ thisdir=$(getdname $0)
 # Setup AOMP variables
 AOMP=${AOMP:-/usr/lib/aomp}
 AOMPHIP=${AOMPHIP:-$AOMP}
+# clone_test.sh puts babelstream in $AOMP_REPOS_TEST/babelstream
+BABELSTREAM_REPO=${BABELSTREAM_REPO:-$AOMP_REPOS_TEST/babelstream}
+BABELSTREAM_BUILD=${BABELSTREAM_BUILD:-/tmp/$USER/babelstream}
 
 # Use function to set and test AOMP_GPU
 setaompgpu
@@ -41,16 +47,38 @@ omp_src="main.cpp OMPStream.cpp"
 hip_src="main.cpp HIPStream.cpp"
 std="-std=c++11"
 
-if [ -d $AOMP_REPOS_TEST/BabelStream ]; then
-  cd $AOMP_REPOS_TEST/BabelStream
-  rm -f results.txt
-else
-  echo "ERROR: BabelStream not found in $AOMP_REPOS_TEST."
+if [ ! -d $BABELSTREAM_REPO ]; then
+  echo "ERROR: BabelStream not found in $BABELSTREAM_REPO"
+  echo "       Consider running these commands:"
+  echo
+  echo "mkdir -p $AOMP_REPOS_TEST"
+  echo "cd $AOMP_REPOS_TEST"
+  echo "git clone https://github.com/UoB-HPC/babelstream"
+  echo
   exit 1
 fi
+curdir=$PWD
+mkdir -p $BABELSTREAM_BUILD
+echo cd $BABELSTREAM_BUILD
+cd $BABELSTREAM_BUILD
+cp $BABELSTREAM_REPO/src/main.cpp .
+cp $BABELSTREAM_REPO/src/Stream.h .
+cp $BABELSTREAM_REPO/src/omp/OMPStream.cpp .
+cp $BABELSTREAM_REPO/src/omp/OMPStream.cpp OMPStream.cpp.orig
+cp $BABELSTREAM_REPO/src/omp/OMPStream.h .
+cp $BABELSTREAM_REPO/src/hip/HIPStream.cpp .
+cp $BABELSTREAM_REPO/src/hip/HIPStream.h .
+patch < $thisdir/patches/babelstream.patch
 
 echo RUN_OPTIONS: $RUN_OPTIONS
+thisdate=`date`
+echo >>results.txt
+echo "=========> RUNDATE:  $thisdate" >>results.txt
+COMPILER=`$AOMP/bin/llc --version  | grep version`
+echo "=========> COMPILER: $COMPILER" >>results.txt
+echo "=========> GPU:      $AOMP_GPU" >>results.txt
 for option in $RUN_OPTIONS; do
+  echo >>results.txt
   if [ "$option" == "openmp" ]; then
     EXEC=omp-stream
     rm -f $EXEC
@@ -73,3 +101,6 @@ for option in $RUN_OPTIONS; do
   fi
   echo >> results.txt
 done
+cd $curdir
+echo
+echo "DONE. See results at end of file $BABELSTREAM_BUILD/results.txt"
