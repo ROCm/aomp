7f11cbb4e75bd911aceae019da93ebe1ebec5663
gregrodgers <Gregory.Rodgers@amd.com>
2021-12-08 11:56:56 -0600

    Add RUNOPTION omp_default which just removes simd from the omp target pragma without NUM_TEAMS or THREAD_LIMIT clause

--- bin/run_babelstream.sh
+++ bin/run_babelstream.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # run_babelstream.sh - runs babelstream in the $BABELSTREAM_BUILD dir.
-# User can set RUN_OPTIONS to control what variants(openmp, hip) are selected.
+# User can set RUN_OPTIONS to control what variants(omp_default, openmp, hip) are selected.
 # User can also set BABELSTREAM_BUILD,  BABELSTREAM_REPO, and AOMP env vars.
 # The babelstream source OMPStream.cpp is patched to override number of teams
 # and threads.
@@ -40,7 +40,7 @@ BABELSTREAM_BUILD=${BABELSTREAM_BUILD:-/tmp/$USER/babelstream}
 # Use function to set and test AOMP_GPU
 setaompgpu
 
-RUN_OPTIONS=${RUN_OPTIONS:-"openmp hip"}
+RUN_OPTIONS=${RUN_OPTIONS:-"omp_default openmp hip"}
 omp_flags="-O3 -fopenmp -fopenmp-targets=amdgcn-amd-amdhsa -Xopenmp-target=amdgcn-amd-amdhsa -march=$AOMP_GPU -DOMP -DOMP_TARGET_GPU"
 hip_flags="-O3 --offload-arch=$AOMP_GPU -DHIP -x hip"
 omp_src="main.cpp OMPStream.cpp"
@@ -61,14 +61,16 @@ curdir=$PWD
 mkdir -p $BABELSTREAM_BUILD
 echo cd $BABELSTREAM_BUILD
 cd $BABELSTREAM_BUILD
-cp $BABELSTREAM_REPO/src/main.cpp .
-cp $BABELSTREAM_REPO/src/Stream.h .
-cp $BABELSTREAM_REPO/src/omp/OMPStream.cpp .
-cp $BABELSTREAM_REPO/src/omp/OMPStream.cpp OMPStream.cpp.orig
-cp $BABELSTREAM_REPO/src/omp/OMPStream.h .
-cp $BABELSTREAM_REPO/src/hip/HIPStream.cpp .
-cp $BABELSTREAM_REPO/src/hip/HIPStream.h .
-patch < $thisdir/patches/babelstream.patch
+if [ "$1" != "nopatch" ] ; then 
+   cp $BABELSTREAM_REPO/src/main.cpp .
+   cp $BABELSTREAM_REPO/src/Stream.h .
+   cp $BABELSTREAM_REPO/src/omp/OMPStream.cpp .
+   cp $BABELSTREAM_REPO/src/omp/OMPStream.cpp OMPStream.cpp.orig
+   cp $BABELSTREAM_REPO/src/omp/OMPStream.h .
+   cp $BABELSTREAM_REPO/src/hip/HIPStream.cpp .
+   cp $BABELSTREAM_REPO/src/hip/HIPStream.h .
+   patch < $thisdir/patches/babelstream.patch
+fi
 
 echo RUN_OPTIONS: $RUN_OPTIONS
 thisdate=`date`
@@ -87,6 +89,14 @@ for option in $RUN_OPTIONS; do
     if [ $? -ne 1 ]; then
       ./$EXEC 2>&1 | tee -a results.txt
     fi
+  elif [ "$option" == "omp_default" ]; then
+    EXEC=omp-stream-default
+    rm -f $EXEC
+    echo $AOMP/bin/clang++ -D_DEFAULTS_NOSIMD $omp_flags $omp_src $std -o $EXEC
+    $AOMP/bin/clang++ -D_DEFAULTS_NOSIMD $omp_flags $omp_src $std -o $EXEC
+    if [ $? -ne 1 ]; then
+      ./$EXEC 2>&1 | tee -a results.txt
+    fi
   elif [ "$option" == "hip" ]; then
     EXEC=hip-stream
     rm -f $EXEC
@@ -99,7 +109,6 @@ for option in $RUN_OPTIONS; do
     echo "ERROR: Option not recognized: $option."
     exit 1
   fi
-  echo >> results.txt
 done
 cd $curdir
 echo
