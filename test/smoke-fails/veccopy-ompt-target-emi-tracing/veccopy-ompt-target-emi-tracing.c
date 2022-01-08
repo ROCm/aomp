8fc24ef0b1a3d25c812e1d1fea16ae1a1381179c
Dhruva Chakrabarti <Dhruva.Chakrabarti@amd.com>
2022-01-07 20:09:33 -0800

    Added 2 tests for OMPT target tracing

---
+++ test/smoke-fails/veccopy-ompt-target-emi-tracing/veccopy-ompt-target-emi-tracing.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <assert.h>
+#include <omp.h>
+
+#include "callbacks.h"
+
+static int start_trace();
+static int flush_trace();
+static int stop_trace();
+
+int main()
+{
+  int N = 100000;
+
+  int a[N];
+  int b[N];
+
+  int i;
+
+  for (i=0; i<N; i++)
+    a[i]=0;
+
+  for (i=0; i<N; i++)
+    b[i]=i;
+
+  start_trace();
+  
+#pragma omp target parallel for
+  {
+    for (int j = 0; j< N; j++)
+      a[j]=b[j];
+  }
+
+  flush_trace();
+  stop_trace();
+
+  start_trace();
+  
+#pragma omp target teams distribute parallel for
+  {
+    for (int j = 0; j< N; j++)
+      a[j]=b[j];
+  }
+
+  stop_trace();
+
+  int rc = 0;
+  for (i=0; i<N; i++)
+    if (a[i] != b[i] ) {
+      rc++;
+      printf ("Wrong value: a[%d]=%d\n", i, a[i]);
+    }
+
+  if (!rc)
+    printf("Success\n");
+
+  return rc;
+}
+
