abb6160f3adb92c4d453624c0a8a2789cae7f106
Ron Lieberman <ron.lieberman@amd.com>
2022-01-14 13:35:13 +0000

    tweak flang usm test to make it an array

--- test/smoke-fails/flang-usm/flang-usm.f90
+++ test/smoke-fails/flang-usm/flang-usm.f90
@@ -1,11 +1,11 @@
 program test
-      integer :: i
-      i = 0
+      integer :: i(5)
+      i(2) = 0
   !$omp requires unified_shared_memory
      !$omp target
-      i = 1
+      i(2) = 1
      !$omp end target
-      if (i .ne. 1) then
+      if (i(2) .ne. 1) then
         print *,'failed'
         call exit(1)
       endif
