a89f48241a6841eae547cc163698117e2d0c3cf0
Raghu Maddhipatla <Raghu.Maddhipatla@amd.com>
2022-02-18 18:36:36 -0600

    Edit run_genasis.sh script to make it run with OpenMP without enabling GPU. 	modified:   patches/genasis.patch 	modified:   run_genasis.sh

--- bin/run_genasis.sh
+++ bin/run_genasis.sh
@@ -36,10 +36,16 @@ setaompgpu
 REPO_DIR=$AOMP_REPOS_TEST/GenASis
 cd $REPO_DIR
 patchrepo $REPO_DIR
-cd Programs/Examples/Basics/FluidDynamics/Executables
+cd $REPO_DIR/Programs/UnitTests/Basics/Runtime/Executables
+export LD_LIBRARY_PATH=$HOME/rocm/aomp/lib:/opt/openmpi-4.0.3/lib:$LD_LIBRARY_PATH
 export GENASIS_MACHINE=ROCm
 make PURPOSE=OPTIMIZE all
 rc=$?
-if [ $rc == 0 ] ; then 
-  mpirun -np 8 ./SineWaveAdvection_Linux_GCC nCells=128,128,128
+if [ $rc == 0 ] ; then
+  export OMP_NUM_THREADS=8
+  ./PROGRAM_HEADER_Singleton_Test_ROCm
+  cd $REPO_DIR/Programs/Examples/Basics/FluidDynamics/Executables
+  make PURPOSE=OPTIMIZE all
+  ./RiemannProblem_ROCm Verbosity=INFO_2 nCells=256,256 \ Dimensionality=2D FinishTime=0.25 nWrite=10
+  #mpirun -np 8 ./SineWaveAdvection_Linux_GCC nCells=128,128,128
 fi
