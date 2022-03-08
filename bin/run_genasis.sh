6a3431d92c9491a8a1425b7a6d8b25835dd4422e
Raghu Maddhipatla <Raghu.Maddhipatla@amd.com>
2022-03-08 17:20:09 -0600

    Change ruscript according to AOMP_SUPP and AOMP_SUPP_INSTALL environment variables. 	modified:   run_genasis.sh

--- bin/run_genasis.sh
+++ bin/run_genasis.sh
@@ -27,9 +27,6 @@ thisdir=$(getdname $0)
 . $thisdir/aomp_common_vars
 # --- end standard header ----
 
-#rm -rf ~/git/aomp-test/GenASis
-#./clone_test.sh
-
 # Setup AOMP variables
 AOMP?=${AOMP:-/usr/lib/aomp}
 
@@ -42,11 +39,12 @@ REPO_DIR=$AOMP_REPOS_TEST/GenASis
 cp Makefile_ROCm $REPO_DIR/Build/Machines/
 
 cd $REPO_DIR
-#patchrepo $REPO_DIR
 
-export OPENMPI_DIR="/opt/openmpi-4.0.4-newflang"
-export SILO_DIR="/usr/local/silo/silo-4.10.2"
-export HDF5_DIR="/usr/include/hdf5/serial"
+AOMP_SUPP=${AOMP_SUPP:-$HOME/local}
+AOMP_SUPP_INSTALL=${AOMP_SUPP_INSTALL:-$AOMP_SUPP/install}
+export OPENMPI_DIR="$AOMP_SUPP_INSTALL/openmpi-4.1.1"
+export SILO_DIR="$AOMP_SUPP_INSTALL/silo-4.10.2"
+export HDF5_DIR="$AOMP_SUPP_INSTALL/hdf5-1.12.0"
 export GENASIS_MACHINE=ROCm
 
 if [ -d "$AOMP" ] ; then
@@ -58,75 +56,28 @@ fi
 currdir=$(pwd)
 if [ -d "$SILO_DIR" ] ; then
     echo "SILO 4.10.2 exists."
-elif [ -d $HOME/rocm/silo-4.10.2-install ] ; then
-    echo "SILO 4.10.2 exists in ROCm directory."
-    export SILO_DIR=$HOME/rocm/silo-4.10.2-install
 else
-    echo "SILO 4.10.2 does not exist. Installing SILO 4.10.2..."
-    cd /tmp/
-    if [ ! -f silo-4.10.2.tgz ] ; then
-      wget https://wci.llnl.gov/sites/wci/files/2021-01/silo-4.10.2.tgz
-    fi
-    tar xvf silo-4.10.2.tgz
-    cd /tmp/silo-4.10.2
-    mkdir -p /tmp/silo-4.10.2-install
-    ./configure --prefix=$HOME/rocm/silo-4.10.2-install
-    make -j8
-    make install
-    export SILO_DIR=$HOME/rocm/silo-4.10.2-install
+    echo "SILO 4.10.2 does not exist. Please run ./build_supp.sh silo in $thisdir"
+    exit
 fi
 if [ -d "$HDF5_DIR" ] ; then
     echo "HDF5-1.12.0 exists."
-    export INCLUDE_HDF5="-I/usr/include/hdf5/serial"
-    export LIBRARY_HDF5="-L/usr/lib/x86_64-linux-gnu/hdf5/serial -lhdf5_fortran -lhdf5"
-elif [ -d $HOME/rocm/hdf5-1.12.0-install ] ; then
-    echo "HDF5-1.12.0 exists in ROCm directory."
-    export HDF5_DIR=$HOME/rocm/hdf5-1.12.0-install
-    export INCLUDE_HDF5="-I${HDF5_DIR}/include"
-    export LIBRARY_HDF5="-L${HDF5_DIR}/lib -lhdf5_fortran -lhdf5"
+    export INCLUDE_HDF5="-I$HDF5_DIR/include"
+    export LIBRARY_HDF5="-L$HDF5_DIR/lib -lhdf5_fortran -lhdf5"
 else
-    echo "HDF5-1.12.0 does not exist. Installing HDF5-1.12.0..."
-    cd /tmp/
-    if [ ! -f hdf5-1.12.0.tar.bz2 ] ; then
-      wget https://support.hdfgroup.org/ftp/HDF5/releases/hdf5-1.12/hdf5-1.12.0/src/hdf5-1.12.0.tar.bz2
-    fi
-    tar xvf hdf5-1.12.0.tar.bz2
-    cd /tmp/hdf5-1.12.0
-    ./configure --prefix=$HOME/rocm/hdf5-1.12.0-install --enable-fortran
-    make -j8
-    make install
-    export HDF5_DIR=$HOME/rocm/hdf5-1.12.0-install
-    export INCLUDE_HDF5="-I${HDF5_DIR}/include"
-    export LIBRARY_HDF5="-L${HDF5_DIR}/lib -lhdf5_fortran -lhdf5"
+    echo "HDF5-1.12.0 does not exist. Please run ./build_supp.sh hdf5 in $thisdir"
+    exit
 fi
-export OMPI_FC=$AOMP/bin/flang
-export OMPI_CC=$AOMP/bin/clang
-export OMPI_CXX=$AOMP/bin/clang++
-export LD_LIBRARY_PATH=$AOMP/lib:$LD_LIBRARY_PATH
-export PATH=$AOMP/bin:$PATH
 if [ -d "$OPENMPI_DIR" ] ; then
-    echo "OpenMPI-4.0.4 exists."
+    echo "OpenMPI-4.1.1 exists."
     export MPI=${OPENMPI_DIR}
-elif [ -d $HOME/rocm/openmpi-4.1.1-install ] ; then
-    echo "OpenMPI-4.1.1 exists in ROCm directory."
-    export OPENMPI_DIR=$HOME/rocm/openmpi-4.1.1-install
 else
-    echo "OpenMPI does not exist. Installing OpenMPI-4.1.1..."
-    cd /tmp/
-    if [ ! -f openmpi-4.1.1.tar.bz2 ] ; then
-      wget https://download.open-mpi.org/release/open-mpi/v4.1/openmpi-4.1.1.tar.bz2
-    fi
-    tar xvf openmpi-4.1.1.tar.bz2
-    cd openmpi-4.1.1
-    ./configure OMPI_CC=clang OMPI_CXX=clang++ OMPI_F90=flang CXX=clang++ CC=cla
-ng FC=flang --prefix=$HOME/rocm/openmpi-4.1.1-install
-    make -j8
-    make install
-    export OPENMPI_DIR=$HOME/rocm/openmpi-4.1.1-install
+    echo "OpenMPI does not exist. Please run ./build_supp.sh openmpi in $thisdir"
+    exit
 fi
 
 export LD_LIBRARY_PATH=$HOME/rocm/aomp/lib:$OPENMPI_DIR/lib:$LD_LIBRARY_PATH
-export FORTRAN_COMPILE="$AOMP/bin/flang -c --save-temps -I$OPENMPI_DIR/lib"
+export FORTRAN_COMPILE="$AOMP/bin/flang -c -I$OPENMPI_DIR/lib"
 export CC_COMPILE="$AOMP/bin/clang"
 export OTHER_LIBS="-lm -L$AOMP/lib -L$OPENMPI_DIR/lib -lmpi_usempif08 -lmpi_mpifh -lmpi -lflang -lflangmain -lflangrti -lpgmath -lomp -lomptarget "
 export FORTRAN_LINK="$AOMP/bin/clang $OTHER_LIBS"
@@ -151,13 +102,22 @@ if [ "$1" != "runonly" ] ; then
      echo "ERROR in 2nd Make"
      exit
   fi
+  echo
+  cd $REPO_DIR/Programs/UnitTests/Basics/Devices/Executables
+  echo "=================  STARTING 3rd  MAKE in $PWD ========"
+  make PURPOSE=OPTIMIZE all
+  rc=$?
+  if [ $rc != 0 ] ; then
+     echo "ERROR in 3rd Make"
+     exit
+  fi
   echo "=================  BUILD IS DONE ========"
 fi
 
 if [ "$1" != "buildonly" ] ; then
   cd $REPO_DIR/Programs/UnitTests/Basics/Runtime/Executables
   echo ./PROGRAM_HEADER_Singleton_Test_$GENASIS_MACHINE
-  gdb ./PROGRAM_HEADER_Singleton_Test_$GENASIS_MACHINE
+  ./PROGRAM_HEADER_Singleton_Test_$GENASIS_MACHINE
   echo
   cd $REPO_DIR/Programs/Examples/Basics/FluidDynamics/Executables
   echo
@@ -168,10 +128,29 @@ if [ "$1" != "buildonly" ] ; then
   echo
   echo "=================  attempting mpirun  ========"
   echo
-  echo ldd ./SineWaveAdvection_$GENASIS_MACHINE
-  ldd ./SineWaveAdvection_$GENASIS_MACHINE
+  #echo ldd ./SineWaveAdvection_$GENASIS_MACHINE
+  #ldd ./SineWaveAdvection_$GENASIS_MACHINE
   echo
   _cmd="$OPENMPI_DIR/bin/mpirun -np 1 $REPO_DIR/Programs/Examples/Basics/FluidDynamics/Executables/SineWaveAdvection_$GENASIS_MACHINE nCells=128,128,128"
   echo $_cmd
   time $_cmd
+  echo "=================  end mpirun  ========"
+  echo "================= Now running Unitests related to offloading ========"
+  cd $REPO_DIR/Programs/UnitTests/Basics/Devices/Executables
+  ./AllocateDevice_Command_Test_$GENASIS_MACHINE
+  echo "AllocateDevice_Command_Test_$GENASIS_MACHINE done!"
+  ./AllocateHost_Command_Test_$GENASIS_MACHINE
+  echo "AllocateHost_Command_Test_$GENASIS_MACHINE done!"
+  ./AssociateHost_Command_Test_$GENASIS_MACHINE
+  echo "AssociateHost_Command_Test_$GENASIS_MACHINE done!"
+  ./DeallocateDevice_Command_Test_$GENASIS_MACHINE
+  echo "DeallocateDevice_Command_Test_$GENASIS_MACHINE done!"
+  ./DeviceAddress_Function_Test_$GENASIS_MACHINE
+  echo "DeviceAddress_Function_Test_$GENASIS_MACHINE done!"
+  ./DeviceInterface_Test_$GENASIS_MACHINE
+  echo "DeviceInterface_Test_$GENASIS_MACHINE done!"
+  ./DisassociateHost_Command_Test_$GENASIS_MACHINE
+  echo "DisassociateHost_Command_Test_$GENASIS_MACHINE done!"
+  ./UpdateDevice_Command_Test_$GENASIS_MACHINE
+  echo "UpdateDevice_Command_Test_$GENASIS_MACHINE done!"
 fi
