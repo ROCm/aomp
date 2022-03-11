56abb9e48ba5d71fb0444c369f7d1230a2732b49
Dominik Adamski <Dominik.Adamski@amd.com>
2022-03-11 21:30:53 +0100

    Add simple test cases for comparing *.ll code generated by flang and clang
    Signed-off-by: Dominik Adamski <Dominik.Adamski@amd.com>

---
+++ test/smoke-fails/flang-ifaces/flang-ifaces.f90
@@ -0,0 +1,39 @@
+program main
+   integer :: a(256)
+   integer :: b(256)
+   integer :: c
+   integer :: d
+   integer :: e(256)
+   integer :: f
+   integer :: i
+   !$omp target teams distribute parallel do map(tofrom: a)
+   DO i = 1, 256
+         a(i) = i
+   END DO 
+   !$omp end target teams distribute parallel do
+
+   !$omp target teams distribute map(tofrom: b)
+   DO i = 1, 256
+         b(i) = i
+   END DO 
+   !$omp end target teams distribute
+
+   !$omp target parallel map(tofrom: c)
+         c = 2
+   !$omp end target parallel
+
+   !$omp target teams map(tofrom: d)
+         d = 3
+   !$omp end target teams
+
+   !$omp target parallel do map(tofrom: e)
+    DO i = 1, 256
+         e(i) = i
+    END DO
+   !$omp end target parallel do 
+
+   !$omp target map(tofrom : f)
+        f = 4
+   !$omp end target
+
+end program main
