50637cca49185589e26bba31b0af26ce783d3552
Ethan Stewart <ethan.stewart@amd.com>
2022-03-21 20:41:30 -0500

    [packaging] - Add links for gpurun and clean up debian files.

--- bin/build-deb-aomp.sh
+++ bin/build-deb-aomp.sh
@@ -81,14 +81,13 @@ fi
 
 # Make the install and links file version specific
 echo "usr/lib/$dirname usr/lib/aomp" > $froot/debian/$pkgname.links
-echo "usr/lib/$dirname/bin/bundle.sh  /usr/bin/bundle.sh" >> $froot/debian/$pkgname.links
-echo "usr/lib/$dirname/bin/unbundle.sh  /usr/bin/unbundle.sh" >> $froot/debian/$pkgname.links
 echo "usr/lib/$dirname/bin/aompExtractRegion /usr/bin/aompExtractRegion" >> $froot/debian/$pkgname.links
 echo "usr/lib/$dirname/bin/cloc.sh /usr/bin/cloc.sh" >> $froot/debian/$pkgname.links
 echo "usr/lib/$dirname/bin/mymcpu  /usr/bin/mymcpu" >> $froot/debian/$pkgname.links
 echo "usr/lib/$dirname/bin/mygpu  /usr/bin/mygpu" >> $froot/debian/$pkgname.links
 echo "usr/lib/$dirname/bin/aompversion /usr/bin/aompversion" >> $froot/debian/$pkgname.links
 echo "usr/lib/$dirname/bin/aompcc /usr/bin/aompcc" >> $froot/debian/$pkgname.links
+echo "usr/lib/$dirname/bin/gpurun /usr/bin/gpurun" >> $froot/debian/$pkgname.links
 echo "usr/lib/$dirname" > $froot/debian/$pkgname.install
 echo "usr/share/doc/$dirname" >> $froot/debian/$pkgname.install
 
