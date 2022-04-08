1f1481241eb5f3ec0b0991828ae6719f42fec85b
DominikAdamski <dominik.adamski@amd.com>
2022-04-08 12:42:54 +0200

    Add verification of the result for flang-272730 (#369)
    Signed-off-by: Dominik Adamski <Dominik.Adamski@amd.com>

--- test/smoke/flang-272730-complex/flang272730-complex.f90
+++ test/smoke/flang-272730-complex/flang272730-complex.f90
@@ -3,8 +3,11 @@ program loop_test
       implicit none
       integer   :: i
       complex   :: C,D
+      complex   :: C_ref,D_ref
       C=(0,0)
       D=(1,2)
+      C_ref=(0,0)
+      D_ref=(1,2)
 
       !$OMP TARGET PARALLEL DO REDUCTION(+:C)
       do i=1, 10
@@ -12,7 +15,16 @@ program loop_test
       end do
       !$OMP  END TARGET PARALLEL DO
 
-      write(*, *) "C= ", C
+      do i=1, 10
+         C_ref=C_ref+EXP(D_ref)
+      end do
 
+      write(*, *) "C= ", C
+      write(*, *) "C_ref= ", C_ref
+
+      if (C .ne. C_ref) then
+             write(*,*) "Failed"
+             stop 2
+      endif
 end program loop_test
 
