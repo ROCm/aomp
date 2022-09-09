fb996e9b1d2288c56119c84320a4db06c4e5162b
grodgers <Gregory.Rodgers@amd.com>
2022-09-09 09:08:35 -0400

    this is a major cleanup of the run_babelstream.sh script.  The RUN_OPTIONS have changed.  We no longer patch the babelstream code.  We now use -fopenmp-target-fast for LLVM 16 and beyond

--- bin/run_babelstream.sh
+++ bin/run_babelstream.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
 # run_babelstream.sh - runs babelstream in the $BABELSTREAM_BUILD dir.
-# User can set RUN_OPTIONS to control what variants(omp_default, openmp, no_loop, hip) are selected.
-# User can also set BABELSTREAM_BUILD,  BABELSTREAM_REPO, and AOMP env vars.
-# The babelstream source OMPStream.cpp is patched to override number of teams
-# and threads.
+# User can set RUN_OPTIONS to control which of the 4 variants to build and run
+# The variants are:  omp-default, omp-fast, hip, and omp-cpu .
+#
+# This script allows overrides to the following environment variables:
+#   BABELSTREAM_BUILD   - Build directory with results
+#   BABELSTREAM_REPO    - Directory for the Babelstream repo
+#   BABLESTREAM_REPEATS - The number of repititions
+#   AOMP                - Compiler install directory
+#   AOMPHIP             - HIP Compiler install directory, defaults to $AOMP
+#   RUN_OPTIONS         - which of 4 variants to build and execute
 
 # --- Start standard header to set AOMP environment variables ----
 realpath=`realpath $0`
@@ -12,28 +18,50 @@ thisdir=`dirname $realpath`
 . $thisdir/aomp_common_vars
 # --- end standard header ----
 
-# Setup AOMP variables
+# Set defaults for environment variables
 AOMP=${AOMP:-/usr/lib/aomp}
 AOMPHIP=${AOMPHIP:-$AOMP}
-# clone_test.sh puts babelstream in $AOMP_REPOS_TEST/babelstream
+
+# note: clone_test.sh puts babelstream in $AOMP_REPOS_TEST/babelstream
 BABELSTREAM_REPO=${BABELSTREAM_REPO:-$AOMP_REPOS_TEST/babelstream}
 BABELSTREAM_BUILD=${BABELSTREAM_BUILD:-/tmp/$USER/babelstream}
+BABELSTREAM_REPEATS=${BABELSTREAM_REPEATS:-10}
 
 # Use function to set and test AOMP_GPU
 setaompgpu
+# skip hip if nvidida
 if [ "${AOMP_GPU:0:3}" == "sm_" ] ; then
    TRIPLE="nvptx64-nvidia-cuda"
-   RUN_OPTIONS=${RUN_OPTIONS:-"omp_default no_loop"}
+   RUN_OPTIONS=${RUN_OPTIONS:-"omp-default omp-fast"}
 else
    TRIPLE="amdgcn-amd-amdhsa"
-   RUN_OPTIONS=${RUN_OPTIONS:-"omp_default no_loop hip"}
+   RUN_OPTIONS=${RUN_OPTIONS:-"omp-default omp-fast hip"}
 fi
 
-omp_flags="-O3 -fopenmp -fopenmp-targets=$TRIPLE -Xopenmp-target=$TRIPLE -march=$AOMP_GPU -DOMP -DOMP_TARGET_GPU"
+omp_target_flags="-O3 -fopenmp -fopenmp-targets=$TRIPLE -Xopenmp-target=$TRIPLE -march=$AOMP_GPU -DOMP -DOMP_TARGET_GPU -Dsimd="
+#  So this script runs with old comilers, we only use -fopenmp-target-fast
+#  for LLVM 16 or higher
+LLVM_VERSION=`$AOMP/bin/clang++ --version | grep version | cut -d" " -f 4 | cut -d"." -f1`
+echo "LLVM VERSION IS $LLVM_VERSION"
+if [[ $LLVM_VERSION -ge 16 ]] ; then
+  omp_fast_flags="-fopenmp-gpu-threads-per-team=1024 -fopenmp-target-fast $omp_target_flags"
+else
+  if [[ $LLVM_VERSION -ge 15 ]] ; then
+    omp_fast_flags="-fopenmp-target-ignore-env-vars -fopenmp-assume-no-thread-state -fopenmp-target-new-runtime $omp_target_flags"
+  else
+    if [[ $LLVM_VERSION -ge 14 ]] ; then
+      omp_fast_flags="-fopenmp-target-new-runtime $omp_target_flags"
+    else
+      omp_fast_flags="$omp_target_flags"
+    fi
+  fi
+fi
 omp_cpu_flags="-O3 -fopenmp -DOMP"
 hip_flags="-O3 --offload-arch=$AOMP_GPU -Wno-unused-result -DHIP -x hip"
+
 omp_src="main.cpp OMPStream.cpp"
 hip_src="main.cpp HIPStream.cpp"
+
 gccver=`gcc --version | grep gcc | cut -d")" -f2 | cut -d"." -f1`
 # ubunutu 18.04 gcc7 requires -stdlib=libc++ gcc9 is ok
 if [ "$gccver" == " 7" ] ; then
@@ -56,7 +84,7 @@ curdir=$PWD
 mkdir -p $BABELSTREAM_BUILD
 echo cd $BABELSTREAM_BUILD
 cd $BABELSTREAM_BUILD
-if [ "$1" != "nopatch" ] ; then 
+if [ "$1" != "nocopy" ] ; then
    cp $BABELSTREAM_REPO/src/main.cpp .
    cp $BABELSTREAM_REPO/src/Stream.h .
    cp $BABELSTREAM_REPO/src/omp/OMPStream.cpp .
@@ -64,7 +92,6 @@ if [ "$1" != "nopatch" ] ; then
    cp $BABELSTREAM_REPO/src/omp/OMPStream.h .
    cp $BABELSTREAM_REPO/src/hip/HIPStream.cpp .
    cp $BABELSTREAM_REPO/src/hip/HIPStream.h .
-   patch < $thisdir/patches/babelstream.patch
 fi
 
 echo RUN_OPTIONS: $RUN_OPTIONS
@@ -75,63 +102,33 @@ COMPILER=`$AOMP/bin/llc --version  | grep version`
 echo "=========> COMPILER: $COMPILER" >>results.txt
 echo "=========> GPU:      $AOMP_GPU" >>results.txt
 for option in $RUN_OPTIONS; do
-  echo >>results.txt
-  if [ "$option" == "openmp" ]; then
-    EXEC=omp-stream
-    rm -f $EXEC
-    echo $AOMP/bin/clang++ $omp_flags $omp_src $std -o $EXEC
-    $AOMP/bin/clang++ $omp_flags $omp_src $std -o $EXEC
-    if [ $? -ne 1 ]; then
-      ./$EXEC 2>&1 | tee -a results.txt
-    fi
-  elif [ "$option" == "omp_default" ]; then
-    EXEC=omp-stream-default
-    rm -f $EXEC
-    echo $AOMP/bin/clang++ -D_DEFAULTS_NOSIMD $omp_flags $omp_src $std -o $EXEC
-    $AOMP/bin/clang++ -D_DEFAULTS_NOSIMD $omp_flags $omp_src $std -o $EXEC
-    if [ $? -ne 1 ]; then
-      ./$EXEC 2>&1 | tee -a results.txt
-    fi
-  elif [ "$option" == "no_loop" ]; then
-    # FAST_DOT requires merge of http://gerrit-git.amd.com/c/lightning/ec/llvm-project/+/705744
-    $AOMP/bin/llvm-nm $AOMP/lib/libomptarget-new-amdgpu-gfx906.bc | grep kmpc_xteam_sum_d >/dev/null
-    FAST_DOT_STATUS=$?
-    # Check if new runtime is disabled via CCC_OVERRIDE_OPTIONS
-    # Set new runtime status = 0 if new runtime has not been disabled
-    echo "Checking CCC_OVERRIDE_OPTIONS"
-    echo $CCC_OVERRIDE_OPTIONS | grep -ve '+-fno-openmp-target-new-runtime'
-    NEW_RT_STATUS=$?
-    FAST_DOT_STATUS=$((FAST_DOT_STATUS+NEW_RT_STATUS))
-    [ $FAST_DOT_STATUS == 0 ] && omp_flags+=" -DOMP_TARGET_FAST_DOT"
-    omp_flags+=" -fopenmp-target-ignore-env-vars"
-    omp_flags+=" -fopenmp-assume-no-thread-state"
-    omp_flags+=" -fopenmp-target-new-runtime"
-    EXEC=omp-stream-no-loop
-    rm -f $EXEC
-    echo $AOMP/bin/clang++ -D_DEFAULTS_NOSIMD $omp_flags $omp_src $std -o $EXEC
-    $AOMP/bin/clang++ -D_DEFAULTS_NOSIMD $omp_flags $omp_src $std -o $EXEC
-    if [ $? -ne 1 ]; then
-      ./$EXEC 2>&1 | tee -a results.txt
-    fi
-  elif [ "$option" == "omp_cpu" ]; then
-    EXEC=omp-stream-cpu
-    rm -f $EXEC
-    echo $AOMP/bin/clang++ $omp_cpu_flags $omp_src $std -o $EXEC
-    $AOMP/bin/clang++ $omp_cpu_flags $omp_src $std -o $EXEC
-    if [ $? -ne 1 ]; then
-      ./$EXEC 2>&1 | tee -a results.txt
-    fi
+  echo ; echo >>results.txt
+  EXEC=stream-$option
+  rm -f $EXEC
+  if [ "$option" == "omp-default" ]; then
+    compile_cmd="$AOMP/bin/clang++ $std $omp_target_flags $omp_src -o $EXEC"
+  elif [ "$option" == "omp-fast" ]; then
+    compile_cmd="$AOMP/bin/clang++ $std $omp_fast_flags   $omp_src -o $EXEC"
+  elif [ "$option" == "omp-cpu" ]; then
+    compile_cmd="$AOMP/bin/clang++ $std $omp_cpu_flags    $omp_src -o $EXEC"
   elif [ "$option" == "hip" ]; then
-    EXEC=hip-stream
-    rm -f $EXEC
-    echo  $AOMP/bin/hipcc $hip_flags $hip_src $std -o $EXEC
-    $AOMPHIP/bin/hipcc $hip_flags $hip_src $std -o $EXEC
-    if [ $? -ne 1 ]; then
-      ./$EXEC 2>&1 | tee -a results.txt
+    if [ ! -f $AOMPHIP/bin/hipcc ] ; then 
+      AOMPHIP="$AOMPHIP/.."
+      if [ ! -f $AOMPHIP/bin/hipcc ] ; then 
+        echo "ERROR: $AOMPHIP/bin/hipcc not found"
+        exit 1
+      fi
     fi
+    compile_cmd="$AOMPHIP/bin/hipcc $std $hip_flags $hip_src -o $EXEC"
   else
     echo "ERROR: Option not recognized: $option."
     exit 1
+  fi
+  echo $compile_cmd | tee -a results.txt
+  $compile_cmd
+  if [ $? -ne 1 ]; then
+     echo ./$EXEC -n $BABELSTREAM_REPEATS | tee -a results.txt
+     ./$EXEC -n 10 2>&1 | tee -a results.txt
   fi
 done
 cd $curdir
