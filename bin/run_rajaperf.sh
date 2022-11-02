61d8907919251b0c9bfb1820fe6a85f3c5528d24
Thorsten Blass <Thorsten.Blass@amd.com>
2022-11-02 12:20:03 +0000

    - adjustmends to build main branch. The developer branch is to unstable.

--- bin/run_rajaperf.sh
+++ bin/run_rajaperf.sh
@@ -60,10 +60,10 @@ build_targets="hip openmp"
 if [ "$2" == "build" ]; then
   # Begin configuration
   pushd $AOMP_REPOS_TEST/RAJAPerf
-  git reset --hard 43b8ad43
-  git submodule update -f --recursive
+  #git reset --hard 43b8ad43
+  #git submodule update -f --recursive
   # Apply patches
-  patchrepo $AOMP_REPOS_TEST/RAJAPerf/tpl/RAJA
+  #patchrepo $AOMP_REPOS_TEST/RAJAPerf/tpl/RAJA
   cd $AOMP_REPOS_TEST/RAJAPerf
   rm -rf build_${BUILD_SUFFIX}
   mkdir build_${BUILD_SUFFIX}
@@ -94,28 +94,45 @@ if [ "$2" == "build" ]; then
       -DCMAKE_BUILD_TYPE=Release \
       -DCMAKE_CXX_COMPILER=${AOMP}/bin/clang++ \
       -DENABLE_CUDA=Off \
+      -DENABLE_HIP=Off \
       -DENABLE_OPENMP=On \
       -DENABLE_TARGET_OPENMP=On \
       -DCMAKE_CXX_FLAGS="-DENABLE_TARGET_OPENMP" \
       -DROCM_ARCH=gfx90a \
-      -DOpenMP_CXX_FLAGS="-fopenmp;-fopenmp-targets=amdgcn-amd-amdhsa;-Xopenmp-target=amdgcn-amd-amdhsa;-march=$AOMP_GPU" \
+      -DOpenMP_CXX_FLAGS="-fopenmp=libomp;-fopenmp-targets=amdgcn-amd-amdhsa;-Xopenmp-target=amdgcn-amd-amdhsa;-march=$AOMP_GPU" \
       -DENABLE_ALL_WARNINGS=Off \
       -DCMAKE_INSTALL_PREFIX=../install_${BUILD_SUFFIX} \
-      -DENABLE_TESTS=On \
+      -DENABLE_TESTS=Off \
+      "$@" \
+      ..
+
+    $AOMP_CMAKE \
+      -DCMAKE_BUILD_TYPE=Release \
+      -DCMAKE_CXX_COMPILER=${AOMP}/bin/clang++ \
+      -DENABLE_CUDA=Off \
+      -DENABLE_HIP=Off \
+      -DENABLE_OPENMP=On \
+      -DENABLE_TARGET_OPENMP=On \
+      -DCMAKE_CXX_FLAGS="-DENABLE_TARGET_OPENMP" \
+      -DROCM_ARCH=gfx90a \
+      -DOpenMP_CXX_FLAGS="-fopenmp=libomp;-fopenmp-targets=amdgcn-amd-amdhsa;-Xopenmp-target=amdgcn-amd-amdhsa;-march=$AOMP_GPU" \
+      -DENABLE_ALL_WARNINGS=Off \
+      -DCMAKE_INSTALL_PREFIX=../install_${BUILD_SUFFIX} \
+      -DENABLE_TESTS=Off \
       "$@" \
       ..
   else
     echo "Option $2 not supported. Please choose from $build_targets"
     usage
   fi
-  make -j $AOMP_JOB_THREADS
+  make #-j $AOMP_JOB_THREADS
   # Do not continue if build fails
   if [ $? != 0 ]; then
     echo "ERROR: Make returned non-zero, exiting..."
     exit 1
   fi
   # Cleanup patches
-  removepatch $AOMP_REPOS_TEST/RAJAPerf/tpl/RAJA
+  #removepatch $AOMP_REPOS_TEST/RAJAPerf/tpl/RAJA
   popd
   popd
   exit 0
