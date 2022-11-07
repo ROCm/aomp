139dde86c074ffbd561b7fd9148e9107a904463e
David Pagan <dave.pagan@amd.com>
2022-11-07 11:56:31 -0800

    [Smoke] Add initial tests for 'target teams loop' combined directive. (#444)
    Tests for 'target teams loop', 'parallel loop', and 'loop'.

---
+++ test/smoke-fails/loop-dir-2/loop_dir_2.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <omp.h>
+
+int main() {
+  int k;
+  int sum = 0;
+  int fail = 0;
+
+  #pragma omp loop reduction(+:sum)
+  for(k=0; k<10; k++) {
+    sum += k;
+  }
+  if (sum != 45) {
+    fail++;
+    printf("Wrong result for 'loop': sum is %d\n", sum);
+  }
+
+  sum = 0;
+  #pragma omp parallel loop reduction(+:sum)
+  for(k=0; k<10; k++) {
+    sum += k;
+  }
+  if (sum != 45) {
+    fail++;
+    printf("Wrong result for 'parallel loop': sum is %d\n", sum);
+  }
+
+  sum = 0;
+  #pragma omp target teams loop reduction(+:sum)
+  for(k=0; k<10; k++) {
+    sum += k;
+  }
+  if (sum != 45) {
+    fail++;
+    printf("Wrong result for 'target teams loop': sum is %d\n", sum);
+  }
+
+  if (!fail)
+    printf("Success\n");
+
+  return fail;
+}
