634fa4b7ea52e41ff307b67d1a77f3cc396a327c
Jan Patrick Lehr <jplehr@users.noreply.github.com>
2022-11-10 15:02:38 +0100

    [OpenMP] Failing test for implicit write to const variable via map (#446)
    Mapping a const-qualified (global) variable via map(), map(from:) or
    map(tofrom:) in a target region may result in a segmentation fault
    at runtime due to write into read-only memory.
    
    The tests include a case with a class with mutable member. Discussion
    about the potential behavior can be found in github discussion
    https://github.com/OpenMP/spec/issues/2158
    
    Patch to introduce the intended OpenMP clarification is up for
    Phabricator Review: https://reviews.llvm.org/D137649

--- test/smoke-fails/target-map-from-fail-array/map-from-into-const-global-scalar.c
+++ test/smoke-fails/target-map-from-fail-array/map-from-into-const-global-scalar.c
@@ -9,32 +9,61 @@ const int N = 128;
 const int A[128] = {0};
 int M = 1;
 
+const int B[] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
+
 
 int main(int argc, char **argv) {
   int tmp = 0;
   int tmp2 = 0;
 
-#pragma omp target teams distribute parallel for reduction(+:tmp) map(to:N) map(M)
+#if 0
+  for (int i =0; i < N; ++i) {
+    tmp2 += 1;
+  }
+
+
+#pragma omp target teams distribute parallel for reduction(+:tmp) map(N) map(M)
   for (int i = 0; i < N; i++) {
     tmp += 1;
     M = 42;
   }
 
-  fprintf(stderr, "tmp: %u == 128\n", tmp);
+  fprintf(stderr, "tmp: %u == %u\n", tmp, tmp2);
   fprintf(stderr, "M: %u == 42\n", M);
+  tmp2 = tmp = 0;
+  
+#endif
+  for (int i =0; i < N; ++i) {
+    tmp2 += A[i] + 1;
+  }
 
-  tmp = 0;
-
-#pragma omp target teams distribute parallel for reduction(+:tmp) map(to: N) map(A) map(M)
+//#pragma omp target teams distribute parallel for reduction(+:tmp) map(to: N) map(tofrom:M) map(to: A)
+#pragma omp target teams distribute parallel for reduction(+:tmp) map(N) map(tofrom:M) map(A)
   for (int i = 0; i < N; i++) {
-    tmp += A[i] + 1;
+    tmp += B[i%15] + 1;
     M = 42;
   }
 
-  fprintf(stderr, "tmp: %u == 128\n", tmp);
+  fprintf(stderr, "tmp: %u == %u\n", tmp, tmp2);
   fprintf(stderr, "M: %u == 42\n", M);
 
 #if 0
+
+  tmp2 = tmp = 0;
+
+  for (int i = 0; i < 15; ++i) {
+    tmp2 += B[i] + 1;
+  }
+
+#pragma omp target teams distribute parallel for reduction(+:tmp) map(M) map(to: B)
+  for (int i = 0; i < 15; i++) {
+    tmp += B[i] + 1;
+    M = 42;
+  }
+
+  fprintf(stderr, "tmp: %u == %u\n", tmp, tmp2);
+  fprintf(stderr, "M: %u == 42\n", M);
+
 
 #pragma omp target teams distribute parallel for private(tmp) map(N) map(A)
   for (int i = 0; i < N; i++) {
@@ -63,7 +92,7 @@ int main(int argc, char **argv) {
   }
 #endif 
 
-  fprintf(stderr, "Passed\n");
+  //fprintf(stderr, "Passed\n");
 
-  return 0;
+  return tmp-tmp2;
 }
