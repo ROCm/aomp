54b8b07e357d48e16ccef2029b90e679fc4ead7c
Dhruva Chakrabarti <Dhruva.Chakrabarti@amd.com>
2022-11-11 01:04:38 -0600

    Added test for optimized kernels w/ defaultmap clause

--- test/smoke-fails/opt_kerns_clauses/opt_kerns_clauses.c
+++ test/smoke-fails/opt_kerns_clauses/opt_kerns_clauses.c
@@ -3,28 +3,25 @@
 
 int main()
 {
-  int use_device = 1;
   int N = 100000;
+
   double a[N], b[N];
-  double sum1 = 0;
-  int scalar1 = 10;
 
   for (int i=0; i<N; i++) 
     a[i]=i;
 
+  double sum1 = 0;
+
+  int scalar1 = 10;
   
 #pragma omp target teams distribute parallel for \
-  device(0), if(use_device) order(concurrent) \
   defaultmap(firstprivate:scalar) defaultmap(to:aggregate) \
-  default(none) firstprivate(N,scalar1,use_device), shared(b,a) \
   map(from:b)
   for (int k = 0; k< N; k++)
     b[k]=a[k] * scalar1;
   
 #pragma omp target teams distribute parallel for \
-  device(0), if(use_device) order(concurrent) \
   defaultmap(firstprivate:scalar) defaultmap(to:aggregate) \
-  default(none) firstprivate(N,scalar1,use_device), shared(a) \
   map(tofrom:sum1) reduction(+:sum1)
   for (int j = 0; j< N; j=j+1)
     sum1 += a[j] + scalar1;
