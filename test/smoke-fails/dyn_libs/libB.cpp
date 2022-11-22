db7de6e439fcb9f5288ae6e355212a00bbea6fda
Carlo Bertolli <carlo.bertolli@amd.com>
2022-11-22 13:46:45 -0600

    Add test for multiple dyn libs containing OpenMP target regions

---
+++ test/smoke-fails/dyn_libs/libB.cpp
@@ -0,0 +1,9 @@
+#include <cstddef>
+
+#pragma omp requires unified_shared_memory
+
+void set_to_one(double *a, size_t n) {
+  #pragma omp target teams distribute parallel for map(from:a[:n])
+  for(size_t i = 0; i < n; i++)
+    a[i] = 1.0;
+}
---
+++ test/smoke-fails/dyn_libs/libB.h
@@ -0,0 +1,1 @@
+void set_to_one(double *a, size_t n);
