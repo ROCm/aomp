9ea64363684959e1da8e219561615c426c45e683
Carlo Bertolli <Carlo.Bertolli@amd.com>
2023-02-14 13:05:13 -0500

    Add checks for both fine and coarse grain

--- test/usm/implicit_maps/implicit_maps.cpp
+++ test/usm/implicit_maps/implicit_maps.cpp
@@ -12,22 +12,34 @@ int main() {
   int A[N], B[N], C[N];
   T t; t.x = 1; t.y = 2; t.z = 3;
   int red = 0;
-  
+
   for(int i = 0; i < N; i++) {
     B[i] = i;
     C[i] = i+3;
   }
-  
+
+  // CHECK-COARSE: tgt_rtl_set_coarse_grain_mem_region
+  // CHECK-COARSE: tgt_rtl_set_coarse_grain_mem_region
+  // CHECK-COARSE: tgt_rtl_set_coarse_grain_mem_region
+  // CHECK-COARSE: tgt_rtl_set_coarse_grain_mem_region
+  // CHECK-FINE-NOT: tgt_rtl_set_coarse_grain_mem_region
+  // CHECK-FINE-NOT: tgt_rtl_set_coarse_grain_mem_region
+  // CHECK-FINE-NOT: tgt_rtl_set_coarse_grain_mem_region
+  // CHECK-FINE-NOT: tgt_rtl_set_coarse_grain_mem_region
+  // CHECK: tgt_rtl_launch_kernel
   #pragma omp target teams distribute parallel for
   for(int i = 0; i < N; i++) {
     A[i] = B[i] + C[i];
     if (i == 0) t.x += t.y+t.z;
   }
 
+  // CHECK-COARSE: tgt_rtl_set_coarse_grain_mem_region
+  // CHECK-FINE-NOT: tgt_rtl_set_coarse_grain_mem_region
+  // CHECK: tgt_rtl_launch_kernel
   #pragma omp target teams distribute parallel for reduction(+:red)
   for(int i = 0; i < N; i++)
     red += i;
-  
+
   int err = 0;
   if (red != ((N-1)*N/2)) {
     err++;
@@ -43,6 +55,6 @@ int main() {
       printf("err at %d: got %d, expected %d\n", i, A[i], B[i]+C[i]);
       if (err > 10) return err;
     }
-  
+
   return err;
 }
