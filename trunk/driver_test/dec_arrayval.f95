4000582e0bd9f25e5e200c999dc19267d28d6b6d
gregrodgers <gregory.rodgers@amd.com>
2023-02-28 14:37:43 -0500

    Fixed two stupid fortran bugs, the array should be 4-byte INT, not long, AND fortran index starts from 1

--- trunk/driver_test/dec_arrayval.f95
+++ trunk/driver_test/dec_arrayval.f95
@@ -2,7 +2,8 @@
 !omp declare target
 subroutine dec_arrayval(i, beta) bind(C)
         use ISO_C_BINDING
-        integer (C_LONG), dimension(*), intent(out) :: beta
+        integer (C_INT), dimension(*), intent(out) :: beta
         integer (C_INT), value :: i
-        beta(i) = beta(i) - 1
+        beta(i+1) = beta(i+1) - 1
 end subroutine
+!omp end declare target
