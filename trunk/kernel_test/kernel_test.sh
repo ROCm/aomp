0a6b3b0251be296aca39c3dfadb38ff1a9384e7a
gregrodgers <gregory.rodgers@amd.com>
2023-03-07 14:33:30 -0500

    added testing of main.f95 which should be similar to offloading with main.c

--- trunk/kernel_test/kernel_test.sh
+++ trunk/kernel_test/kernel_test.sh
@@ -1,19 +1,14 @@
 #!/bin/bash
 #
-# driver_test.sh : create two heterogeneous objects and link to main c code. 
-#                  to demonstrate offloading driver. 
+# kernel_test.sh : Compare c and fortran versions of write_index
 #
 # Three environment variables are used by this script:
 #    TRUNK:  Location of LLVM trunk installation, default is $HOME/rocm/trunk
-#    OARCH:  Offload architecture, default is sm_70 
-#    USE_FLANG: Use flang-new, default is NO because flang offload driver still broken
+#    OFFLOAD: MANDATORY OR DISABLED
+#    OARCH:  Offload architecture, sm_70, gfx908, etc
 #
-# Example: To use clang to avoid flang-new lane function test , run:
-#   USE_FLANG=NO ./driver_test.sh
-
 # Set environment variable defaults here:
 TRUNK=${TRUNK:-$HOME/rocm/trunk}
-USE_FLANG=${USE_FLANG:-YES}
 OFFLOAD=${OFFLOAD:-MANDATORY} # Also use disabled
 
 if [ ! -f $TRUNK/bin/amdgpu-arch ] ; then
@@ -29,18 +24,19 @@ _llvm_bin_dir=$TRUNK/bin
 #extra_args="-v -save-temps"
 extra_args="-save-temps"
 
-main_c_binary="main_in_c"
-[ -f $main_c_binary ] && rm $main_c_binary
-compile_main_cmd="$_llvm_bin_dir/clang -save-temps -fopenmp --offload-arch=$OARCH  main.c -o $main_c_binary"
+tmpc="tmpc"
+rm -rf $tmpc ; mkdir -p $tmpc ; cd $tmpc
+[ -f main_in_c ] && rm main_in_c
+compile_main_cmd="$_llvm_bin_dir/clang -save-temps -fopenmp --offload-arch=$OARCH  ../main.c -o main_in_c"
 echo
 echo $compile_main_cmd
 $compile_main_cmd
-echo OMP_TARGET_OFFLOAD=$OFFLOAD ./$main_c_binary
-OMP_TARGET_OFFLOAD=$OFFLOAD ./$main_c_binary
+echo OMP_TARGET_OFFLOAD=$OFFLOAD ./main_in_c
+OMP_TARGET_OFFLOAD=$OFFLOAD ./main_in_c
 rc=$?
-echo "RETURN CODE IS: $rc"
+echo "C RETURN CODE IS: $rc"
+
 echo "CONVERTING temp bc files to ll.  See files host_c.ll, device_c.ll"
-#   Eventually host_f.ll device_f.ll
 $TRUNK/bin/llvm-dis main-host-x86_64-unknown-linux-gnu.bc -o host_c.ll
 $TRUNK/bin/llvm-dis main-openmp-amdgcn-amd-amdhsa-gfx908.tmp.bc -o device_c.ll
 echo "-----------------------------------------------------"
@@ -56,3 +52,41 @@ echo
 echo "-----------------------------------------------------"
 echo
 
+cd ..
+tmpf="tmpf"
+rm -rf $tmpf ; mkdir -p $tmpf ; cd $tmpf
+[ -f main_in_f ] && rm main_in_f
+compile_main_f_cmd="$_llvm_bin_dir/flang-new -save-temps -fopenmp --offload-arch=$OARCH ../main.f95 -o main_in_f"
+#compile_main_f_cmd="$_llvm_bin_dir/flang-new -save-temps -fopenmp ../main.f95 -o main_in_f"
+echo
+echo $compile_main_f_cmd
+$compile_main_f_cmd
+if [ -f main_in_f ] ; then 
+   echo
+   echo OMP_TARGET_OFFLOAD=$OFFLOAD ./main_in_f
+   OMP_TARGET_OFFLOAD=$OFFLOAD ./main_in_f
+   rc=$?
+   echo "FORTRAN RETURN CODE IS: $rc"
+else
+   echo "COMPILE FAILED, SKIPPING EXECUTION"
+fi
+echo "CONVERTING temp bc files to ll"
+if [ -f main.bc ] ; then  
+   $TRUNK/bin/llvm-dis main.bc -o host_no_offload.ll
+fi
+if [ -f main-host-x86_64-unknown-linux-gnu.bc ] ; then 
+   $TRUNK/bin/llvm-dis main-host-x86_64-unknown-linux-gnu.bc -o host_f.ll
+   echo "-----------------------------------------------------"
+   echo "===> HOST function defs and calls in host_f.ll"
+   echo
+   grep "define\|call" host_f.ll | grep -v requires | tee host_calls.txt
+fi
+if [ -f main-openmp-amdgcn-amd-amdhsa-gfx908.tmp.bc ] ; then 
+   $TRUNK/bin/llvm-dis main-openmp-amdgcn-amd-amdhsa-gfx908.tmp.bc -o device_f.ll
+   echo "-----------------------------------------------------"
+   echo "===> DEVICE function defs and calls in device_f.ll"
+   echo
+   grep "define\|call" device_f.ll || tee device_calls.txt
+fi
+echo "-----------------------------------------------------"
+cd ..
