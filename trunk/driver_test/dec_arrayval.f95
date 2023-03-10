24b81225f17a2cedc8a87fba83a9cf680c8fca38
gregrodgers <gregory.rodgers@amd.com>
2023-03-10 09:21:29 -0500

    correct fortran syntax for declare target

--- trunk/driver_test/dec_arrayval.f95
+++ trunk/driver_test/dec_arrayval.f95
@@ -1,9 +1,8 @@
 !  interface
-!omp declare target
 subroutine dec_arrayval(i, beta) bind(C)
+!$omp declare target
         use ISO_C_BINDING
         integer (C_INT), dimension(*), intent(out) :: beta
         integer (C_INT), value :: i
         beta(i+1) = beta(i+1) - 1
 end subroutine
-!omp end declare target
