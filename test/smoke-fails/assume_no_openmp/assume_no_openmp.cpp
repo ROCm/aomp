da5fbc270dd685d2ab032fd8e1fde72e620d1245
Carlo Bertolli <carlo.bertolli@amd.com>
2023-03-16 16:34:10 -0500

    Fix assume example to something that can be parsed.

--- test/smoke-fails/assume_no_openmp/assume_no_openmp.cpp
+++ test/smoke-fails/assume_no_openmp/assume_no_openmp.cpp
@@ -1,9 +1,11 @@
-
+#include<cstdio>
 
 #pragma omp begin declare target
-void foo(double *a, int i) {
+#pragma omp begin assumes no_openmp
+void foo(int *a, int i) {
   a[i] = i;
 }
+#pragma opm end assumes
 #pragma omp end declare target
 
 #define N 100000
@@ -13,10 +15,7 @@ int main() {
 
   #pragma omp target teams distribute parallel for map(from: a[:N])
   for(int i = 0; i < N; i++) {
-    #pragma omp assume no_openmp
-    {
     foo(a,i);
-    }
   }
 
   int err = 0;
