2fdb6631ebea79b0f755ced50d2fb66ff110c68f
gregrodgers <gregory.rodgers@amd.com>
2023-04-21 09:44:54 -0400

    correct method to silence gpurun

--- bin/run_babelstream.sh
+++ bin/run_babelstream.sh
@@ -52,7 +52,7 @@ omp_target_flags="-O3 -fopenmp -fopenmp-targets=$TRIPLE -Xopenmp-target=$TRIPLE 
 #  for LLVM 16 or higher
 LLVM_VERSION=`$AOMP/bin/clang++ --version | grep version | cut -d" " -f 3 | cut -d"." -f1`
 if [ "$LLVM_VERSION" == "version" ]  ; then
-  # If 3rd  arg is version, must be vendor cmppiler, so get version from 4th field.
+  # If 3rd  arg is version, must be aomp or rocm compiler, so get version from 4th field.
   LLVM_VERSION=`$AOMP/bin/clang++ --version | grep version | cut -d" " -f 4 | cut -d"." -f1`
   special_aso_flags="-fopenmp-gpu-threads-per-team=1024 -fopenmp-target-fast"
 else
@@ -60,8 +60,12 @@ else
   special_aso_flags=""
 fi
 echo "LLVM VERSION IS $LLVM_VERSION"
+_SILENT=""
+# for old versions of gpurun without -s flag, silence gpurun with GPURUN_VERBOSE
+export GPURUN_VERBOSE=0
 if [[ $LLVM_VERSION -ge 16 ]] ; then
   omp_fast_flags="$special_aso_flags $omp_target_flags"
+  _SILENT="-s"
 else
   if [[ $LLVM_VERSION -ge 15 ]] ; then
     omp_fast_flags="-fopenmp-target-ignore-env-vars -fopenmp-assume-no-thread-state -fopenmp-target-new-runtime $omp_target_flags"
@@ -156,19 +160,19 @@ for option in $RUN_OPTIONS; do
     set -o pipefail
     if [ -f $AOMP/bin/gpurun ] ; then
       if [ "$option" == "omp-usm" ]; then
-         echo HSA_XNACK=1 $AOMP/bin/gpurun -s ./$EXEC -n $BABELSTREAM_REPEATS | tee -a results.txt
-         HSA_XNACK=1 $AOMP/bin/gpurun -s ./$EXEC -n $BABELSTREAM_REPEATS 2>&1 | tee -a results.txt
+         echo HSA_XNACK=1 $AOMP/bin/gpurun $_SILENT ./$EXEC -n $BABELSTREAM_REPEATS | tee -a results.txt
+         HSA_XNACK=1 $AOMP/bin/gpurun $_SILENT ./$EXEC -n $BABELSTREAM_REPEATS 2>&1 | tee -a results.txt
       elif [ "$option" == "hip-um" ]; then
-         echo HSA_XNACK=1 HIP_UM=1 $AOMP/bin/gpurun -s ./$EXEC -n $BABELSTREAM_REPEATS | tee -a results.txt
-         HSA_XNACK=1 HIP_UM=1 $AOMP/bin/gpurun -s ./$EXEC -n $BABELSTREAM_REPEATS 2>&1 | tee -a results.txt
+         echo HSA_XNACK=1 HIP_UM=1 $AOMP/bin/gpurun $_SILENT ./$EXEC -n $BABELSTREAM_REPEATS | tee -a results.txt
+         HSA_XNACK=1 HIP_UM=1 $AOMP/bin/gpurun $_SILENT ./$EXEC -n $BABELSTREAM_REPEATS 2>&1 | tee -a results.txt
       elif [ "$option" == "omp-fast" ]; then
          echo LIBOMPTARGET_AMDGPU_KERNEL_BUSYWAIT=1000000 LIBOMPTARGET_AMDGPU_DATA_BUSYWAIT=3000000 \
-                $AOMP/bin/gpurun -s ./$EXEC -n $BABELSTREAM_REPEATS 2>&1 | tee -a results.txt
+                $AOMP/bin/gpurun $_SILENT ./$EXEC -n $BABELSTREAM_REPEATS 2>&1 | tee -a results.txt
          LIBOMPTARGET_AMDGPU_KERNEL_BUSYWAIT=1000000 LIBOMPTARGET_AMDGPU_DATA_BUSYWAIT=3000000 \
-                $AOMP/bin/gpurun -s ./$EXEC -n $BABELSTREAM_REPEATS 2>&1 | tee -a results.txt
+                $AOMP/bin/gpurun $_SILENT ./$EXEC -n $BABELSTREAM_REPEATS 2>&1 | tee -a results.txt
       else
-         echo $AOMP/bin/gpurun -s ./$EXEC -n $BABELSTREAM_REPEATS | tee -a results.txt
-         $AOMP/bin/gpurun -s ./$EXEC -n $BABELSTREAM_REPEATS 2>&1 | tee -a results.txt
+         echo $AOMP/bin/gpurun $_SILENT ./$EXEC -n $BABELSTREAM_REPEATS | tee -a results.txt
+         $AOMP/bin/gpurun $_SILENT ./$EXEC -n $BABELSTREAM_REPEATS 2>&1 | tee -a results.txt
       fi
       if [ $? -ne 0 ]; then
         runtime_error=1
