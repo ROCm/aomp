7f1330797a547f2657d8016955ecb7895740f186
JP Lehr <JanPatrick.Lehr@amd.com>
2023-05-04 10:54:58 -0400

    More flexibility in the script

--- bin/run_miniQMCPack.sh
+++ bin/run_miniQMCPack.sh
@@ -11,7 +11,11 @@ thisdir=`dirname $realpath`
 . $thisdir/aomp_common_vars
 # --- end standard header ----
 
-ROCM_INSTALL_PATH=/opt/rocm-5.3.0
+# Default ROCm installation
+: ${ROCM_INSTALL_PATH:=/opt/rocm-5.3.0}
+
+# Control how many OpenMP threads are used by MiniQMCPack
+: ${MQMC_OMP_THREADS_TO_USE:=64}
 
 export PATH=$AOMP/bin:$PATH
 #export PATH=/home/janplehr/rocm/trunk/bin:$PATH
@@ -30,22 +34,24 @@ export rocsolver_DIR=${ROCM_INSTALL_PATH}/lib/cmake/rocsolver/
 : ${MQMCPACK_BUILD_DIR:=${MQMCPACK_BUILD_PREFIX}/build_aomp_clang}
 # Path to the miniqmc source directory
 : ${MQMC_SOURCE_DIR:=.}
+# how many threads should be used for building miniqmc
+: ${MQMC_NUM_BUILD_PROCS:=32}
 
 rm -rf ${MQMCPACK_BUILD_DIR}
-CMAKE_PREFIX_PATH=${ROCM_INSTALL_PATH}/lib/cmake/ cmake -B ${MQMCPACK_BUILD_DIR} -S ${MQMC_SOURCE_DIR} -DCMAKE_CXX_COMPILER=clang++ -DENABLE_OFFLOAD=ON -DQMC_ENABLE_ROCM=ON -DCMAKE_CXX_FLAGS='-g -fopenmp-target-fast' -DAMDGPU_DISABLE_HOST_DEVMEM=ON -DCMAKE_VERBOSE_MAKEFILE=ON
+CMAKE_PREFIX_PATH=${ROCM_INSTALL_PATH}/lib/cmake/ cmake -B ${MQMCPACK_BUILD_DIR} -S ${MQMC_SOURCE_DIR} -DCMAKE_CXX_COMPILER=clang++ -DENABLE_OFFLOAD=ON -DQMC_ENABLE_ROCM=ON -DCMAKE_CXX_FLAGS='' -DAMDGPU_DISABLE_HOST_DEVMEM=ON -DCMAKE_VERBOSE_MAKEFILE=ON
 #CMAKE_PREFIX_PATH=${ROCM_INSTALL_PATH}/lib/cmake/ cmake -B ${MQMCPACK_BUILD_DIR} -S ${MQMC_SOURCE_DIR} -DCMAKE_CXX_COMPILER=clang++ -DENABLE_OFFLOAD=ON -DQMC_ENABLE_ROCM=ON -DAMDGPU_DISABLE_HOST_DEVMEM=OFF -DCMAKE_VERBOSE_MAKEFILE=ON
 # -DCLANGRT_BUILTINS=/home/janplehr/rocm/trunk/lib/clang/17/lib/x86_64-unknown-linux-gnu/libclang_rt.builtins.a
 
-cmake --build ${MQMCPACK_BUILD_DIR}  --clean-first -j 32
+cmake --build ${MQMCPACK_BUILD_DIR}  --clean-first -j ${MQMC_NUM_BUILD_PROCS}
 
 echo "Running Tests"
-echo "OMP_NUM_THREADS=64 ${MQMCPACK_BUILD_DIR}/bin/check_spo_batched_reduction -n 10"
-LIBOMPTARGET_AMDGPU_KERNEL_BUSYWAIT=2000000 LIBOMPTARGET_AMDGPU_DATA_BUSYWAIT=2000000 OMP_NUM_THREADS=64 ${MQMCPACK_BUILD_DIR}/bin/check_spo_batched_reduction -n 10
+echo "OMP_NUM_THREADS=${MQMC_OMP_THREADS_TO_USE} ${MQMCPACK_BUILD_DIR}/bin/check_spo_batched_reduction -n 10"
+OMP_NUM_THREADS=${MQMC_OMP_THREADS_TO_USE} ${MQMCPACK_BUILD_DIR}/bin/check_spo_batched_reduction -n 10
 
 echo ""
-echo "OMP_NUM_THREADS=64 ${MQMCPACK_BUILD_DIR}/bin/miniqmc"
-LIBOMPTARGET_AMDGPU_KERNEL_BUSYWAIT=2000000 LIBOMPTARGET_AMDGPU_DATA_BUSYWAIT=2000000 OMP_NUM_THREADS=64 ${MQMCPACK_BUILD_DIR}/bin/miniqmc -v
+echo "OMP_NUM_THREADS=${MQMC_OMP_THREADS_TO_USE} ${MQMCPACK_BUILD_DIR}/bin/miniqmc"
+OMP_NUM_THREADS=${MQMC_OMP_THREADS_TO_USE} ${MQMCPACK_BUILD_DIR}/bin/miniqmc -v
 
 echo ""
-echo "OMP_NUM_THREADS=64 ${MQMCPACK_BUILD_DIR}/bin/check_spo -n 10"
-LIBOMPTARGET_AMDGPU_KERNEL_BUSYWAIT=2000000 LIBOMPTARGET_AMDGPU_DATA_BUSYWAIT=2000000 OMP_NUM_THREADS=64 ${MQMCPACK_BUILD_DIR}/bin/check_spo -n 10 -v
+echo "OMP_NUM_THREADS=${MQMC_OMP_THREADS_TO_USE} ${MQMCPACK_BUILD_DIR}/bin/check_spo -n 10"
+OMP_NUM_THREADS=${MQMC_OMP_THREADS_TO_USE} ${MQMCPACK_BUILD_DIR}/bin/check_spo -n 10 -v
