f11f3100d1310dcdd8ec11b0ea6aabaa4cd2b277
JP Lehr <JanPatrick.Lehr@amd.com>
2023-05-04 16:08:29 -0400

    Adds documentation and git clone step

--- bin/run_miniQMCPack.sh
+++ bin/run_miniQMCPack.sh
@@ -1,9 +1,14 @@
 #! /usr/bin/env bash
 
+# run_miniQMCPack.sh - runs several MiniQMC binaries
+# The user should set at least these two variables:
+# MQMC_BUILD_PREFIX       miniqmc build prefix (default: ~/miniqmc_build)
+# MQMC_SOURCE_DIR         path to miniqmc sources (default: .)
 #
-# run_miniQMCPack.sh - runs several MiniQMCPack binaries
-#
-#
+# The user can set different variables to control execution.
+# MQMC_OMP_THREADS_TO_USE how many OpenMP threads should be used (default 64)
+# MQMC_NUM_BUILD_PROCS    how many processes to build miniqmc (default 32)
+# ROCM_INSTALL_PATH       top-level ROCm install directory (default: /opt/rocm-5.3.0)
 
 # --- Start standard header to set AOMP environment variables ----
 realpath=`realpath $0`
@@ -20,6 +25,7 @@ thisdir=`dirname $realpath`
 export PATH=$AOMP/bin:$PATH
 #export PATH=/home/janplehr/rocm/trunk/bin:$PATH
 
+# We export all these paths, so they will be picked-up in the CMake command.
 export hsaruntime64_DIR=${ROCM_INSTALL_PATH}/lib/cmake/hsa-runtime64/
 export hipblas_DIR=${ROCM_INSTALL_PATH}/lib/cmake/hipblas/
 export hip_DIR=${ROCM_INSTALL_PATH}/lib/cmake/hip
@@ -29,18 +35,21 @@ export rocblas_DIR=${ROCM_INSTALL_PATH}/lib/cmake/rocblas/
 export rocsolver_DIR=${ROCM_INSTALL_PATH}/lib/cmake/rocsolver/
 
 # Set the default build prefix, i.e., build-top-level
-: ${MQMCPACK_BUILD_PREFIX:=~/miniqmc_build}
+: ${MQMCPACK_BUILD_PREFIX:=$HOME/miniqmc_build}
 # Set the default build directory name
 : ${MQMCPACK_BUILD_DIR:=${MQMCPACK_BUILD_PREFIX}/build_aomp_clang}
 # Path to the miniqmc source directory
-: ${MQMC_SOURCE_DIR:=.}
+: ${MQMC_SOURCE_DIR:=$HOME/miniqmc_src}
 # how many threads should be used for building miniqmc
 : ${MQMC_NUM_BUILD_PROCS:=32}
 
+
+if [ ! -d $MQMC_SOURCE_DIR ]; then
+  git clone https://github.com/ye-luo/miniqmc $MQMC_SOURCE_DIR
+fi
+
 rm -rf ${MQMCPACK_BUILD_DIR}
-CMAKE_PREFIX_PATH=${ROCM_INSTALL_PATH}/lib/cmake/ cmake -B ${MQMCPACK_BUILD_DIR} -S ${MQMC_SOURCE_DIR} -DCMAKE_CXX_COMPILER=clang++ -DENABLE_OFFLOAD=ON -DQMC_ENABLE_ROCM=ON -DCMAKE_CXX_FLAGS='' -DAMDGPU_DISABLE_HOST_DEVMEM=ON -DCMAKE_VERBOSE_MAKEFILE=ON
-#CMAKE_PREFIX_PATH=${ROCM_INSTALL_PATH}/lib/cmake/ cmake -B ${MQMCPACK_BUILD_DIR} -S ${MQMC_SOURCE_DIR} -DCMAKE_CXX_COMPILER=clang++ -DENABLE_OFFLOAD=ON -DQMC_ENABLE_ROCM=ON -DAMDGPU_DISABLE_HOST_DEVMEM=OFF -DCMAKE_VERBOSE_MAKEFILE=ON
-# -DCLANGRT_BUILTINS=/home/janplehr/rocm/trunk/lib/clang/17/lib/x86_64-unknown-linux-gnu/libclang_rt.builtins.a
+CMAKE_PREFIX_PATH=${ROCM_INSTALL_PATH}/lib/cmake/ cmake -B ${MQMCPACK_BUILD_DIR} -S ${MQMC_SOURCE_DIR} -DCMAKE_CXX_COMPILER=clang++ -DENABLE_OFFLOAD=ON -DQMC_ENABLE_ROCM=ON -DCMAKE_CXX_FLAGS='-fopenmp-assume-no-thread-state' -DAMDGPU_DISABLE_HOST_DEVMEM=ON -DCMAKE_VERBOSE_MAKEFILE=ON
 
 cmake --build ${MQMCPACK_BUILD_DIR}  --clean-first -j ${MQMC_NUM_BUILD_PROCS}
 
