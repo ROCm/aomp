d2a8ef8650e964412e809f1c83f12f69a8e8adb1
Ron Lieberman <ron.lieberman@amd.com>
2023-05-10 06:18:12 -0500

    improve rocm_quick_check.sh

--- bin/rocm_quick_check.sh
+++ bin/rocm_quick_check.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
 
-if [ -e ~/local/openmpi/lib/libmpi.so ]; then exit; fi;
-if [ -e /opt/openmpi-4.1.5/lib/libmpi.so ]; then exit; fi;
-if [ -e /opt/openmpi-4.1.4/lib/libmpi.so ]; then exit; fi;
-if [ -e /usr/local/openmpi/lib/libmpi.so ]; then exit; fi;
-if [ -e /usr/lib/x86_64-linux-gnu/openmpi/lib/libmpi.so ]; then exit; fi;
-if [ -e /usr/lib/openmpi/lib/libmpi.so ]; then exit; fi;
-if [ -e /usr/local/lib/libmpi.so ]; then exit ;fi;
-echo "No mpi found , not good."
-
-if [ -e /usr/bin/python ]; then exit; fi;
-echo "/usr/bin/python is missing"
+if [ -e /usr/bin/python ]; then
+  echo "/usr/bin/python OK"
+else
+  echo "/usr/bin/python is missing FAIL"
+fi
+if [ -e ~/local/openmpi/lib/libmpi.so ]; then
+  echo "~/local/openmpi/lib/libmpi.so OK"
+elif [ -e /opt/openmpi-4.1.5/lib/libmpi.so ]; then
+  echo "/opt/openmpi-4.1.5/lib/libmpi.so OK"
+elif [ -e /opt/openmpi-4.1.4/lib/libmpi.so ]; then
+  echo "/opt/openmpi-4.1.4/lib/libmpi.so  OK"
+elif [ -e /usr/local/openmpi/lib/libmpi.so ]; then
+  echo "/usr/local/openmpi/lib/libmpi.so OK"
+elif [ -e /usr/lib/x86_64-linux-gnu/openmpi/lib/libmpi.so ]; then
+  echo "/usr/lib/x86_64-linux-gnu/openmpi/lib/libmpi.so OK"
+elif [ -e /usr/lib/openmpi/lib/libmpi.so ]; then
+  echo "/usr/lib/openmpi/lib/libmpi.so OK"
+elif [ -e /usr/local/lib/libmpi.so ]; then
+  echo "/usr/local/lib/libmpi.so OK"
+else
+  echo "No mpi found , not good. FAIL"
+fi
