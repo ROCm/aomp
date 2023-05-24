d15ae1f6ea93c705b2a5f9da3d041831ef42b88a
Dhruva Chakrabarti <Dhruva.Chakrabarti@amd.com>
2023-05-24 12:04:05 -0500

    Updated test.

--- test/smoke/iteam-red-1/iteam_red_1.c
+++ test/smoke/iteam-red-1/iteam_red_1.c
@@ -2,13 +2,12 @@
 #include "assert.h"
 #include <unistd.h>
 
-void vmul(int*a, int*b, int*c, int N){
-#pragma omp target map(to: a[0:N],b[0:N]) map(from:c[0:N])
-//#pragma omp teams distribute parallel for collapse(2)
-#pragma omp teams distribute
+void vmul(double*a, double*b, double*c, int N){
+#pragma omp target map(to: a[0:N],b[0:N]) map(from:c[0:N]) 
+#pragma omp teams distribute num_teams(104)
   for(int i=0;i<N;i++) {
-    int sum = 0;
-#pragma omp parallel for reduction(+:sum)
+    double sum = 0;
+#pragma omp parallel for reduction(+:sum) num_threads(256)
     for(int j=0;j<N;j++) {
       sum += a[i]*b[j];
     }
@@ -17,12 +16,12 @@ void vmul(int*a, int*b, int*c, int N){
 }
 
 int main(){
-    const int N = 100;
-    int a[N],b[N],c[N],validate[N];
+    const int N = 10000;
+    double a[N],b[N],c[N],validate[N];
     int flag=-1; // Mark Success
     for(int i=0;i<N;i++){
       a[i]=i+1;
-      int sum = 0;
+      double sum = 0;
       for(int j=0;j<N;j++){
         b[j]=j+2;
         sum += a[i]*b[j];
@@ -36,7 +35,7 @@ int main(){
         if(c[i]!=validate[i]) {
 //          print 1st bad index
             if( flag == -1 ) 
-              printf("First fail: c[%d](%d) != validate[%d](%d)\n",i,c[i],i,validate[i]);
+              printf("First fail: c[%d](%f) != validate[%d](%f)\n",i,c[i],i,validate[i]);
             flag = i;
         }
     }
@@ -44,7 +43,7 @@ int main(){
       printf("Success\n");
         return 0;
     } else {
-        printf("Last fail: c[%d](%d) != validate[%d](%d)\n",flag,c[flag],flag,validate[flag]);
+        printf("Last fail: c[%d](%f) != validate[%d](%f)\n",flag,c[flag],flag,validate[flag]);
         printf("Fail\n");
         return 1;
     }
