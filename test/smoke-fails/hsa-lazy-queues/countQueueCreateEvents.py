89e4903daf41fa0e9143a7e981221c74ba36fe4e
JP Lehr <JanPatrick.Lehr@amd.com>
2023-05-25 17:01:50 -0400

    Fix to pass expected number of occurences

--- test/smoke-fails/hsa-lazy-queues/countQueueCreateEvents.py
+++ test/smoke-fails/hsa-lazy-queues/countQueueCreateEvents.py
@@ -2,13 +2,18 @@
 
 import json
 import sys
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('num_occurences', type=int, help='How many hsa_queue_create events are expected')
 
 filename = 'results.json'
 hsa_queue_create_name = 'hsa_queue_create'
 
-def searchAndCount(TheJSON) -> None:
+def searchAndCount(TheJSON, args) -> None:
   TraceEvents = TheJSON['traceEvents']
 
+  ExpectedNumOccurs = args.num_occurences
   NumOccurs = 0
   for e in TraceEvents:
     if len(e) == 0:
@@ -17,10 +22,11 @@ def searchAndCount(TheJSON) -> None:
     if e['name'] == hsa_queue_create_name:
       NumOccurs += 1
 
-  if NumOccurs < 2:
+  if NumOccurs != ExpectedNumOccurs:
       sys.exit(1)
 
 if __name__ == '__main__':
+  args = parser.parse_args()
   with open(filename, "r") as f:
     J = json.load(f)
-    searchAndCount(J)
+    searchAndCount(J, args)
