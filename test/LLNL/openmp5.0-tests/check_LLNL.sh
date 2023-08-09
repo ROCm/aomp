7851a771d151a5f5fdcb8ee2dc5058eeab0822e6
Ron Lieberman <ron.lieberman@amd.com>
2023-08-09 09:12:31 -0500

    Add check to only add requires once, and insert a timeout

--- test/LLNL/openmp5.0-tests/check_LLNL.sh
+++ test/LLNL/openmp5.0-tests/check_LLNL.sh
@@ -19,8 +19,11 @@ clean
 
 lspci | grep -s VMware
 if [ $? -ne 0 ] ; then
-  echo test_requires_unified_shared_memory.cxx >> test_list
-  echo test_requires_unified_shared_memory.cxx added
+  grep -s test_requires_unified_shared_memory.cxx test_list
+  if [ $? -ne 0 ] ; then
+    echo test_requires_unified_shared_memory.cxx >> test_list
+    echo test_requires_unified_shared_memory.cxx added
+  fi
 fi
 AOMP=${AOMP:-/usr/lib/aomp}
 AOMP_GPU=${AOMP_GPU:-`$AOMP/bin/mygpu`}
@@ -36,7 +39,7 @@ if [ "$1" == "log" ]; then
     log="LLNL.run.log.$date"
   fi
   echo "Log enabled: $log"
- ./test.py $AOMP $AOMP_GPU 2>&1 | tee $log
+ timeout -s 9 120 ./test.py $AOMP $AOMP_GPU 2>&1 | tee $log
 else
- ./test.py $AOMP $AOMP_GPU
+ timeout -s 9 120 ./test.py $AOMP $AOMP_GPU
 fi
