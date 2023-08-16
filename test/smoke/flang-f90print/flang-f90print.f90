127950b9d86a9a9c7397c8a64daa37792f86126c
Saiyedul Islam <Saiyedul.Islam@amd.com>
2023-08-16 16:09:24 +0530

    Fix testing for target region printing
    `make run` now checks if the desired message `f90print inside target region` has been printed or not.

--- test/smoke/flang-f90print/flang-f90print.f90
+++ test/smoke/flang-f90print/flang-f90print.f90
@@ -3,7 +3,8 @@ program test
     use f90deviceio
     write(0, *) "Before target "
     !$omp target
-       call f90print("In tagret ")
+       call f90print("f90print inside target region")
+       !if the above line is not printed then the test has failed
     !$omp end target
     write(0,*) "After target "
 end program test
