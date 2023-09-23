17ca139831d89fb3effdacdf7d24371dc228d729
Ron Lieberman <ron.lieberman@amd.com>
2023-09-23 07:22:39 -0500

    Disable MPI check for openmpapps

--- bin/run_openmpapps.sh
+++ bin/run_openmpapps.sh
@@ -13,13 +13,13 @@ thisdir=`dirname $realpath`
 AOMP=${AOMP:-/usr/lib/aomp}
 FLANG=${FLANG:-flang}
 
-export OMPIDIR=$AOMP_SUPP/openmpi
-if [ -d "$OMPIDIR" ] ; then
-  echo "OpenMPI-4.1.1 exists."
-else
-  echo "$OMPIDIR does not exist. Install openmpi using ./build_supp.sh ompi and try again."
-  exit
-fi
+#export OMPIDIR=$AOMP_SUPP/openmpi
+#if [ -d "$OMPIDIR" ] ; then
+#  echo "OpenMPI-4.1.1 exists."
+#else
+#  echo "$OMPIDIR does not exist. Install openmpi using ./build_supp.sh ompi and try again."
+#  exit
+#fi
 
 # Use function to set and test AOMP_GPU
 setaompgpu
