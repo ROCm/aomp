533c1c3c2838911d33d4e76d14e5526a52634efa
Dan Palermo <dan.palermo@amd.com>
2023-11-13 15:41:44 -0600

    [smoke] Don't rerun tests that hit timeout
        - Capture/examine run status in a TEST_STATUS file in test directory
        - Makefile exit status is otherwise 2 on error so it is not possible
          to detect timeout by looking at the exit status of the make command

--- test/smoke/check_smoke.sh
+++ test/smoke/check_smoke.sh
@@ -246,6 +246,8 @@ if [ "$AOMP_PARALLEL_SMOKE" == 1 ]; then
         sem --jobs 4 --id def_sem -u 'make check > /dev/null 2>&1'
       fi
       #---
+      test_status=`cat TEST_STATUS`
+      if [ "$test_status" == "124" ]; then break; fi       # don't rerun timeouts
       run=$(($run+1))
       done
       #--- End test iteration
@@ -312,6 +314,8 @@ for directory in $SMOKE_DIRS; do
   make
   if [ $? -ne 0 ]; then
     echo "$base: Make Failed" >> ../make-fail.txt
+    test_status=`cat TEST_STATUS`
+    if [ "$test_status" == "124" ]; then break; fi       # don't rerun timeouts
     run=$(($run+1))
     continue
   fi
@@ -329,10 +333,15 @@ for directory in $SMOKE_DIRS; do
     # liba_bundled has an additional Makefile, that may fail on the make check
     if [ $? -ne 0 ] && ( [ $base == 'liba_bundled' ] || [ $base == 'liba_bundled_cmdline' ] ) ; then
       echo "$base: Make Failed" >> ../make-fail.txt
+      test_status=`cat TEST_STATUS`
+      if [ "$test_status" == "124" ]; then break; fi       # don't rerun timeouts
+      break     # don't rerun timeouts
     fi
   fi
   echo ""
   #---
+  test_status=`cat TEST_STATUS`
+  if [ "$test_status" == "124" ]; then break; fi       # don't rerun timeouts
   run=$(($run+1))
   done
   #--- End test iteration
