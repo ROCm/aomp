7287a7e0f5663878d497bce192d396bf7ce02b24
Jan Leyonberg <jan_sjodin@yahoo.com>
2023-12-20 14:37:26 -0500

    Add reduction tests (#766)

---
+++ trunk/reduction_no_teams_test/main.f95
@@ -0,0 +1,43 @@
+program main
+        integer :: hostArray(10000), errors = 0, sum = 0, hostsum = 0
+        do i = 1, 10000
+                hostArray(i) = 0
+        end do
+        call writeIndex(hostArray, 10000)
+        do i = 1, 10000
+                hostsum = hostsum + hostArray(i)
+                if ( hostArray(i) /= i ) then
+                        errors = errors + 1
+                end if
+        end do
+        call computeSum(hostArray, 10000, sum)
+!        print*, "Sum and hostsum and second", sum, hostsum
+        if (hostsum /= sum) then
+           print*, "======= Sum mismatch ======="
+           stop 1
+        end if
+        if ( errors /= 0 ) then
+           print*, "======= Errors found ======="
+           stop 1
+        end if
+        print*, "======= FORTRAN Test passed! ======="
+!       stop 0
+end program main
+subroutine writeIndex(int_array, array_length)
+        integer :: int_array(10000)
+        integer :: array_length
+        do index_ = 1, 10000
+          int_array(index_) = index_
+        end do
+end subroutine writeIndex
+
+subroutine computeSum(int_array, array_length, sum)
+        integer :: int_array(10000)
+        integer :: array_length
+        integer :: sum
+!$omp target parallel do map(to:int_array) map(tofrom:sum) reduction(+:sum)
+        do index_ = 1, 10000
+          sum = sum + int_array(index_)
+        end do
+!$omp end target parallel do
+end subroutine computeSum
