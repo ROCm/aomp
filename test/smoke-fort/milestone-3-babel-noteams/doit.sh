23904d99146b7d82f428e00a04e2af950ed1e8fc
Dan Palermo <dan.palermo@amd.com>
2024-01-10 20:40:05 -0600

    [smoke-fort] Reduce milestone-3-babel-noteams (slow w/o teams)

--- test/smoke-fort/milestone-3-babel-noteams/doit.sh
+++ test/smoke-fort/milestone-3-babel-noteams/doit.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -x
-./milestone-3-babel-noteams -n 4 -s 1000000000
+# skip to avoid timeout (too slow without teams)
+# ./milestone-3-babel-noteams -n 4 -s 1000000000
 ./milestone-3-babel-noteams -n 4 -s 100000000
 ./milestone-3-babel-noteams -n 4
