caf5a5b0d61258c9121224a003a137531525c7ce
Ethan Stewart <ethan.stewart@amd.com>
2024-01-17 17:11:39 -0600

    [smoke] - Add error checking for flang_math.

--- test/smoke/flang_math/flang_math.f90
+++ test/smoke/flang_math/flang_math.f90
@@ -6,9 +6,15 @@ subroutine MATH(a,b)
    !$omp end target 
         end subroutine
 program main
-   REAL*8 :: x,y
+   REAL*8 :: x,y,hostresult
    x = 2.0
    y = 12.0
+   hostresult = cos(y) + sin(y) + sqrt(y) + exp(y) + x**y
    call MATH(x,y)
    print *,x
+   if (x .ne. hostresult) then
+      print *, "Failed: ", x, "!=",hostresult
+      stop 2
+   endif
+   print *, "Passed"
 end program
