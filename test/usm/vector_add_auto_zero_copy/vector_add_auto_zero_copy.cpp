f31fe3451d8f725407bf94e4c43f70f9930e4da1
Carlo Bertolli <carlo.bertolli@amd.com>
2024-01-26 11:23:43 -0600

    Add simple usm and auto zero-copy tests.

---
+++ test/usm/vector_add_auto_zero_copy/vector_add_auto_zero_copy.cpp
@@ -0,0 +1,33 @@
+#include <cstdio>
+
+int main()
+{
+  const size_t n = 1024*100;
+
+  double *a = new double[n];
+  double *b = new double[n];
+  double *c = new double[n];
+
+  // initialize
+  for(size_t i = 0; i < n; i++) {
+    b[i] = i;
+    c[i] = 2*i;
+  }
+
+  #pragma omp target teams loop map(from:a[:n]) map(to:b[:n],c[:n])
+  for(size_t i = 0; i < n; i++) {
+    a[i] = b[i] + c[i];
+  }
+
+  int err = 0;
+  for(size_t i = 0; i < n; i++)
+    if (a[i] != b[i]+c[i]) {
+      printf("Error at %zu: got %lf, expected %lf\n", i, a[i], b[i]+c[i]);
+      if (err > 10) return err;
+    }
+
+  if (!err)
+    printf("Success\n");
+
+  return err;
+}
