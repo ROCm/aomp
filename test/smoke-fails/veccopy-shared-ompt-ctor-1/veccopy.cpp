242a2f549f6ec7aa23bb251534124a9fbd822066
Dhruva Chakrabarti <Dhruva.Chakrabarti@amd.com>
2024-02-03 00:42:30 -0600

    Added 4 tests with OpenMP directives in a global constructor.
    (1) smoke-limbo/veccopy-ctor-1: OpenMP directive in a global constructor
    but not built as a shared library. This currently works.
    (2) smoke-limbo/veccopy-ompt-ctor-1: Enabled OMPT for an OpenMP directive
    in a global constructor but not built as a shared library. This
    currently works.
    (3) smoke-fails/veccopy-shared-ctor-1: OpenMP directive in a global
    constructor and built as a shared library. This fails an assert, needs
    further investigation.
    (4) smoke-fails/veccopy-shared-ompt-ctor-1: Enabled OMPT for an OpenMP
    directive in a global constructor and built as a shared library. This
    fails with the same assert as (3).

---
+++ test/smoke-fails/veccopy-shared-ompt-ctor-1/veccopy.cpp
@@ -0,0 +1,49 @@
+/*
+ * Test whether OMPT support works for OpenMP directives in a global constructor
+ * used in a shared library. 
+ * This currently fails with the following assertion.
+ * Needs investigation.
+omptarget.cpp:1617: int target(ident_t *, DeviceTy &, void *, KernelArgsTy &, AsyncInfoTy &): Assertion `TargetTable && "Global data has not been mapped\n"' failed.
+ */
+
+#include <stdio.h>
+#include <omp.h>
+
+extern int status;
+
+int main()
+{
+  int N = 10;
+
+  int a[N];
+  int b[N];
+
+  int i;
+
+  for (i=0; i<N; i++)
+    a[i]=0;
+
+  for (i=0; i<N; i++)
+    b[i]=i;
+
+#pragma omp target parallel for
+  {
+    for (int j = 0; j< N; j++)
+      a[j]=b[j];
+  }
+
+  int rc = 0;
+  for (i=0; i<N; i++)
+    if (a[i] != b[i] ) {
+      rc++;
+      printf ("Wrong varlue: a[%d]=%d\n", i, a[i]);
+    }
+
+  if (!rc && !status)
+    printf("Success\n");
+
+  return rc;
+}
+
+// clang-format off
+/// CHECK: Record Target kernel
