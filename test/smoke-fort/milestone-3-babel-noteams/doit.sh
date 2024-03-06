e19e6b2b478609ff8329d32c6b748b02ec4b006a
dpalermo <dan.palermo@amd.com>
2024-03-06 16:49:43 -0600

    [smoke-fort] Skip running larger memory tests on smaller devices (#854)

--- test/smoke-fort/milestone-3-babel-noteams/doit.sh
+++ test/smoke-fort/milestone-3-babel-noteams/doit.sh
@@ -3,6 +3,5 @@ echo "ZZZZZZZZZZ BabelStream"   # marker for extract-tests
 set -x
 ./milestone-3-babel-noteams -n 4                # 805.3 MB
 ./milestone-3-babel-noteams -n 4 -s 100000000   # 2400.0 MB
-./milestone-3-babel-noteams -n 4 -s 500000000   # 12000.0 MB
-# skip 24GB, too big for smaller 16GB test devices
-# ./milestone-3-babel-noteams -n 4 -s 1000000000  # 24000.0 MB
+./milestone-3-babel-noteams -n 4 -s 200000000   # 4400.0 MB
+# skip larger test cases, very slow without teams
--- test/smoke-fort/milestone-3-babel/doit.sh
+++ test/smoke-fort/milestone-3-babel/doit.sh
@@ -3,6 +3,14 @@ echo "ZZZZZZZZZZ BabelStream"   # marker for extract-tests
 set -x
 ./milestone-3-babel -n 20               # 805.3 MB
 ./milestone-3-babel -n 20 -s 100000000  # 2400.0 MB
+./milestone-3-babel -n 20 -s 200000000  # 4800.0 MB
+if [[ "$AOMP_GPU" =~ ^gfx10.* ]]; then
+    echo "$AOMP_GPU: Skipping remaining larger memory tests"
+    exit 0
+fi
 ./milestone-3-babel -n 20 -s 500000000  # 12000.0 MB
-# skip 24GB, too big for smaller 16GB test devices
-# ./milestone-3-babel -n 20 -s 1000000000 # 24000.0 MB
+if [[ "$AOMP_GPU" =~ ^gfx11.* ]]; then
+    echo "$AOMP_GPU: Skipping remaining larger memory tests"
+    exit 0
+fi
+./milestone-3-babel -n 20 -s 1000000000 # 24000.0 MB
