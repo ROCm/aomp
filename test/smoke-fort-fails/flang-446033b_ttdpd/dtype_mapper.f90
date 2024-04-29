b6def42bb67d2dd87007d27b4dedaac1c671ad56
Dan Palermo <dan.palermo@amd.com>
2024-04-29 11:31:09 -0500

    [smoke-fort] Add derived type mapping test cases for flang-446033

---
+++ test/smoke-fort-fails/flang-446033b_ttdpd/dtype_mapper.f90
@@ -0,0 +1,41 @@
+module dtype
+
+        type :: my_dtype
+                integer :: s, e
+                integer,dimension(:),allocatable :: values
+        end type
+
+end module
+
+program offload_types
+        use dtype
+
+        !$omp declare mapper (my_dtype :: v) map(v, v%values(s:e))
+
+        type(my_dtype),target :: my_instance
+        integer,dimension(:),pointer :: values_ptr
+        integer :: i
+
+        ! $omp requires unified_shared_memory
+
+        allocate(my_instance%values(1024))
+        my_instance%s=1
+        my_instance%e=1024
+
+        values_ptr => my_instance%values
+
+        !$omp target enter data map(to:my_instance)
+
+        !$omp target teams distribute parallel do
+        do i =1,1024
+                my_instance%values(i) = i
+        end do
+
+        !$omp target exit data map(from:my_instance)
+
+        write(*,*) my_instance%values(10)
+
+
+        deallocate(my_instance%values)
+
+end program
