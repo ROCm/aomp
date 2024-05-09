a0e290a13624fa6e1680e3fd55813aaaa9903651
Catherine Moore <catherine.moore@amd.com>
2024-05-09 14:47:47 -0500

    Add OpenMP atomic_vs_reduction example.  Includes testcase, Makefile, and README.

---
+++ examples/openmp/atomic_vs_reduction/README.md
@@ -0,0 +1,7 @@
+This example demonstrates the relative speed of an addition operations using:
+
+1. OpenMP atomic with hint
+2. OpenMP atomic
+3. OpenMP reduction operation
+
+concluding that the reduction operation is superior.
---
+++ examples/openmp/atomic_vs_reduction/atomic_vs_reduction.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <omp.h>
+int main() {
+//
+//  atomic_vs_reduction.c: This test demonstrates how much faster reductions are than atomic operations 
+//
+  int main_rc = 0;
+  int N       = 5001;
+  float expect = (float) (((float)N-1)*(float)N)/2.0;
+
+  float a    = 0.0;
+  double t0 = omp_get_wtime();
+  #pragma omp target teams distribute parallel for map(tofrom:a)
+  for(int ii = 0; ii < N; ++ii) {
+    #pragma omp atomic hint(AMD_fast_fp_atomics)
+    a+=(float)ii;
+  }
+  double t1 = omp_get_wtime()-t0;
+  if (a == expect) {
+    printf("Success atomic with hint (fast FP atomic) sum of %d integers is: %f in \t\t%f secs\n",N,a,t1);
+  } else {
+    printf("FAIL ATOMIC SUM N:%d result: %f != expect: %f \n", N,a,expect);
+    main_rc=1;
+  }
+
+  float casa    = 0.0;
+  double t_cas0 = omp_get_wtime();
+  #pragma omp target teams distribute parallel for map(tofrom:casa)
+  for(int ii = 0; ii < N; ++ii) {
+    #pragma omp atomic
+    casa+=(float)ii;
+  }
+  double t_cas1 = omp_get_wtime()-t_cas0;
+  if (casa == expect) {
+    printf("Success atomic without hint (cas loop) sum of %d integers is: %f in \t\t%f secs\n",N,casa,t_cas1);
+  } else {
+    printf("FAIL ATOMIC SUM N:%d result: %f != expect: %f \n", N,casa,expect);
+    main_rc=1;
+  }
+
+  // Now do the sum as a reduction
+  float ra = 0.0;
+  double t2 = omp_get_wtime();
+  #pragma omp target teams distribute parallel for reduction(+:ra) 
+  for(int ii = 0; ii < N; ++ii) {
+    ra+=(float)ii;
+  }
+  double t3 = omp_get_wtime() - t2;
+
+  if (ra == expect) {
+    printf("Success reduction sum of %d integers is: %f in \t\t\t\t\t%f secs\n",N,ra,t3);
+  } else {
+    printf("FAIL REDUCTION SUM N:%d result: %f != expect: %f \n", N,ra,expect);
+    main_rc=1;
+  }
+  return main_rc;
+}
