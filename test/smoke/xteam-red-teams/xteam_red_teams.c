74012a67755f27e264dc12332fbab42ae11145ad
Dhruva Chakrabarti <Dhruva.Chakrabarti@amd.com>
2024-09-20 14:20:48 -0500

    Use a lower number of teams in num_teams clause.

--- test/smoke/xteam-red-teams/xteam_red_teams.c
+++ test/smoke/xteam-red-teams/xteam_red_teams.c
@@ -10,32 +10,22 @@ int main()
   for (int i=0; i<N; i++)
     a[i]=i;
 
-  double sum1, sum2, sum3, sum4;
-  sum1 = sum2 = sum3 = sum4 = 0;
+  double sum1, sum2;
+  sum1 = sum2 = 0;
   
 #pragma omp target teams distribute parallel for reduction(+:sum1) num_teams(1)
   for (int j = 0; j< N; j=j+1)
     sum1 += a[j];
 
-#pragma omp target teams distribute parallel for reduction(+:sum2) num_teams(50)
+#pragma omp target teams distribute parallel for reduction(+:sum2) num_teams(5)
   for (int j = 0; j< N; j=j+1)
     sum2 += a[j];
 
-#pragma omp target teams distribute parallel for reduction(+:sum3) num_teams(40)
-  for (int j = 0; j< N; j=j+1)
-    sum3 += a[j];
-
-#pragma omp target teams distribute parallel for reduction(+:sum4) num_teams(10)
-  for (int j = 0; j< N; j=j+1)
-    sum4 += a[j];
-
-  printf("%f %f %f %f\n", sum1, sum2, sum3, sum4);
-  
+  printf("%f %f\n", sum1, sum2);
+ 
   int rc =
     (sum1 != 499999500000) ||
-    (sum2 != 499999500000) ||
-    (sum3 != 499999500000) ||
-    (sum4 != 499999500000);
+    (sum2 != 499999500000);
 
   if (!rc)
     printf("Success\n");
@@ -44,7 +34,5 @@ int main()
 }
 
 /// CHECK: DEVID:[[S:[ ]*]][[DEVID:[0-9]+]] SGN:8 ConstWGSize:1024 args: 7 teamsXthrds:(   1X1024) 
-/// CHECK: DEVID:[[S:[ ]*]][[DEVID:[0-9]+]] SGN:8 ConstWGSize:1024 args: 7 teamsXthrds:( 50X1024)
-/// CHECK: DEVID:[[S:[ ]*]][[DEVID:[0-9]+]] SGN:8 ConstWGSize:1024 args: 7 teamsXthrds:( 40X1024)
-/// CHECK: DEVID:[[S:[ ]*]][[DEVID:[0-9]+]] SGN:8 ConstWGSize:1024 args: 7 teamsXthrds:( 10X1024)
+/// CHECK: DEVID:[[S:[ ]*]][[DEVID:[0-9]+]] SGN:8 ConstWGSize:1024 args: 7 teamsXthrds:(  5X1024)
 
