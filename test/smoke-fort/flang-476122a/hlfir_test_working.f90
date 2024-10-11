edab41ea3646707de7258c7cf391b9b12f326c8f
Sergio Afonso <safonsof@amd.com>
2024-10-11 16:35:02 +0100

    Remove constants from map clauses

--- test/smoke-fort/flang-476122a/hlfir_test_working.f90
+++ test/smoke-fort/flang-476122a/hlfir_test_working.f90
@@ -10,14 +10,14 @@ program main
   allocate(U(dimsB(1):dimsE(1),dimsB(2):dimsE(2)))
   U(:,:) = 1.0
 
-  !$OMP TARGET ENTER DATA MAP(to: U, dimsB, dimsE)
+  !$OMP TARGET ENTER DATA MAP(to: U)
   !$OMP TARGET TEAMS DISTRIBUTE PARALLEL DO SIMD COLLAPSE(2)
   do idim1 = dimsB(1), dimsE(1)
     do idim2 = dimsB(2), dimsE(2)
       U(idim1, idim2) = U(idim1, idim2) + 1.0
     END DO
   END DO
-  !$OMP TARGET EXIT DATA MAP(from: U) MAP(release: dimsB, dimsE)
+  !$OMP TARGET EXIT DATA MAP(from: U)
 
   if (abs(sum(U) - 50.0d0) < TOL) then
     print*,"OMP offload test passed."
