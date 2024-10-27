0655cba06e146716567f4341e2507a36f2bd6077
Ron Lieberman <ron.lieberman@amd.com>
2024-10-27 07:34:34 -0500

    Reduction perf problem from nekbone

---
+++ test/smoke-fort-fails/flang-494405/redprob.f90
@@ -0,0 +1,22 @@
+      function glsc3_acc(a,b,mult,n)
+      real a(n),b(n),mult(n)
+      real tmp,work(1)
+      tmp = 0.0
+!$omp target teams distribute parallel do simd map(tofrom:tmp)
+!$omp& reduction(+:tmp)
+      do 10 i=1,n
+         tmp = tmp + a(i)*b(i)*mult(i)
+ 10   continue
+
+      glsc3_acc = tmp
+      return
+      end
+program vmm
+    implicit none
+    integer, parameter :: N = 100000
+    real a(N), b(N), c(N)
+    integer i, j, num, flag;
+    print *, glsc3_acc(a,b,c,N)
+end program
+
+
