b617430d5b2730df880663a1ce2b2dcf2e642486
Dan Palermo <dan.palermo@amd.com>
2024-11-18 09:44:13 -0600

    [smoke-fort-fails] Make flang-485161 self checking

--- test/smoke-fort-fails/flang-485161/dynamic_alloc.f90
+++ test/smoke-fort-fails/flang-485161/dynamic_alloc.f90
@@ -28,5 +28,34 @@ module dynamic_alloc
 end module dynamic_alloc
 
 program main
+  use dynamic_alloc
   implicit none
+  double precision :: A(50), B(50), C(50)
+  integer i
+  do i = 1, size(A)
+      A(i) = i
+  enddo
+  B = 0
+  C = 0
+!$omp target
+  call sub_dynamic_alloc(A, B)
+  C = func_dynamic_alloc(A)
+!$omp end target
+  print *, "A = "
+  print *, A
+  print *, "B = "
+  print *, B
+  print *, "C = "
+  print *, C
+  do i = 1, size(A)
+      if ((A(i)+2) /= B(i)) then
+          print *, "Error B @", i
+          stop(1)
+      endif
+      if ((A(i)+2) /= C(i)) then
+          print *, "Error C @", i
+          stop(2)
+      endif
+  enddo
+  print *, "Success"
 end program
