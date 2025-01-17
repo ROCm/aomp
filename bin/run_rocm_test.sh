fa7c81381d3b76b553d5bb9e0368cf39ae60ba06
Ethan Stewart <ethan.stewart@amd.com>
2025-01-17 19:33:06 +0000

    run_rocm_test - Use package manager detection for test package download
    If new OS support is added the script would fall over.
    We now try to determine the package manager to cover a wider
    range of operating systems.

--- bin/run_rocm_test.sh
+++ bin/run_rocm_test.sh
@@ -172,51 +172,74 @@ if [ "$aomp" != 1 ]; then
       fi
       rm -rf $tmpdir
       mkdir -p $tmpdir
-      # Determine OS and download package not using sudo.
-      if [[ "$os_name" =~ "Ubuntu" ]] || [[ "$os_name" =~ "Debian" ]]; then
+      # Determine package manager and download package not using sudo.
+      # apt support
+      echo Testing apt...
+      apt --version && pkg_mgr_found=1 || pkg_mgr_found=0
+      if [ $pkg_mgr_found -eq 1 ]; then
+        echo Using apt package manager
         cd $tmpdir
         apt-get download $test_package_name
         test_package=$(ls -lt $tmpdir | grep -Eo -m1 openmp-extras-tests.*)
         dpkg -x $test_package .
         script=$(find . -type f -name 'run_rocm_test.sh')
         cd $(dirname $script)
-      # CentOS/RHEL support. CentOS 7 requires a different method.
-      elif [[ "$os_name" =~ "CentOS" ]] || [[ "$os_name" =~ "Red Hat" ]] || [[ "$os_name" =~ "Oracle Linux Server" ]] || [[ "$os_name" =~ "Alibaba Linux Server" ]]; then
-        osversion=$(cat /etc/os-release | grep -e ^VERSION_ID)
-        if [[ $osversion =~ '"7' ]]; then
-          yumdownloader --destdir=$tmpdir $test_package_name
-        else
-          yum download --destdir $tmpdir $test_package_name
+      fi
+      # dnf/yum support, CentOS 7 requires a different method.
+      if [ $pkg_mgr_found -eq 0 ]; then
+        echo Testing dnf...
+        dnf --version && pkg_mgr_found=1 || pkg_mgr_found=0
+        if [ $pkg_mgr_found -eq 1 ]; then
+          echo Using dnf package manager
+	  dnf download --destdir=$tmpdir openmp-extras-tests
+	  test_package=$(ls -lt $tmpdir | grep -Eo -m1 openmp-extras-tests.*)
+	  extract_rpm $test_package
         fi
-        test_package=$(ls -lt $tmpdir | grep -Eo -m1 openmp-extras-tests.*)
-        extract_rpm $test_package
-      # SLES support.
-      elif [[ "$os_name" =~ "SLES" ]]; then
-        local_dir=~/openmp-extras-test
-        rm -f "$local_dir"/*
-        zypper --pkg-cache-dir $local_dir download $test_package_name
-        test_package=$(ls -lt "$local_dir"/rocm/ | grep -Eo -m1 openmp-extras-tests.*)
-        cp "$local_dir"/rocm/"$test_package" $tmpdir
-        extract_rpm $test_package
-      elif [[ "$os_name" =~ "Microsoft Azure Linux" ]]; then
-	dnf download --destdir=$tmpdir openmp-extras-tests
-	test_package=$(ls -lt $tmpdir | grep -Eo -m1 openmp-extras-tests.*)
-	extract_rpm $test_package
-      else
-        echo "Error: Could not determine operating system package manager type."
+      fi
+      if [ $pkg_mgr_found -eq 0 ]; then
+        echo Testing yum...
+        yum --version && pkg_mgr_found=1 || pkg_mgr_found=0
+        if [ $pkg_mgr_found -eq 1 ]; then
+          echo Using yum package manager
+          osversion=$(cat /etc/os-release | grep -e ^VERSION_ID)
+          if [[ $osversion =~ '"7' ]]; then
+            yumdownloader --destdir=$tmpdir $test_package_name
+          else
+            yum download --destdir $tmpdir $test_package_name
+          fi
+          test_package=$(ls -lt $tmpdir | grep -Eo -m1 openmp-extras-tests.*)
+          extract_rpm $test_package
+        fi
+      fi
+      # zypper support
+      if [ $pkg_mgr_found -eq 0 ]; then
+        echo Testing zypper...
+        zypper --version && pkg_mgr_found=1 || pkg_mgr_found=0
+        if [ $pkg_mgr_found -eq 1 ]; then
+          echo Using zypper package manager
+          local_dir=~/openmp-extras-test
+          rm -f "$local_dir"/*
+          zypper --pkg-cache-dir $local_dir download $test_package_name
+          test_package=$(ls -lt "$local_dir"/rocm/ | grep -Eo -m1 openmp-extras-tests.*)
+          cp "$local_dir"/rocm/"$test_package" $tmpdir
+          extract_rpm $test_package
+        fi
+      fi
+      if [ $pkg_mgr_found -eq 0 ]; then
+        echo "Error: Could not determine package manager type."
         exit 1
       fi
-    # Environment already has test package
+    # Environment already has test package, use it
     else
       rm -rf $tmpdir
       mkdir -p $tmpdir
       cp -ra "$ROCMINF"/share/openmp-extras/tests $tmpdir
       cd $tmpdir/tests/bin
     fi
-  ./rocm_quick_check.sh
-  export SKIP_TEST_PACKAGE=1
-  ./run_rocm_test.sh
-  exit $?
+    ./rocm_quick_check.sh
+    export SKIP_TEST_PACKAGE=1
+    ./run_rocm_test.sh
+    exit $?
   fi
 fi
 echo $AOMP $REAL_AOMP using test branch $TEST_BRANCH
