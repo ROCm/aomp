1b734185276f1ad43c03d7261ac9700abf35159a
Dhruva Chakrabarti <Dhruva.Chakrabarti@amd.com>
2025-02-25 19:12:54 -0600

    Added 3 tests that evaluate min/max reduction using fmin/fmax. (1) smoke-dev/xteam-red-min-max: Using regular types. (2) smoke-dev/xteamr_builtins: A perf test using regular types. (3) smoke-fails/xteam-red-min-max-small-precision: Using small types.

---
+++ test/smoke-dev/xteam-red-min-max/xteam-red-min-max.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <math.h>
+#include <climits>
+#include <omp.h>
+
+#define N 100000
+
+template<typename T>
+int compute_min_max() {
+  T min_t = std::numeric_limits<T>::max();
+  T max_t = std::numeric_limits<T>::min();
+  T *arr_t = new T[N];
+  for (int i = 0; i < N; i++)
+    arr_t[i] = fmin(i, std::numeric_limits<T>::max()); // no overflow
+#pragma omp target data map(to : arr_t[0 : N])
+  {
+#pragma omp target teams distribute parallel for reduction(min : min_t)
+    for (int j = 0; j < N; j = j + 1)
+      min_t = fmin(min_t, arr_t[j]);
+
+#pragma omp target teams distribute parallel for reduction(max : max_t)
+    for (int j = 0; j < N; j = j + 1)
+      max_t = fmax(max_t, arr_t[j]);
+  }
+  delete[] arr_t;
+  std::cout << "min_t = " << min_t << " max_t = " << max_t << "\n";
+  int rc = 0;
+  if (min_t != 0) {
+    std::cout << "Failed min: expected 0\n";
+    rc = 1;
+  }
+  T expected_max_t = fmin(N - 1, std::numeric_limits<T>::max());
+  if (max_t != expected_max_t) {
+    std::cout << "Failed max: expected " << expected_max_t << " \n";
+    rc = 1;
+  }
+  return rc;
+}
+
+int main()
+{
+  int rc = 0;
+  if (compute_min_max<short>())
+    rc = 1;
+  if (compute_min_max<ushort>())
+    rc = 1;
+  if (compute_min_max<int>())
+    rc = 1;
+  if (compute_min_max<unsigned int>())
+    rc = 1;
+  if (compute_min_max<long long>())
+    rc = 1;
+  if (compute_min_max<unsigned long long>())
+    rc = 1;
+  if (compute_min_max<float>())
+    rc = 1;
+  if (compute_min_max<double>())
+    rc = 1;
+
+  if (!rc)
+    std::cout << "Success\n";
+  else
+    std::cout << "Failed\n";
+  return rc;
+}
