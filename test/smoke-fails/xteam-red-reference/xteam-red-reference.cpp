d8df88d7d512cfdb91d154489eca6ee70fea7be4
Dhruva Chakrabarti <Dhruva.Chakrabarti@amd.com>
2025-03-07 13:32:33 -0600

    Added a test case for reduction on C++ reference variable.
    Sergio found it fails with Xteam reduction and provided the test.

---
+++ test/smoke-fails/xteam-red-reference/xteam-red-reference.cpp
@@ -0,0 +1,24 @@
+// Test reduction on C++ reference.
+#include <iostream>
+#include <omp.h>
+
+void compute_reduced_sum(int n, int &x) {
+#pragma omp target teams distribute parallel for reduction(+ : x)
+  for (int i = 0; i < n; ++i)
+    x += i;
+}
+
+int main()
+{
+  int n = 1000;
+  int sum = 0;
+  compute_reduced_sum(n, sum);
+
+  std::cout << "sum = " << sum << "\n";
+  int rc = (sum != 499500);
+  if (!rc)
+    std::cout << "Success\n";
+  else
+    std::cout << "Failed\n";
+  return rc;
+}
