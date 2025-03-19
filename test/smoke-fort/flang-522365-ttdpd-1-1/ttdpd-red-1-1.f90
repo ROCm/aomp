b3e0a2ce608d5b0ca51f4da4c9ea369a8248f882
dpalermo <dan.palermo@amd.com>
2025-03-18 21:30:23 -0500

    [smoke-fort,-fails] Add test cases for flang-522365 (#1287)

---
+++ test/smoke-fort/flang-522365-ttdpd-1-1/ttdpd-red-1-1.f90
@@ -0,0 +1,35 @@
+program loop_reduction_repro
+  implicit none
+  integer :: i, int1, int2, exp1, exp2, errcnt
+  integer, parameter :: n = 1000, s1 = 1, s2 = 2
+  int1 = s1
+  int2 = s2
+
+  !$omp target teams distribute parallel do reduction(+:int1)
+  do i=1,n
+    int1 = int1 + 1
+  end do
+  !$omp target teams distribute parallel do reduction(+:int2)
+  do i=1,n
+    int2 = int2 + i
+  end do
+  write(*,*) n, int1, int2
+
+  exp1 = n + s1
+  exp2 = (n*(n+1))/2 + s2
+  write(*,*) n, exp1, exp2
+
+  errcnt = 0
+  if (int1 .ne. exp1) then
+    print *, "FAILED chk1: ", int1, " != ", exp1
+    errcnt = errcnt + 1
+  endif
+  if (int2 .ne. exp2) then
+    print *, "FAILED chk2: ", int2, " != ", exp2
+    errcnt = errcnt + 1
+  endif
+  if (errcnt .gt. 0) then
+    stop errcnt
+  endif
+  print *, "PASSED"
+end program
