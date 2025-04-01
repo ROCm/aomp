7e45f52fa1f2b42282dea96c0d0055492d27ab23
Ron Lieberman <ron.lieberman@amd.com>
2025-03-31 20:25:01 -0500

    transition from flang-new to flang

--- test/smoke-fort/flang-439387a/test_crayptr.f90
+++ test/smoke-fort/flang-439387a/test_crayptr.f90
@@ -1,6 +1,6 @@
 program test_crayptr
      implicit none
-     integer*8 var(2,*) !works with gfortran, but not accepted by flang-new
+     integer*8 var(2,*) !works with gfortran, but not accepted by flang
      pointer(ivar,var)
      integer*8, allocatable :: location(:,:)
      integer i,j
