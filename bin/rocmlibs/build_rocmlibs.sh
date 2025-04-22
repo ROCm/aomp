f3c680e961daaacfe69297ca6288bb1d86dd3569
gregrodgers <Gregory.Rodgers@amd.com>
2025-04-21 20:48:20 -0500

    temporarily remove hipSOLVER till it works on more machines. Add path for prereq build of cmake

--- bin/rocmlibs/build_rocmlibs.sh
+++ bin/rocmlibs/build_rocmlibs.sh
@@ -99,7 +99,8 @@ echo
 components="prereq rocm-cmake"
 if [ "$AOMP_STANDALONE_BUILD" == 1 ] ; then
   # This ordered build is important when starting from scratch
-  components="$components rocBLAS rocPRIM rocSPARSE rocSOLVER hipBLAS-common hipBLAS rocRAND hipRAND rccl half hipSOLVER"
+  #components="$components rocBLAS rocPRIM rocSPARSE rocSOLVER hipBLAS-common hipBLAS rocRAND hipRAND rccl half hipSOLVER"
+  components="$components rocBLAS rocPRIM rocSPARSE rocSOLVER hipBLAS-common hipBLAS rocRAND hipRAND rccl half "
 else
   echo "ERROR: Cannot run $0 with AOMP_STANDALONE_BUILD=$AOMP_STANDALONE_BUILD"
   echo "       Please set $AOMP_STANDALONE_BUILD=1"
