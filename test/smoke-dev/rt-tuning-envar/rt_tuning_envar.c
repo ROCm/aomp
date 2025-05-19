8cc06348907fe78c7cc3459d5814281c8a7e798d
Kewen12 <Kewen.Meng@amd.com>
2025-05-19 12:50:34 -0700

    [Smoke Test] Update smoke test to cover new changes (#1201)
    * [Smoke Test] Update smoke test to cover new changes
    
    This PR is to update the smoke test to reflect the new changes on runtime tuning.
    
    * updated check pattern to reflect recent changes

--- test/smoke-dev/rt-tuning-envar/rt_tuning_envar.c
+++ test/smoke-dev/rt-tuning-envar/rt_tuning_envar.c
@@ -26,7 +26,7 @@ int main()
   for (i=0; i<N; i++)
     if (a[i] != b[i] ) {
       rc++;
-      printf ("Wrong varlue: a[%d]=%d\n", i, a[i]);
+      printf ("Wrong value: a[%d]=%d\n", i, a[i]);
     }
 
   if (!rc)
@@ -35,4 +35,4 @@ int main()
   return rc;
 }
 
-/// CHECK: Kernel Duration: {{[0-9]+}} ns
+/// CHECK: [Autotuning run] Kernel {{[^ ]+}} with {{[0-9]+}} teams and {{[0-9]+}} threads completed in {{[0-9]+}} ns.
