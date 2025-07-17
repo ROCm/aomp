3b13d1bc038c769cd91feceee6a2a24524f17dc2
Michael Klemm <michael.klemm@amd.com>
2025-07-17 13:52:25 +0200

    Test flang-539234

---
+++ test/smoke-fort-fails/flang-539234/split_teams_distribute_parallel_do.f90
@@ -0,0 +1,48 @@
+! compile with:
+!
+! amdflang -O2 -fopenmp --offload-arch=gfx90a -o split_teams_distribute_parallel_do split_teams_distribute_parallel_do.f90
+
+module foo_mod
+   implicit none
+contains
+   subroutine foo(ib,val,arr)
+      implicit none
+      integer, intent(in) :: ib, val
+      integer, dimension(:), intent(inout) :: arr
+      integer :: i
+      !$omp declare target(foo)
+
+      !$omp parallel do
+      do i=1,ib
+         arr(i)=arr(i)+val
+      end do
+   end subroutine foo
+end module foo_mod
+
+program hierarchical
+   use foo_mod
+   implicit none
+   integer :: i, j, val
+   integer :: a(10,10)
+
+   a=1
+   val=2
+   !$omp target enter data map(to:a)
+
+   !$omp target teams distribute
+   do j=1,10
+      call foo(10,val,a(:,j))
+   end do
+
+   !$omp target update from(a)
+   do j=1,10
+      do i=1,10
+         if (a(i,j)/=3) then
+            print *, 'Error for indexes', i, j
+            print *, 'Value is', a(i,j)
+            stop 1
+         end if
+      end do
+   end do
+   print *, 'Success!!!'
+end program hierarchical
