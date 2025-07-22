2423136374243dc26cbbb1eba5608c5c21d8cc32
Amit Pandey <pandey.kumaramit2023@gmail.com>
2025-07-22 14:49:12 +0530

    [SWDEV-483895][ASan]  Add 'hipMallocManaged' regression tests

---
+++ test/smoke-asan/hip-managed-uaf/hip-managed-uaf.cpp
@@ -0,0 +1,37 @@
+#define __HIP_PLATFORM_AMD__ 1
+#include <hip/hip_runtime.h>
+
+void printHipError(hipError_t error) {
+  printf("Hip Error: %s\n", hipGetErrorString(error));
+}
+
+bool hipCallSuccessfull(hipError_t error) {
+  if (error != hipSuccess)
+    printHipError(error);
+  return error == hipSuccess;
+}
+
+__global__ void TestKernel(int n, int *ptr) {
+  int index = blockDim.x * blockIdx.x + threadIdx.x;
+  if (index < n) {
+    ptr[index] = 2 * (index + 1);
+  }
+}
+
+int main(int argc, char *argv[]) {
+  int N = 100;
+  size_t NBytes = N * sizeof(int);
+  int *D_Ptr;
+  int NumOfThreadBlocks = (N + 64 - 1) / 64;
+  int ThreadBlockSize = 64;
+  hipCallSuccessfull(hipMallocManaged(&D_Ptr, NBytes));
+  hipCallSuccessfull(hipFree(D_Ptr));
+  hipLaunchKernelGGL(TestKernel, dim3(NumOfThreadBlocks), dim3(ThreadBlockSize),
+                     0, 0, N, D_Ptr);
+  return 0;
+}
+
+/// CHECK:=================================================================
+/// CHECK-NEXT:=={{[0-9]+}}==ERROR: AddressSanitizer: heap-use-after-free on amdgpu device 0 at pc [[PC:.*]]
+/// CHECK-NEXT:WRITE of size 4 in workgroup id ({{[0-9]+}},0,0)
+/// CHECK-NEXT:  #0 [[PC]] in TestKernel(int, int*) at {{.*}}aomp/test/smoke-asan/hip-managed-uaf/hip-managed-uaf.cpp:17:{{[0-9]+}}
