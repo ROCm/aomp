663c3ea3d5d62b961954f9196e743fb8bc3a063a
Michael Klemm <michael.klemm@amd.com>
2025-07-22 18:02:14 +0200

    [Flang] Add test for linker issues on target devices and array statements (#1504)

---
+++ test/smoke-fort-dev/device_aassign/device_aassign.f90
@@ -0,0 +1,38 @@
+module mod
+    implicit none
+contains
+    subroutine assgn(a, b)
+        implicit none
+        real, dimension(:), allocatable :: a
+        real, dimension(:), allocatable :: b
+
+        !$omp target
+            a = b + 1.0
+        !$omp end target
+    end subroutine assgn
+end module mod
+
+program bla
+    use mod
+    implicit none
+
+    integer, parameter :: n = 1024
+    real, dimension(:), allocatable :: a
+    real, dimension(:), allocatable :: b
+
+    allocate(a(n), b(n))
+
+    b = 41.0
+
+    !$omp target enter data map(to:a, b)
+    call assgn(a, b)
+    !$omp target exit data map(from: a,b)
+
+    if (sum(a / 42.0) /= real(n)) then
+        print '(a)', 'ERROR'
+        stop 1
+    end if
+    print '(a)', 'SUCCESS'
+
+    deallocate(a, b)
+end program bla
