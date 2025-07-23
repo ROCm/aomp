6c88451f8ca6d1b355862daa75bfc8fa3f708e5d
agozillon <Andrew.Gozillon@amd.com>
2025-07-23 14:08:23 -0500

    [Flang][smoke-dev-fort] Add smoke tests for issue with missing load/stores to maps in branches

---
+++ test/smoke-fort-dev/flang-544421/repro_544421.f90
@@ -0,0 +1,55 @@
+module reproducer_mod
+contains
+   subroutine branching_target_call(dt, switch)
+      implicit none
+      real(4), dimension(:), intent(inout) :: dt
+      logical, intent(in) :: switch
+      integer :: dim, idx
+
+      dim = size(dt)
+
+      if (switch) then
+
+!$omp target teams distribute parallel do
+         do idx = 1, dim
+            dt(idx) = 20
+         end do
+
+      else
+
+!$omp target teams distribute parallel do
+         do idx = 1, dim
+            dt(idx) = 30
+         end do
+      end if
+
+   end subroutine branching_target_call
+end module reproducer_mod
+
+program reproducer
+   use reproducer_mod
+   implicit none
+   real(4), dimension(:), allocatable :: dt
+   integer :: n = 21312
+   integer :: i
+
+   allocate (dt(n))
+
+   call branching_target_call(dt, .FALSE.)
+
+   do i = 1, n
+      if (dt(i) /= 30) then
+         print *, "failed"
+      end if
+   end do
+
+   call branching_target_call(dt, .TRUE.)
+
+   do i = 1, n
+      if (dt(i) /= 20) then
+         print *, "failed"
+      end if
+   end do
+
+   print *, "success"
+end program reproducer
