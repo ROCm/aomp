3f8467416043395d1ec035dd0c4c0a591dc21f5b
Michael Halkenhaeuser <MichaelGerald.Halkenhauser@amd.com>
2025-08-11 07:31:44 -0500

    [cuda][offload][openmp] Fix omitting symlinks in CUDA header search
    Fix CUDA header search by also including symlinks in the results.

--- bin/build_openmp.sh
+++ bin/build_openmp.sh
@@ -19,9 +19,9 @@ _ompd_src_dir="$LLVM_INSTALL_LOC/share/gdb/python/ompd/src"
 _ompd_dir="$LLVM_INSTALL_LOC/share/gdb/python/ompd"
 
 if [ "$AOMP_BUILD_CUDA" == 1 ] ; then
-   CUDAH=$(find "$CUDAT" -type f -name "cuda.h" 2>/dev/null)
+   CUDAH=$(find "$CUDAT" -type f,l -name "cuda.h" 2>/dev/null)
    if [ "$CUDAH" == "" ] ; then
-      CUDAH=$(find "$CUDAINCLUDE" -type f -name "cuda.h" 2>/dev/null)
+      CUDAH=$(find "$CUDAINCLUDE" -type f,l -name "cuda.h" 2>/dev/null)
    fi
    if [ "$CUDAH" == "" ] ; then
       echo
