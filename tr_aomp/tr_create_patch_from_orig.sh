c85ccd0ece05d3ea89477b43843d9e13c25eb236
gregrodgers <Gregory.Rodgers@amd.com>
2025-08-27 16:32:34 -0500

    [tr_aomp] Removed need for TheRock.orig and reuse TheRock when it exists to significantly speed up tr_clone_aomp.sh.  The rarely used create_patch tool now builds an orig.TheRock from saved branches in each submodule

--- tr_aomp/tr_create_patch_from_orig.sh
+++ tr_aomp/tr_create_patch_from_orig.sh
@@ -8,21 +8,46 @@ thisdir=$(dirname "$realpath")
 . "$thisdir/tr_aomp_common_vars"
 # --- end standard header ----
 
-_therockdir=$TR_AOMP_REPOS/TheRock
+_origdir=$TR_AOMP_REPOS/orig.TheRock
 
-_patchdir=$TR_AOMP_REPOS/aomp/tr_aomp/patches
-mkdir -p $_patchdir
-_patchfile=$_patchdir/tr_aomp.patch
+mkdir -p $TR_AOMP_REPOS/aomp/tr_aomp/patches
+_patchfile=$TR_AOMP_REPOS/aomp/tr_aomp/patches/tr_aomp.patch
 
 cd $TR_AOMP_REPOS
+[ -d orig.TheRock ] && echo "rm -rf orig.TheRock" && rm -rf orig.TheRock
+mkdir orig.TheRock
+echo rsync -a TheRock/ orig.TheRock/
+rsync -a TheRock/ orig.TheRock/
 
-if [ ! -d TheRock.orig ] ; then 
-   echo "ERROR: missing directory $TR_AOMP_REPOS/TheRock.orig"
-   exit 1
-fi
+cd orig.TheRock
+echo "--- git status"
+git status
+echo "--- git status DONE"
 
+cd $_origdir
+_tmpfile=/tmp/submod$$
+git submodule > $_tmpfile
+while read _line ; do
+  _subdir=`echo $_line | cut -d" " -f2`
+  cd $_origdir/$_subdir
+  if [ "$_subdir" != "compiler/amd-llvm" ] && [ "$_subdir" != "compiler/hipify" ] ; then
+     echo "DIR:$PWD "
+     git checkout .
+     echo git checkout tr_aomp_orig_$AOMP_VERSION_STRING
+     git checkout tr_aomp_orig_$AOMP_VERSION_STRING 2>/dev/null
+  fi
+done < $_tmpfile
+
+echo cd $_origdir
+cd $_origdir
+git checkout .
+echo git checkout tr_aomp_orig_$AOMP_VERSION_STRING
+git checkout tr_aomp_orig_$AOMP_VERSION_STRING
+
+rm $_tmpfile
+
+cd $TR_AOMP_REPOS
 # Important avoid development changes to llvm-project aka amd-llvm
 # Also skip .git diffs and python cache 
-echo "diff -aur -x .git -x amd-llvm -x hipify -x __pycache__ -x build  -x .ccache TheRock.orig TheRock > $_patchfile" 
-diff -aur -x .git -x amd-llvm -x hipify -x __pycache__ -x build  -x .ccache TheRock.orig TheRock > $_patchfile
-
+echo "diff -aur -x .git -x amd-llvm -x hipify -x __pycache__ -x build  -x .ccache orig.TheRock TheRock > $_patchfile"
+diff -aur -x .git -x amd-llvm -x hipify -x __pycache__ -x build  -x .ccache orig.TheRock TheRock > $_patchfile
