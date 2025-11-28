b53e852c1d9941b47284b433f4143806cf8bf965
ergawy <kareem.ergawy@amd.com>
2025-11-28 03:31:51 -0600

    [smoke-fort][OpenMP] Add tests for GPU reductions on allocatables and static arrays
    Adds smoke tests for the changes introduced in upstream PR: https://github.com/llvm/llvm-project/pull/165714. These tests cannot be added as offloading tests upstream (yet) since they need to link wit h flang's device RT lib.

---
+++ test/smoke-fort-dev/byref-reduction-static-array/byref_reduction_static_array.f90
@@ -0,0 +1,39 @@
+program main
+  implicit none
+
+  real, parameter :: num_iters = 1000000
+  integer, parameter :: arr_size = 10
+  real :: arr(arr_size)
+  real :: expected_arr(arr_size)
+  integer :: i
+
+  do i = 1, arr_size
+    arr(i) = i
+    expected_arr(i) = i + num_iters
+  end do
+
+  !$omp target map(tofrom: arr)
+  call foo(arr)
+  !$omp end target
+
+  print *, "result: ", arr
+
+  if (any(arr /= expected_arr)) then
+    print *, "Incorrect result! (actual): ", arr, " vs. (expected): ", expected_arr
+    stop 1
+  end if
+
+contains
+subroutine foo(arr)
+  implicit none
+  integer :: i
+  real, intent(inout) :: arr(arr_size)
+
+  !$omp parallel do reduction(+: arr)
+  do i = 1, num_iters
+    arr = arr + 1
+  end do
+end subroutine
+
+
+end program
