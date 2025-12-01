45d1e2485785366ca5621e7fb1d953c2b8c83c35
Kareem Ergawy <kareem.ergawy@amd.com>
2025-12-01 12:39:40 +0100

    [smoke-fort][OpenMP] Add tests for GPU reductions on allocatables and static arrays (#1718)
    * [smoke-fort][OpenMP] Add tests for GPU reductions on allocatables and static arrays
    
    Adds smoke tests for the changes introduced in upstream PR: https://github.com/llvm/llvm-project/pull/165714. These tests cannot be added as offloading tests upstream (yet) since they need to link wit h flang's device RT lib.
    
    * review comments, Michael

---
+++ test/smoke-fort-dev/byref-reduction-scalar-allocatable/byref_reduction_scalar_allocatable.f90
@@ -0,0 +1,33 @@
+program main
+  implicit none
+
+  real, allocatable :: scalar_alloc
+  integer :: i
+  real, parameter :: num_iters = 1000000
+
+  allocate(scalar_alloc)
+  scalar_alloc = 0
+
+  !$omp target map(tofrom: scalar_alloc)
+  call foo(scalar_alloc)
+  !$omp end target
+
+  print *, "result: ", scalar_alloc
+
+  if (scalar_alloc /= num_iters) then
+    print *, "Incorrect result! (actual): ", scalar_alloc, " vs. (expected): ", num_iters
+    stop 1
+  end if
+
+contains
+  subroutine foo(scalar_alloc)
+    implicit none
+    integer :: i
+    real, allocatable, intent(inout) :: scalar_alloc
+
+    !$omp parallel do reduction(+: scalar_alloc)
+    do i = 1, num_iters
+      scalar_alloc = scalar_alloc + 1
+    end do
+  end subroutine
+end program
