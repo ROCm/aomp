645cdcf49e002b25f7eea3ec7dc243db63dfbd6e
Ethan Stewart <ethan.stewart@amd.com>
2025-12-05 16:55:36 -0600

    Update run_hpc2021.sh to match aomp-dev.
    This is causing problems with using 'git am' on merges

--- bin/run_hpc2021.sh
+++ bin/run_hpc2021.sh
@@ -40,8 +40,6 @@ else
 fi
 export PATH=$AOMP/../bin:$AOMP/../../bin:$PATH
 export MPI=$INST
-# strange UCX warnings in dockers using openmpi, this suppresses
-export UCX_LOG_LEVEL=error
 ./runOne
 rm -rf $INST
 #grep ratio= result/*.log
