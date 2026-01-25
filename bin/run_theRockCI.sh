deaad0bf57d64ef30c94763f5099b8531b484e4f
Ron Lieberman <ron.lieberman@amd.com>
2026-01-25 13:08:27 -0600

    debug 3

--- bin/run_theRockCI.sh
+++ bin/run_theRockCI.sh
@@ -9,7 +9,7 @@
 
 SUITE_LIST=${SUITE_LIST:-"smoke-limbo smoke-fort-limbo smoke smoke0firt nekbone babelstream fortran-babelstream accel2023 hpc2021 openmpapps"}
 
-export PATH=$PATH:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+export PATH=$PATH:/opt/rocm/bin
 echo "PATH=" $PATH
 set +x
 which lspci
@@ -17,7 +17,6 @@ which rocm-smi
 which rocminfo
 which make
 set -x
-
 pip install --no-warn-script-location filecheck
 export PATH=$PATH:/home/$USER/.local/bin
 which filecheck
