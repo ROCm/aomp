551927ff97774f7d5b1ddec8ce862cc63a5266fc
JP Lehr <JanPatrick.Lehr@amd.com>
2026-03-04 03:09:43 -0600

    [NFC] Address shellcheck issues

--- bin/run_rushlarsen.sh
+++ bin/run_rushlarsen.sh
@@ -4,11 +4,11 @@
 # User can set RUN_OPTIONS to control what variants(openmp, hip) are selected.
 
 # --- Start standard header to set AOMP environment variables ----
-realpath=`realpath $0`
-thisdir=`dirname $realpath`
+realpath=$(realpath "$0")
+thisdir=$(dirname "$realpath")
 export AOMP_USE_CCACHE=0
 
-. $thisdir/aomp_common_vars
+. "$thisdir"/aomp_common_vars
 # --- end standard header ----
 
 # Setup AOMP variables
@@ -38,8 +38,8 @@ hip_flags="-D__HIP_PLATFORM_AMD__ -I${AOMPHIP}/include -O3 --cuda-gpu-arch=${AOM
 hip_dir="tests/rush_larsen/rush_larsen_gpu_hip"
 hip_exec="rush_larsen_gpu_hip"
 
-if [ -d $AOMP_REPOS_TEST/goulash ]; then
-  cd $AOMP_REPOS_TEST/goulash
+if [ -d "$AOMP_REPOS_TEST"/goulash ]; then
+  cd "$AOMP_REPOS_TEST"/goulash || exit
   goulash_home=$AOMP_REPOS_TEST/goulash
   rm -f results.txt
   rm -f Lresults.txt
@@ -48,36 +48,36 @@ else
   exit 1
 fi
 
-echo RUN_OPTIONS: $RUN_OPTIONS
+echo RUN_OPTIONS: "$RUN_OPTIONS"
 for option in $RUN_OPTIONS; do
   if [ "$option" == "openmp" ]; then
-    cd $goulash_home/$omp_dir
+    cd "$goulash_home"/$omp_dir || exit
     make clean
     make CXX="$omp_cxx" CXXFLAGS="$omp_flags" COMPILERID="-DCOMPILERID=amd"
     if [ $? -ne 1 ]; then
-      ./$omp_exec 100 10 2>&1 | tee -a $goulash_home/results.txt
-      ./$omp_exec 100000 .00000001 2>&1 | tee -a $goulash_home/Lresults.txt
+      ./$omp_exec 100 10 2>&1 | tee -a "$goulash_home"/results.txt
+      ./$omp_exec 100000 .00000001 2>&1 | tee -a "$goulash_home"/Lresults.txt
     fi
   elif [ "$option" == "hip" ]; then
-    cd $goulash_home/$hip_dir
+    cd "$goulash_home"/$hip_dir || exit
     make clean
     make CXX="$hip_cxx" CXXFLAGS="$hip_flags" COMPILERID="-DCOMPILERID=amd"
     if [ $? -ne 1 ]; then
-      ./$hip_exec 100 10 2>&1 | tee -a $goulash_home/results.txt
-      ./$hip_exec 100000 .00000001 2>&1 | tee -a $goulash_home/Lresults.txt
+      ./$hip_exec 100 10 2>&1 | tee -a "$goulash_home"/results.txt
+      ./$hip_exec 100000 .00000001 2>&1 | tee -a "$goulash_home"/Lresults.txt
     fi
   elif [ "$option" == "fomp" ]; then
-    cd $goulash_home/$fomp_dir
+    cd "$goulash_home"/$fomp_dir || exit
     make clean
     make FC="$fomp_f90" FCFLAGS="$fomp_flags" COMPILERID='-DCOMPILERID=\"amd\"'
     if [ $? -ne 1 ]; then
-      ./$fomp_exec 100 10 2>&1 | tee -a $goulash_home/results.txt
-      ./$fomp_exec 100000 .00000001 2>&1 | tee -a $goulash_home/Lresults.txt
+      ./$fomp_exec 100 10 2>&1 | tee -a "$goulash_home"/results.txt
+      ./$fomp_exec 100000 .00000001 2>&1 | tee -a "$goulash_home"/Lresults.txt
     fi
   else
     echo "ERROR: Option not recognized: $option."
     exit 1
   fi
-  echo >>  $goulash_home/results.txt
-  echo >>  $goulash_home/Lresults.txt
+  echo >>  "$goulash_home"/results.txt
+  echo >>  "$goulash_home"/Lresults.txt
 done
