b39024500b1e2163f2a377895e7e30d32f02e7d5
JP Lehr <JanPatrick.Lehr@amd.com>
2026-03-05 07:45:45 -0600

    Adding remaining fixes

--- bin/run_miniQMC.sh
+++ bin/run_miniQMC.sh
@@ -13,6 +13,7 @@ realpath=$(realpath "$0")
 thisdir=$(dirname "$realpath")
 export AOMP_USE_CCACHE=0
 
+# shellcheck source-path=SCRIPTDIR
 . "$thisdir"/aomp_common_vars
 # --- end standard header ----
 
@@ -43,7 +44,7 @@ export rocsolver_DIR=${ROCM}/lib/cmake/rocsolver/
 # how many threads should be used for building miniqmc
 : "${MQMC_NUM_BUILD_PROCS:=32}"
 # We pin the version by default, so we have only AOMP as moving target
-: ${MQMC_GIT_TAG:=9d9d7d3}
+: "${MQMC_GIT_TAG:=9d9d7d3}"
 
 
 if [ ! -d "$MQMC_SOURCE_DIR" ]; then
