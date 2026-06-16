a59965ae2c35ec060fa7e4e32ee09b2d2a7fa71f
Julian Brown <julian.brown@amd.com>
2026-06-16 07:19:34 -0500

    [AOMP] Taskify rocmlibs/build_rocBLAS.sh
    Refactor the legacy-idiom build_rocBLAS.sh into the taskified form:
    split into task_* functions over the single (default) config, move the
    Tensile commit checkout and repo patching out of top-level into
    task_patch (so introspection has no side effects), and modernize quoting.
    nocmake remains unsupported via a do_nocmake override that errors, and
    the AOMP_STANDALONE_BUILD-only restriction is enforced in task_precheck.
    
    Add AOMP_BUILD_TENSILE and ROCBLAS_USE_HIPBLASLT to the
    get_config_var_bool allow-list in aomp_utils.

--- bin/rocmlibs/build_rocBLAS.sh
+++ bin/rocmlibs/build_rocBLAS.sh
@@ -8,47 +8,24 @@
 #  build_rocblas.sh:  Script to build and install rocblas library
 #
 #
-BUILD_TYPE=${BUILD_TYPE:-Release}
+
+# Without these options, we can lose error status from command subtitutions,
+# etc.
+set -e
+shopt -s inherit_errexit
 
 # --- Start standard header to set AOMP environment variables ----
-realpath=`realpath $0`
-thisdir=`dirname $realpath`
+realpath=$(realpath -- "$0")
+thisdir=$(dirname "$realpath")
 . "$thisdir/../aomp_utils"
-. $thisdir/../aomp_common_vars
+. "$thisdir/../aomp_common_vars"
 # --- end standard header ----
 
 _repo_dir=$AOMP_REPOS/rocmlibs/rocBLAS
+_tensile_repo_dir=$AOMP_REPOS/rocmlibs/Tensile
 
-# Check if Tensile is to be built with rocBLAS
 AOMP_BUILD_TENSILE=${AOMP_BUILD_TENSILE:-1}
-if [ $AOMP_BUILD_TENSILE == 0 ] ; then 
-   echo 
-   echo "WARNING: Building rocblas without Tensile"
-   _local_tensile_opt="--no_tensile"
-else
-   _cwd=$PWD
-   _tensile_repo_dir=$AOMP_REPOS/rocmlibs/Tensile
-   cd $_tensile_repo_dir
-   # Read the commit SHA from the file rocBLAS/tensile_tag.txt
-   _tensile_commit_sha=$(cat $_repo_dir/tensile_tag.txt)
-   # Checkout the specific commit SHA
-   git checkout $_tensile_commit_sha
-   echo "Checking out Tensile commit $_tensile_commit_sha"
-   cd $_cwd
-   _local_tensile_opt="--test_local_path=$_tensile_repo_dir"
-   patchrepo $_tensile_repo_dir
-fi
-
-# Check if rocBLAS is to be built with hipBLASLT
-# It won't work unless hipBLASLT is already installed
 ROCBLAS_USE_HIPBLASLT=${ROCBLAS_USE_HIPBLASLT:-0}
-if [ $ROCBLAS_USE_HIPBLASLT == 0 ] ; then
-   echo
-   echo "WARNING: Building rocblas without hipBLASLT"
-   _local_hipblaslt_opt="--no_hipblaslt"
-fi
-
-patchrepo $_repo_dir
 
 export CC=$LLVM_INSTALL_LOC/bin/amdclang
 export CXX=$LLVM_INSTALL_LOC/bin/amdclang++
@@ -61,131 +38,202 @@ export USE_PERL_SCRIPTS=1
 export CXXFLAGS="-I$AOMP_INSTALL_DIR/include -D__HIP_PLATFORM_AMD__=1"
 export LDFLAGS="-fPIC"
 
-## this causes fail when building with Tensile
-#export TENSILE_SKIP_LIBRARY=1
-if [ "$AOMP_USE_CCACHE" != 0 ] ; then
-   _ccache_bin=`which ccache`
-  # export CMAKE_CXX_COMPILER_LAUNCHER=$_ccache_bin
-fi
+cfgvar() {
+  get_config_var_string rocblas "$1"
+}
 
-# Set _build_type_option to Release or Debug based on BUILD_TYPE
-if [ "$BUILD_TYPE" == "Debug" ] ; then
-   _build_type_option="--debug"
-fi
+cfgbool() {
+  get_config_var_bool rocblas "$1"
+}
 
-if [ $AOMP_STANDALONE_BUILD == 1 ] ; then 
-   if [ ! -L $AOMP ] ; then 
-     if [ -d $AOMP ] ; then 
-        echo "ERROR: Directory $AOMP is a physical directory."
-        echo "       It must be a symbolic link or not exist"
-        exit 1
-     fi
-   fi
-else
-   echo "ERROR: $0 only valid for AOMP_STANDALONE_BUILD=1"
-   exit 1
-fi
+get_src_dir() {
+   echo "$_repo_dir"
+}
 
-if [ "$1" == "nocmake" ] ; then 
+# Print the build dir for a given config, passed as $1.
+get_build_dir() {
+   local Cfg=$1
+   case "$Cfg" in
+   "default")
+     echo -n "$(cfgvar BUILD_DIR)/rocmlibs/rocBLAS"
+     ;;
+   *)
+     >&2 echo "Unknown config '$Cfg'"
+     exit 1
+     ;;
+   esac
+}
+
+# Print the install dir for a given config, passed as $1.
+get_install_dir() {
+   echo "$AOMP_INSTALL_DIR"
+}
+
+# rocBLAS does not support an incremental (nocmake) rebuild.
+do_nocmake() {
    echo "ERROR: nocmake is not an option for $0 because we use rmake.py"
    exit 1
-fi
+}
 
-# Make sure we can update the install directory 
-if [ "$1" == "install" ] ; then
-   $SUDO mkdir -p $AOMP_INSTALL_DIR
-   $SUDO touch $AOMP_INSTALL_DIR/testfile
-   if [ $? != 0 ] ; then 
-      echo "ERROR: No update access to $AOMP_INSTALL_DIR"
+task_precheck() {
+   if ! "$(cfgbool AOMP_STANDALONE_BUILD)"; then
+      echo "ERROR: $0 only valid for AOMP_STANDALONE_BUILD=1"
       exit 1
    fi
-   $SUDO rm $AOMP_INSTALL_DIR/testfile
-fi
+   if [ ! -L "$AOMP" ] && [ -d "$AOMP" ] ; then
+      echo "ERROR: Directory $AOMP is a physical directory."
+      echo "       It must be a symbolic link or not exist"
+      exit 1
+   fi
 
-if [ "$1" != "install" ] ; then
-   echo 
-   echo "This is a FRESH START. ERASING any previous builds in $BUILD_DIR/build/rocmlibs/rocBLAS"
-   echo "Use ""$0 install"" to avoid FRESH START."
-   echo rm -rf $BUILD_DIR/build/rocmlibs/rocBLAS
-   rm -rf $BUILD_DIR/build/rocmlibs/rocBLAS
-   mkdir -p $BUILD_DIR/build/rocmlibs/rocBLAS
-   if [ $AOMP_BUILD_TENSILE != 0 ] ; then 
+   check_writable_installdir "$1" "$AOMP_INSTALL_DIR"
+}
+
+task_patch() {
+   local _tensile_commit_sha
+   if "$(cfgbool AOMP_BUILD_TENSILE)"; then
+      cd "$_tensile_repo_dir" || exit
+      # Read the commit SHA from the file rocBLAS/tensile_tag.txt
+      _tensile_commit_sha=$(cat "$_repo_dir/tensile_tag.txt")
+      echo "Checking out Tensile commit $_tensile_commit_sha"
+      git checkout "$_tensile_commit_sha"
+      patchrepo "$_tensile_repo_dir"
+   fi
+   patchrepo "$_repo_dir"
+}
+
+task_unpatch() {
+   removepatch "$_repo_dir"
+   if "$(cfgbool AOMP_BUILD_TENSILE)"; then
+      removepatch "$_tensile_repo_dir"
+   fi
+}
+
+task_clean() {
+   local Cfg=$1
+   local BuildDir
+   BuildDir=$(get_build_dir "$Cfg")
+   echo "rm -rf $(shquot "$BuildDir")"
+   rm -rf "$BuildDir"
+   if "$(cfgbool AOMP_BUILD_TENSILE)"; then
       # Cleanup possible old tensile build area
-      echo rm -rf $_tensile_repo_dir/build
-      rm -rf $_tensile_repo_dir/build
+      echo "rm -rf $_tensile_repo_dir/build"
+      rm -rf "$_tensile_repo_dir/build"
    fi
-else
-   if [ ! -d $BUILD_DIR/build/rocmlibs/rocBLAS ] ; then
-      echo "ERROR: The build directory $BUILD_DIR/build/rocmlibs/rocBLAS"
-      echo "       run $0 without install option. "
-      exit 1
-   fi
-fi
+}
 
-if [ "$1" != "install" ] ; then
-   # Remember start directory to return on exit
-   _curdir=$PWD
-   MYCMAKEOPTS="
-     -DCMAKE_TOOLCHAIN_FILE=toolchain-linux.cmake
-     -DCMAKE_CXX_COMPILER=$CXX
-     -DCMAKE_C_COMPILER=$CC
-     -DROCM_DIR:PATH=$AOMP_INSTALL_DIR
-     -DCPACK_PACKAGING_INSTALL_PREFIX=$AOMP_INSTALL_DIR
-     -DCMAKE_INSTALL_PREFIX=$AOMP_INSTALL_DIR
-     -DROCM_PATH=$AOMP_INSTALL_DIR
-     -DCMAKE_PREFIX_PATH:PATH=$AOMP_INSTALL_DIR
-     -DCPACK_SET_DESTDIR=OFF
-     -DCMAKE_BUILD_TYPE=Release
-     -DTensile_CODE_OBJECT_VERSION=default
-     -DTensile_LOGIC=asm_full
-     -DTensile_TEST_LOCAL_PATH=$AOMP_REPOS/rocmlibs/Tensile
-     -DTensile_SEPARATE_ARCHITECTURES=ON
-     -DTensile_LAZY_LIBRARY_LOADING=ON
-     -DTensile_LIBRARY_FORMAT=msgpack
-     -DBUILD_WITH_HIPBLASLT=OFF
-     -DROCTX_PATH=$AOMP_INSTALL_DIR
-     -DGPU_TARGETS="""$ROCMLIBS_GFXLIST"""
-    "
+task_cmake() {
+   local Cfg=$1
+   local BuildDir
+   local SrcDir
+   local -a MYCMAKEOPTS
+
+   BuildDir="$(get_build_dir "$Cfg")"
+   SrcDir="$(get_src_dir)"
+
+   if ! "$(cfgbool AOMP_BUILD_TENSILE)"; then
+      echo
+      echo "WARNING: Building rocblas without Tensile"
+   fi
+   if ! "$(cfgbool ROCBLAS_USE_HIPBLASLT)"; then
+      echo
+      echo "WARNING: Building rocblas without hipBLASLT"
+   fi
+
+   MYCMAKEOPTS=(-DCMAKE_TOOLCHAIN_FILE=toolchain-linux.cmake
+                -DCMAKE_CXX_COMPILER="$CXX"
+                -DCMAKE_C_COMPILER="$CC"
+                -DROCM_DIR:PATH="$AOMP_INSTALL_DIR"
+                -DCPACK_PACKAGING_INSTALL_PREFIX="$AOMP_INSTALL_DIR"
+                -DCMAKE_INSTALL_PREFIX="$AOMP_INSTALL_DIR"
+                -DROCM_PATH="$AOMP_INSTALL_DIR"
+                -DCMAKE_PREFIX_PATH:PATH="$AOMP_INSTALL_DIR"
+                -DCPACK_SET_DESTDIR=OFF
+                -DCMAKE_BUILD_TYPE=Release
+                -DTensile_CODE_OBJECT_VERSION=default
+                -DTensile_LOGIC=asm_full
+                -DTensile_TEST_LOCAL_PATH="$AOMP_REPOS/rocmlibs/Tensile"
+                -DTensile_SEPARATE_ARCHITECTURES=ON
+                -DTensile_LAZY_LIBRARY_LOADING=ON
+                -DTensile_LIBRARY_FORMAT=msgpack
+                -DBUILD_WITH_HIPBLASLT=OFF
+                -DROCTX_PATH="$AOMP_INSTALL_DIR"
+                -DGPU_TARGETS="$ROCMLIBS_GFXLIST")
+
    echo "Beginning cmake for rocblas..."
-   cd $BUILD_DIR/build/rocmlibs/rocBLAS
-   echo $AOMP_CMAKE $MYCMAKEOPTS $_repo_dir
-   $AOMP_CMAKE $MYCMAKEOPTS $_repo_dir
-   if [ $? != 0 ] ; then
-     echo "ERROR cmake failed. Cmake flags"
-     echo "      $MYCMAKEOPTS"
-     exit 1
-   fi
+   mkdir -p "$BuildDir"
+   pushd "$BuildDir" >& /dev/null || exit
+   echo "${AOMP_CMAKE}" "$(shquot "${MYCMAKEOPTS[@]}")" "$SrcDir"
 
-   make -j$AOMP_JOB_THREADS
-
-   if [ $? != 0 ] ; then
-      echo "ERROR make -j $AOMP_JOB_THREADS failed"
+   if ! ${AOMP_CMAKE} "${MYCMAKEOPTS[@]}" "$SrcDir"; then
+      echo "ERROR cmake failed. Cmake flags"
+      echo "      $(shquot "${MYCMAKEOPTS[@]}")"
       exit 1
    fi
-fi
+   popd >& /dev/null || exit
+}
 
-if [ "$1" == "install" ] ; then
-   echo " -----Installing to $AOMP_INSTALL_DIR ---- "
+task_build() {
+   local Cfg=$1
+   local BuildDir
+   local Jobs
+   BuildDir="$(get_build_dir "$Cfg")"
+   Jobs="$(cfgvar AOMP_JOB_THREADS)"
 
-   if [ "$BUILD_TYPE" == "Release" ] ; then
-      _build_type_dir=release
-   else
-      _build_type_dir=debug
-   fi
-   cd $BUILD_DIR/build/rocmlibs/rocBLAS
-   make -j$AOMP_JOB_THREADS install
-   if [ $? != 0 ] ; then
-      echo "ERROR install to $AOMP_INSTALL_DIR failed "
+   pushd "$BuildDir" >& /dev/null || exit
+   echo " -----Running make for rocblas ---- "
+   if ! make -j"$Jobs"; then
+      echo "ERROR make -j $Jobs failed"
       exit 1
    fi
+   popd >& /dev/null || exit
+}
+
+task_install() {
+   local Cfg=$1
+   local BuildDir
+   local InstallDir
+   local Jobs
+   BuildDir="$(get_build_dir "$Cfg")"
+   InstallDir="$(get_install_dir "$Cfg")"
+   Jobs="$(cfgvar AOMP_JOB_THREADS)"
+
+   pushd "$BuildDir" >& /dev/null || exit
+   echo " -----Installing to $InstallDir ---- "
+   if ! make -j"$Jobs" install; then
+      echo "ERROR install to $InstallDir failed "
+      exit 1
+   fi
+   popd >& /dev/null || exit
    echo
-   echo "SUCCESSFUL INSTALL to $AOMP_INSTALL_DIR"
+   echo "SUCCESSFUL INSTALL to $InstallDir"
    echo
-   removepatch $_repo_dir
-   removepatch $_tensile_repo_dir
-else 
-   echo 
-   echo "SUCCESSFUL BUILD, please run:  $0 install"
-   echo "  to install into $AOMP_INSTALL_DIR"
-   echo 
-fi
+}
+
+do_list_configs() {
+  echo "default"
+}
+
+do_list_init() {
+  echo "precheck"
+  echo "patch"
+}
+
+do_list_fini() {
+  echo "unpatch"
+}
+
+# List of tasks per config.
+do_list_tasks() {
+  local Cfg=$1
+  if valid_config "$Cfg"; then
+    echo "clean"
+    echo "cmake"
+    echo "build"
+    echo "install"
+  else
+    echo "Unknown config '$Cfg'"
+  fi
+}
+
+command_dispatcher "$@"
