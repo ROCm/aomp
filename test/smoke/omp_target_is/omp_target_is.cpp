cb55007e7c7efeacb3fb89c4d913ddc85be5b02f
Ron Lieberman <ron.lieberman@amd.com>
2026-08-03 08:34:33 -0500

    [smoke] omp_target_is: 28943 , test missing mi350 ifdef

--- test/smoke/omp_target_is/omp_target_is.cpp
+++ test/smoke/omp_target_is/omp_target_is.cpp
@@ -3,7 +3,7 @@
 
 // The test is meant to run in both USM and non-USM mode.
 
-#if defined(__OFFLOAD_ARCH_gfx90a__) || defined(__OFFLOAD_ARCH_gfx90c__) || defined(__OFFLOAD_ARCH_gfx940__) || defined(__OFFLOAD_ARCH_gfx941__) || defined(__OFFLOAD_ARCH_gfx942__) || defined(__OFFLOAD_ARCH_gfx1010__) || defined(__OFFLOAD_ARCH_gfx1011__) || defined(__OFFLOAD_ARCH_gfx1012__) || defined(__OFFLOAD_ARCH_gfx1013__)
+#if defined(__OFFLOAD_ARCH_gfx90a__) || defined(__OFFLOAD_ARCH_gfx90c__) || defined(__OFFLOAD_ARCH_gfx940__) || defined(__OFFLOAD_ARCH_gfx941__) || defined(__OFFLOAD_ARCH_gfx942__) || defined(__OFFLOAD_ARCH_gfx1010__) || defined(__OFFLOAD_ARCH_gfx1011__) || defined(__OFFLOAD_ARCH_gfx1012__) || defined(__OFFLOAD_ARCH_gfx1013__) || defined(__OFFLOAD_ARCH_gfx950__) || defined(__OFFLOAD_ARCH_gfx1250__) || defined(__OFFLOAD_ARCH_gfx1251__)
 #define IS_USM 1
 #else
 #define IS_USM 0
